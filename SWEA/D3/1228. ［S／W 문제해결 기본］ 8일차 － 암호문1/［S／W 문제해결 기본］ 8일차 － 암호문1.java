9c5ab455d0ed51e65524bf16ed50f318374fd5bf
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-02-05 16:43:13 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 161 ms, Memory: 24,068 KB -BaekjoonHub

--- SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -1,3 +1,4 @@
+// package ps;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -6,46 +7,39 @@ import java.util.Scanner;
 
 public class Solution {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		int tc = 10;
+
 		
-		for(int t = 1; t <= tc; t++) {
+		for (int t = 1; t <= tc; t++) {
 			List<Integer> list = new ArrayList<>();
-			
+
 			int n = sc.nextInt();
-			for(int i = 0; i < n; i++) {
+			for (int i = 0; i < n; i++) {
 				int num = sc.nextInt();
 				list.add(num);
 			}
-			
+
 			
 			int m = sc.nextInt();
-			for(int i = 0; i < m; i++) {
-				 char ch = sc.next().charAt(0);
-				 int here = sc.nextInt();
-				 int range = sc.nextInt();
-				 
-				 int cnt = 0;
-				 for(int j = here; j < here + range; j++) {
-					 int temp = sc.nextInt();
-					 list.add(j + cnt, temp);
-				 }
-				
+			for (int i = 0; i < m; i++) {
+				char ch = sc.next().charAt(0); // I
+				int here = sc.nextInt(); // 앞에서부터 x의 위치 바로 다음
+				int y = sc.nextInt(); // y개의 숫자
+
+				int offset = 0;
+				for (int j = here; j < here + y; j++) {
+					int temp = sc.nextInt();
+					list.add(j + offset, temp);
+				}
+
 			}
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+
 			System.out.print("#" + t + " ");
-			for(int i = 0; i < 10; i++)System.out.printf("%d ", list.get(i));
-            System.out.println();
+			for (int i = 0; i < 10; i++)
+				System.out.printf("%d ", list.get(i));
+			System.out.println();
 		}
 
 	}
