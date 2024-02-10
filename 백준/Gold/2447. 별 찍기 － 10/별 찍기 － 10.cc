40687948c30a2252940a2ca4e27df614975bb6c4
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-02-10 20:58:10 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 116 ms, Memory: 26240 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -1,34 +1,54 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
+char a[4000][6200];
+int n;
+void star(int k, int i, int j);
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-void recur(int i, int j, int x) {
-    if (x == 1) {
-        cout << "*";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+
+        for (int j = 0; j < n; j++) { 
+            a[i][j] = ' ';
+        }
+    }
+
+    star(n, 0, 0);
+
+    for (int i = 0; i < n; i++) { // n - 1 까지
+
+        for (int j = 0; j < n; j++) { // 2n - 1 까지
+
+            cout << a[i][j];
+        }
+        cout << '\n';
+    }
+
+}
+void star(int k, int i, int j) {
+    if (k == 3) {
+
+        a[i][j] = a[i][j + 1] = a[i][j + 2] = '*'; // 1층
+
+        a[i + 1][j] = a[i + 1][j + 2] = '*'; // 2층
+
+        a[i + 2][j] = a[i + 2][j + 1] = a[i + 2][j + 2] = '*'; // 3층
         return;
     }
-    else if ((i/(x/3)) % 3 == 1 && (j/(x/3)) % 3 == 1) {
-        cout << " ";
-    }
-    else {
-        recur(i, j, (x/3));
-    }
-}
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    int n;
-    cin >> n;
-    
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            recur(i, j, n);
-        }
-        cout << "\n";
-    }
+    int a = k / 3; 
+    int b =  a * 2;
 
-    return 0;
+    star(k / 3, i, j);
+    star(k / 3, i, j + a);
+    star(k / 3, i, j + b);
+
+    star(k / 3, i + a, j);
+    star(k / 3, i + a, j + b);
+
+    star(k / 3, i + b, j);
+    star(k / 3, i + b, j + a);
+    star(k / 3, i + b, j + b);
 }
