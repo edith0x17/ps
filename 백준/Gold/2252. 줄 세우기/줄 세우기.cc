387e46900f400103484acac08b52487ba0f6841a
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-02-13 21:57:22 +0900

    [Gold III] Title: 줄 세우기, Time: 32 ms, Memory: 3956 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int n, m;
+int ind[32004];
+vector<int> adj[32004];
+queue<int> q;
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n >> m;
+
+    for(int i = 0; i < m; i++){
+        int f, t;
+        cin >> f >> t;
+
+        adj[f].push_back(t);
+        ind[t]++; // 노드 indegree++;
+    }
+
+    for(int i = 1; i <= n; i++){ 
+        if (ind[i] == 0)q.push(i); // indegree 0인 노드 -> queue에 넣기
+    }
+
+    while(!q.empty()){ // nextNode, nowNode
+        int nowNode = q.front(); 
+        q.pop();
+
+        cout << nowNode << ' ';
+        
+        for(int nextNode: adj[nowNode]){
+            
+            ind[nextNode]--; // 해당 노드 indegree--;
+
+            if (ind[nextNode] == 0) {
+               q.push(nextNode);
+            }
+        }
+    }
+
+    return 0;
+}
