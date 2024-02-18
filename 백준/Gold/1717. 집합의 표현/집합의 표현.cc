80c0bce405a35d610abb8c33964b8ec03ffbba74
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-02-18 22:15:27 +0900

    [Gold V] Title: 집합의 표현, Time: 36 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,48 +1,36 @@
-#include<iostream>
-#include<vector>
-
+#include <iostream>
 using namespace std;
 int n, m;
-int parent[1000001];
-
-int getParent(int x) {
-    if (parent[x] == x) return x;
-    return parent[x] = getParent(parent[x]);
-}
-void unionParent(int a, int b) {
-    a = getParent(a);
-    b = getParent(b);
-    if (a > b) parent[a] = b;
-    else parent[b] = a;
-}
-void findParent(int a, int b) {
-    a = getParent(a);
-    b = getParent(b);
-    if (a == b) cout << "YES\n";
-    else cout << "NO\n";
+int p[1000004];
+// p[i] -> i의 부모, p[i]는 i의 부모가 누구에요???
+int Find(int x){
+    if(x == p[x])return x; // 부모 찾음
+    else return p[x] = Find(p[x]); // 부모 못찾음
 }
 
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+void Union(int x, int y){
+    x = Find(x); // x의 부모
+    y = Find(y); // y의 부모
+    p[y] = p[x]; // y의 부모 <- x의 부모
+}
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     cin >> n >> m;
-    for (int i = 1; i <= n; i++) {
-        parent[i] = i;
-    }
-    for (int i = 0; i < m; i++) {
-        int o, a, b;
-        cin >> o >> a >> b;
-        if (!o) {// union연산
-            unionParent(a, b);
-        }
-        else { //find연산
-            findParent(a, b);
-        }
+    
+    for(int i = 0; i <= n + 1; i++)p[i] = i;
 
-    }
+    for(int i = 0; i < m; i++){
+        int type, x, y;
+		cin >> type >> x >> y;
 
+        if (type == 0) {
+			Union(x, y);
+		}
+		else {
+			if (Find(x) == Find(y))cout << "YES\n";
+			else cout << "NO\n";
+		}
+    }
     return 0;
 }
