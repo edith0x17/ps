a2754c273f25f0535c4ce2e8a022a7078c381f16
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-02-22 13:41:03 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,241 ms, Memory: 117,352 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,68 +1,85 @@
-import java.util.*;
+import java.awt.dnd.DropTarget;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-class Solution {
+public class Solution {
 
-    static class Edge implements Comparable<Edge> {
-        long from, to, c; // int를 long으로 변경
+    static long v, e; // int를 long으로 변경
+    static long[] p; // int를 long으로 변경
+    static Data[] d;
 
-        public Edge(long from, long to, long c) { // int를 long으로 변경
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= t; tc++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            v = Long.parseLong(st.nextToken()); // int를 long으로 변경
+            e = Long.parseLong(st.nextToken()); // int를 long으로 변경
+
+            p = new long[(int)v+ 1]; // int를 long으로 변경
+            for (int i = 0; i <= v; i++) p[i] = i;
+
+            d = new Data[(int) (e)]; // int를 long으로 변경
+
+            for (int i = 0; i < e; i++) {
+                st = new StringTokenizer(br.readLine());
+                long from = Long.parseLong(st.nextToken()); // int를 long으로 변경
+                long to = Long.parseLong(st.nextToken()); // int를 long으로 변경
+                long c = Long.parseLong(st.nextToken()); // int를 long으로 변경
+
+                d[i] = new Data(from, to, c);
+            }
+
+            Arrays.sort(d);
+
+            long ret = 0; // int를 long으로 변경
+
+            for (int i = 0; i < e; i++) {
+                long t1 = Find(d[i].from), t2 = Find(d[i].to); // int를 long으로 변경
+
+                if (t1 != t2) { // 다른그룹 이면 -> Union
+                    ret += d[i].c;
+                    Union(t1, t2);
+                }
+            }
+            System.out.println("#" + tc + ' ' + ret);
+        }
+    }
+
+    static long Find(long x) { // int를 long으로 변경
+        if (x == p[(int)x]) return x;
+        else return p[(int)x] = Find(p[(int)x]); // int를 long으로 변경
+    }
+
+    static void Union(long x, long y) { // int를 long으로 변경
+        x = Find(x); // int를 long으로 변경
+        y = Find(y); // int를 long으로 변경
+        p[(int)y] = p[(int)x]; // int를 long으로 변경
+    }
+
+    static class Data implements Comparable<Data> {
+        long from; // int를 long으로 변경
+        long to; // int를 long으로 변경
+        long c; // int를 long으로 변경
+
+        public Data() {
+        }
+
+        public Data(long from, long to, long c) { // int를 long으로 변경
             this.from = from;
             this.to = to;
             this.c = c;
         }
 
         @Override
-        public int compareTo(Edge other) {
-            return Long.compare(this.c, other.c); // int를 long으로 변경
+        public int compareTo(Data o) {
+            return Long.compare(this.c, o.c); // int를 long으로 변경
         }
-    }
-
-    static long[] p; // int를 long으로 변경
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = scanner.nextInt();
-        for(int tc = 1; tc <= t; tc++){
-            int n = scanner.nextInt();
-            int m = scanner.nextInt();
-
-            p = new long[n + 1]; // int를 long으로 변경
-            for (int i = 1; i <= n; i++) p[i] = i;
-
-            Edge[] edges = new Edge[m];
-            for (int i = 0; i < m; i++) {
-                long from = scanner.nextLong(); // int를 long으로 변경
-                long to = scanner.nextLong(); // int를 long으로 변경
-                long c = scanner.nextLong(); // int를 long으로 변경
-                edges[i] = new Edge(from, to, c);
-            }
-
-            Arrays.sort(edges);
-
-            long ret = 0; // int를 long으로 변경
-            for (Edge edge : edges) {
-                long t1 = find(edge.from);
-                long t2 = find(edge.to);
-
-                if (t1 != t2) {
-                    ret += edge.c;
-                    union(t1, t2);
-                }
-            }
-
-            System.out.println("#" + tc + ' ' + ret);
-        }
-
-    }
-
-    static long find(long x) { // int를 long으로 변경
-        if (x == p[(int)x]) return x; // int를 long으로 변경
-        return p[(int)x] = find(p[(int)x]); // int를 long으로 변경
-    }
-
-    static void union(long x, long y) { // int를 long으로 변경
-        x = find(x); // int를 long으로 변경
-        y = find(y); // int를 long으로 변경
-        p[(int)y] = (int)x; // int를 long으로 변경
     }
 }
