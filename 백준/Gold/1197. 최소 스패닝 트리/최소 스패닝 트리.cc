44386afbdb91247ad154c57871fe52802ae672c2
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-02-22 10:36:05 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 32 ms, Memory: 3236 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -9,16 +9,16 @@ struct A {
    }
 };
 A a[100001];
-int p[10001];
 
-int Find(int x) {
-   if (p[x] == x)return x;
-   else return p[x] = Find(p[x]);
+int p[10001];
+int Find(int x){
+    if(x == p[x])return x; // 부모 찾음
+    else return p[x] = Find(p[x]); // 부모 못찾음
 }
 void Union(int x, int y) {
    x = p[x];
    y = p[y];
-   p[y] = x;
+   p[y] = p[x];
 }
 
 int main() {
@@ -27,24 +27,25 @@ int main() {
    int n, m;
    cin >> n >> m;
 
-   for (int i = 1; i <= n; i++)p[i] = i; // 부모 <- 자식
-
+   for (int i = 1; i <= n; i++)p[i] = i; // make-set
+		
+	 // 연결
    for (int i = 0; i < m; i++) {
       cin >> a[i].f >> a[i].t >> a[i].c;
    }
 
-   sort(a, a + m);
+   sort(a, a + m); // 정렬
 
-   int ans = 0;
-   for (int i = 0; i < m; i++){
+   int ret = 0;
+   for (int i = 0; i < m; i++){ // 간선들을...
 
       int t1 = Find(a[i].f), t2 = Find(a[i].t);
 
       if (t1 != t2){ // 다른그룹 이면 -> Union
-         ans += a[i].c;
+         ret += a[i].c;
          Union(t1, t2);
       }
    }
    
-   cout << ans;
+   cout << ret;
 }
