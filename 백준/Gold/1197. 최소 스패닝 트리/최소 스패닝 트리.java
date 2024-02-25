bdc49a2fad715a6ec03249a29ff31d61f830285c
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-02-25 22:20:00 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 576 ms, Memory: 47628 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,79 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 
     static int v, e;
-    static int[] p;
-    static Node[] nodes;
+    static int[] p = new int[10004]; // UnionFind
+    static PriorityQueue<Data> pq = new PriorityQueue<>();
 
-    static class Node implements Comparable<Node>{
-        int from;
-        int to;
-        int c;
-
-        public Node() {
-        }
-
-        public Node(int from, int to, int c) {
-            this.from = from;
-            this.to = to;
-            this.c = c;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return Integer.compare(this.c, o.c);
-        }
-    }
-    
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         v = Integer.parseInt(st.nextToken());
         e = Integer.parseInt(st.nextToken());
 
-        p = new int[v + 4];
-        for (int i = 1; i <= v; i++) p[i] = i; // make set
-
-        nodes = new Node[e];
-
-        for(int i = 0; i < e; i++){
+        for (int i = 0; i < 10004; i++) p[i] = i; // make set
+        for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
-            int f = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
 
-            nodes[i] = new Node(f, t, c);
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            pq.add(new Data(from, to, cost));
         }
 
-        Arrays.sort(nodes);
-
         int ret = 0;
-        for(int i = 0; i < e; i++){
+        while(!pq.isEmpty()){
+            Data d = pq.poll();
 
-            int t1 = Find(nodes[i].from);
-            int t2 = Find(nodes[i].to);
-
-            if(t1 != t2){ // 다른 그룹이면 Union
+            int t1 = Find(d.f); int t2 = Find(d.t);
+            if(t1 != t2){
                 Union(t1, t2);
-                ret += nodes[i].c;
+                ret += d.c;
             }
         }
 
         System.out.println(ret);
     }
 
-    static void Union(int x, int y){
+    static void Union(int x, int y) {
         x = Find(x);
         y = Find(y);
         p[y] = p[x];
     }
 
-    static int Find(int x){
-        if(x == p[x])return x;
+    static int Find(int x) {
+        if (x == p[x]) return x;
         else return p[x] = Find(p[x]);
+    }
+
+    static class Data implements Comparable<Data> {
+        int f;
+        int t;
+        int c;
+
+        public Data(int f, int t, int c) {
+            this.f = f;
+            this.t = t;
+            this.c = c;
+        }
+
+        @Override
+        public int compareTo(Data o) {
+            return Integer.compare(this.c, o.c); // 오름차순
+        }
     }
 }
