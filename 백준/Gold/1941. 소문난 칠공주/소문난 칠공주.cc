4b957c56fffd963db11d810a0ebd22998dcc86f9
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-03-01 18:07:23 +0900

    [Gold III] Title: 소문난 칠공주, Time: 108 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
@@ -1,50 +1,103 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
 using namespace std;
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
+const int n = 5;
 
-struct xy {int x, y;};
-string board[5];
+int ret;
+int a[9][9], aTemp[9][9], visited[9][9];
+vector<pair<int, int>> vv;
 
-int abs(int a) {return a < 0 ? -a : a;}
+int dfs(int x, int y){
+	int ret = 1;
 
-bool isConnected(const vector<xy> &v) {
-    queue<int> Q;
-    Q.push(6);
-    bool vis[6];
-    for(int i=0; i<6; i++) vis[i] = false;
-    while(!Q.empty()) {
-        int cur = Q.front(); Q.pop();
-        for(int nxt = 0; nxt < 6; nxt++) {
-            if(vis[nxt]) continue;
-            if(abs(v[nxt].x - v[cur].x) + abs(v[nxt].y - v[cur].y) == 1) {
-                vis[nxt] = true;
-                Q.push(nxt);
-            }
-        }
-    }
-    for(bool x:vis) if(!x) return false;
-    return true;
+	visited[x][y] = 1;
+	for(int i = 0; i < 4; i++){
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		if (nx < 0 || nx >= n || ny < 0 || ny >= n)continue;
+
+		if (aTemp[nx][ny] == 1 && !visited[nx][ny]){
+			ret += dfs(nx, ny);
+		}
+	}
+
+	return ret;
 }
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    for(int i=0; i<5; i++) cin >> board[i];
-    
-    int ans = 0;
-    vector mask(25, 1);
-    for(int i=0; i<7; i++) mask[i] = 0;
-    do {
-        vector<xy> selected;
-        for(int i=0; i<25; i++)
-            if(mask[i] == 0) selected.push_back({ i / 5, i % 5 });
-        if(!isConnected(selected)) continue;
+bool colorCheck(vector<int> temp){
 
-        int cnt = 0;
-        for(const auto &[x, y]:selected)
-            if(board[x][y] == 'S' && ++cnt == 4) break;
-        if(cnt == 4) ans++;
-    } while(next_permutation(mask.begin(), mask.end()));
-    cout << ans;
+	int cntS = 0, cntY = 0;
+	for(int num = 0; num < temp.size(); num++){
+		int x = vv[temp[num]].first;
+		int y = vv[temp[num]].second;
+		if(a[x][y] == 1)cntS++;
+		else cntY++;
+	}
+
+	if(cntY < cntS)return true;
+	else return false;
+}
+
+bool connectCheck(vector<int> temp){
+	
+	fill(&aTemp[0][0], &aTemp[0][0] + 9 * 9, 0);
+	fill(&visited[0][0], &visited[0][0] + 9 * 9, 0);
+
+	for(int num = 0; num < temp.size(); num++){
+		int x = vv[temp[num]].first;
+		int y = vv[temp[num]].second;
+		
+		aTemp[x][y] = 1;
+	}
+
+	int x = vv[temp[0]].first;
+	int y = vv[temp[0]].second;
+
+	if(dfs(x, y) == 7){
+		return true;
+	}
+
+	return false;
+}
+
+void combi(int start, vector<int> b){
+	if(b.size() == 7){
+
+		if(colorCheck(b)){
+			if(connectCheck(b))ret++;
+		}
+		return;
+	}
+
+	for(int i = start + 1; i < vv.size(); i++){
+		b.push_back(i);
+		combi(i, b);
+		b.pop_back();
+	}
+}
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+
+			char ch;
+			cin >> ch;
+			if(ch == 'S')a[i][j] = 1;
+			else a[i][j] = 2;
+
+			vv.push_back({i, j});
+		}
+	}
+
+	vector<int> b;
+	combi(-1, b);
+
+	cout << ret << '\n';
+	return 0;
 }
