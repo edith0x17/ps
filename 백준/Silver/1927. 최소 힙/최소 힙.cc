f4689a790bdff0926770ee64a5805ffe706d2756
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-03-01 21:50:17 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int n;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+	cin >> n;
+
+	for(int i = 0; i < n; i++){
+
+		int x;
+		cin >> x;
+		if(x == 0){
+			if(!pq.empty()){
+				cout << pq.top() << '\n';
+				pq.pop();
+			}else cout << 0 << '\n';
+		}else pq.push(x);
+	}
+	return 0;
+}
