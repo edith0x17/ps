bb4fbe929b4d6b2fdd225745c1437df4a152f0b1
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-05-28 00:00:46 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 84 ms, Memory: 11696 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,73 +1,60 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static ArrayList<int[]> v;
-    static int[] a;
+    static int n;
+    static boolean[] a;
+    static int m;
+    static int flag, idx;
 
-    static void fastIO() {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        a = new int[n + 1];
+        n = Integer.parseInt(br.readLine());
+
+        a = new boolean[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
-            a[i] = scanner.nextInt();
+            int temp = Integer.parseInt(st.nextToken());
+            a[i] = temp == 1;
         }
 
-        m = scanner.nextInt();
-
-        v = new ArrayList<>();
+        m = Integer.parseInt(br.readLine());
         for (int i = 0; i < m; i++) {
-            int x = scanner.nextInt();
-            int y = scanner.nextInt();
-            v.add(new int[]{x, y});
+            st = new StringTokenizer(br.readLine());
+            flag = Integer.parseInt(st.nextToken());
+            idx = Integer.parseInt(st.nextToken());
+
+            go(flag, idx);
         }
-    }
-
-    static void solve() {
-        for (int[] p : v) { // <성별, 스위치 번호>
-            if (p[0] == 1) { // 남자
-                for (int i = 1; i <= n; i++) {
-                    if (i % p[1] == 0) { // 배수 -> 반전
-                        if (a[i] == 1) a[i] = 0; // ON
-                        else a[i] = 1; // OFF
-                    }
-                }
-            } else { // 여자
-                int left = p[1], right = p[1];
-
-                int cnt = 0;
-                while (true) {
-                    if (left - 1 < 1 || right + 1 > n) break;
-
-                    if (a[left - 1] != a[right + 1]) break;
-
-                    left = left - 1;
-                    right = right + 1;
-                    cnt++;
-                }
-
-                if (cnt == 0) { // 대칭 X
-                    if (a[p[1]] == 1) a[p[1]] = 0;
-                    else a[p[1]] = 1;
-                } else { // 대칭 O
-                    for (int i = left; i <= right; i++) {
-                        if (a[i] == 1) a[i] = 0;
-                        else a[i] = 1;
-                    }
-                }
-            }
-        }
-
+        
         for (int i = 1; i <= n; i++) {
-            System.out.print(a[i] + " ");
+            System.out.print(a[i] ? "1 " : "0 ");
             if (i % 20 == 0) System.out.println();
         }
     }
 
-    public static void main(String[] args) {
-        fastIO();
-        solve();
+    static void go(int flag, int idx) {
+        if (flag == 1) { // 남자
+            for (int i = idx; i <= n; i++) {
+                if((i % idx) == 0){
+                    a[i] = !a[i];
+                }
+            }
+        } else { // 여자
+            a[idx] = !a[idx];
+            int i = 1;
+            while (idx - i >= 1 && idx + i <= n) {
+                if (a[idx - i] == a[idx + i]) {
+                    a[idx - i] = !a[idx - i];
+                    a[idx + i] = !a[idx + i];
+                } else {
+                    break;
+                }
+                i++;
+            }
+        }
     }
 }
