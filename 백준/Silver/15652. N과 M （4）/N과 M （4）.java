c8b346130b3cc167ccc4fc9bc82721bf2bae52f6
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-05-29 02:13:00 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 19796 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,34 +1,50 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main{
-
     static int n, m;
-    static int[] ret;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static int[] a, ret;
+    static boolean[] visited;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = sc.nextInt();
-        m = sc.nextInt();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        ret = new int[m];
+        a = new int[n + 4];
+        for(int i = 0; i < n; i++){
+            a[i] = i + 1;
+        }
+        ret = new int[m + 4];
+        visited = new boolean[n + 4];
 
-//        go(int cnt, int start);
-        go(0, 1);
+        combi(0, 0);
+
+        bw.flush();
+        bw.close();
     }
 
-    static void go(int cnt, int start){
-        if(cnt == m){
-            for(int num: ret){
-                System.out.printf("%d ", num);
-            }
-            System.out.println();
+    static void combi(int depth, int start) throws IOException{
+        if(depth == m){
+            print();
             return;
         }
 
-        for(int i = start; i <= n; i++){
-            ret[cnt] = i;
-            go(cnt + 1, i);
+        for(int i = start; i < n; i++){
+            ret[depth] = a[i];
+            combi(depth + 1, i );
+            //
         }
+    }
 
+    static void print() throws IOException{
+        for(int i = 0; i < m; i++){
+//            System.out.printf("%d ", ret[i]);
+            bw.write(ret[i] + " ");
+        }
+//        System.out.println();
+        bw.write("\n");
     }
 }
