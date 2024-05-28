f731bc09178c39b3f79f420353f6245d3474ebf8
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-05-29 03:14:31 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 812 ms, Memory: 69944 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,32 +1,45 @@
+import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main{
 
-    static int n, m;
-    static int[] arr, sum;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static int n, m; // n개의 수, m번
+    static int[] a, aSum;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = sc.nextInt();
-        m = sc.nextInt();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        arr = new int[n + 1];
-        sum = new int[n + 1];
+        a = new int[n + 4];
+        aSum = new int[n + 4];
 
+        st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++){
-            arr[i] = sc.nextInt();
+            a[i] = Integer.parseInt(st.nextToken());
         }
 
         for(int i = 1; i <= n; i++){
-            sum[i] = sum[i - 1] + arr[i];
+            aSum[i] = aSum[i - 1] + a[i]; // 전 + 현
         }
+
+//        System.out.println(Arrays.toString(a));
+//        System.out.println(Arrays.toString(aSum));
 
         for(int i = 0; i < m; i++){
-            int f= sc.nextInt();
-            int t = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
 
-            System.out.println(sum[t] - sum[f - 1]);
+            int ret = aSum[e] - aSum[s - 1];
+            bw.write(ret + "");
+            bw.write("\n");
         }
+
+        bw.flush();
+        bw.close();
     }
 }
