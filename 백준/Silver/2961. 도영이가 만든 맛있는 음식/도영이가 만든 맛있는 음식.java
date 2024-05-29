aa69fee4ff9cbfe628ee17034579d39cfe809c9b
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-05-29 15:13:09 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 76 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,39 +1,42 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
 
-	static int n;
-	static int[] s, b;
-	static int mi = 1000000000;
+    static int n;
+    static long[] s, b; // 신맛(곱), 쓴맛(합)
+    static long ret = Long.MAX_VALUE;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		Scanner sc = new Scanner(System.in);
+        n = Integer.parseInt(br.readLine());
+        s = new long[n];
+        b = new long[n];
 
-		n = sc.nextInt();
-		s = new int[n];
-		b = new int[n];
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            s[i] = Long.parseLong(st.nextToken());
+            b[i] = Long.parseLong(st.nextToken());
+        }
 
-		for (int i = 0; i < n; i++) {
-			s[i] = sc.nextInt();
-			b[i] = sc.nextInt();
-		}
+        for(int i = 1; i < (1 << n); i++){
+            long sour = 1;
+            long bitter = 0;
+            for(int j = 0; j < n; j++){
+                if((i & (1 << j)) != 0){ // 선택O
+                    sour *= s[j];
+                    bitter += b[j];
+                }else{ // 선택X
+                    continue;
+                }
+            }
+            ret = Math.min(ret, Math.abs(sour - bitter));
+        }
 
-		int S = (1 << n);
-
-		for (int i = 1; i < S; i++) {
-
-			int sumS = 1, sumB = 0;
-			for (int j = 0; j < n; j++) {
-				if ((i & (1 << j)) != 0) {
-					sumS *= s[j];
-					sumB += b[j];
-				}
-			}
-
-			mi = Math.min(mi, Math.abs(sumS - sumB));
-
-		}
-		System.out.println(mi);
-	}
+        bw.write(ret + "\n");
+        bw.flush();
+        bw.close();
+    }
 }
