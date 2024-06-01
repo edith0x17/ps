1efade3529fe7043f2206cf610dc783a51e6baff
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-06-01 15:38:09 +0900

    [Silver IV] Title: 카드2, Time: 128 ms, Memory: 32136 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,34 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class Main{
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-//		System.out.println(n);
-		
-		Queue<Integer> q = new ArrayDeque<>();
-		
-		for(int i = 1; i <= n; i++) {
-			q.offer(i);
-		}
-		
-		// System.out.println("q.size() : " + q.size());
-		
-		while(q.size() != 1) {
-			// 첫번째 삭제
-			q.poll(); 
-			
-			// 두번째 삭제 후 큐에 삽입
-			int p = q.poll(); 
-			q.offer(p); 
-		}
-		
-		System.out.println(q.poll());
-	}
+    static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n;
+    static Queue<Integer> q = new ArrayDeque<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            q.offer(i + 1);
+        }
+
+        while(q.size() > 1){
+            q.poll(); // 제일 위에 있는 카드를 바닥에 버린다
+
+            int temp = q.poll(); // 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다
+            q.offer(temp);
+        }
+
+        sb.append(q.peek());
+
+        bw.write(sb + "");
+        bw.flush();
+        bw.close();
+    }
 }
