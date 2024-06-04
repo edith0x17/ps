a15ea5d3d87b5061bebf5c54cd5f71c1c3e233a9
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-06-04 17:00:50 +0900

    [Silver V] Title: 색종이, Time: 84 ms, Memory: 11708 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,42 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {
-
-    static int[][] a = new int[100][100];
+public class Main{
+    static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n;
+    static int[][] map = new int[104][104];
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        for(int k = 0; k < n; k++){
-            int y, x;
+        n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            y = Integer.parseInt(st.nextToken());
-            x = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            // x행 y열
-            for(int i = x; i < x + 10; i++ ){
-                for(int j = y; j < y + 10; j++){
-                    if(i < 0 || i >= 100 || j < 0 || j >= 100)continue;
-
-                    a[i][j] = 1;
+            for(int x = a; x < a + 10; x++){
+                for(int y = b; y < b + 10; y++){
+                    map[x][y]++;
                 }
             }
         }
 
         int ret = 0;
-        for(int i = 0; i < 100; i++ ){
+        for(int i = 0; i < 100; i++){
             for(int j = 0; j < 100; j++){
-                ret += a[i][j];
+                if(map[i][j] != 0)ret++;
             }
         }
+        sb.append(ret);
 
-        StringBuilder sb = new StringBuilder();
-        sb.append(ret).append('\n');
-        System.out.println(sb);
-//        System.out.println(ret);
+        bw.write(sb+ "");
+        bw.flush();
+        bw.close();
     }
 }
