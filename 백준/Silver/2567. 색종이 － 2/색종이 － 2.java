ee94d516740673c98360da188574b8c5ae7ff1d6
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-06-04 17:20:58 +0900

    [Silver IV] Title: 색종이 - 2, Time: 88 ms, Memory: 11736 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
@@ -1,56 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int[][] a = new int[1004][1004];
-
+public class Main{
+    static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {-1, 0, 1, 0};
+    static int n;
+    static int[][] map = new int[104][104];
+    static int ret;
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
-        for(int k = 0; k < n; k++){
-            int y, x;
+        n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            y = Integer.parseInt(st.nextToken());
-            x = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            // x행 y열
-            for(int i = x; i < x + 10; i++ ){
-                for(int j = y; j < y + 10; j++){
-                    if(i < 0 || i >= 100 || j < 0 || j >= 100)continue;
-
-                    a[i][j] = 1;
+            for(int x = a; x < a + 10; x++){
+                for(int y = b; y < b + 10; y++){
+                    map[x][y]++;
                 }
             }
         }
 
-        int ret = 0;
-
-        for(int i = 0; i < 100; i++ ){
+        for(int i = 0; i < 100; i++){
             for(int j = 0; j < 100; j++){
-
-                if(a[i][j] == 1){
-
-                    for(int k = 0; k < 4; k++){
-
-                        int nx = i + dx[k];
-                        int ny = j + dy[k];
-
-                        if(a[nx][ny] == 0 || nx < 0 || nx >= 100 || ny < 0 || ny >= 100)ret++; // 0인 부분 카운트 || 범위 초과 부분 카운트
-
-                    }
-                }
+                if(map[i][j] != 0)go(i, j);
             }
         }
-
         sb.append(ret);
-        System.out.println(sb);
 
+        bw.write(sb+ "");
+        bw.flush();
+        bw.close();
+    }
+
+    static void go(int x, int y){
+        for(int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || nx >= 101 || ny < 0 || ny >= 101)continue; // 범위
+
+            if(map[nx][ny] == 0)ret++;
+        }
     }
 }
