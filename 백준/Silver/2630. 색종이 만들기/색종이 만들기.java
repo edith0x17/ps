020c79822a93038a92f23620b399b60d297076e5
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-06-08 23:22:47 +0900

    [Silver II] Title: 색종이 만들기, Time: 116 ms, Memory: 13104 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,62 +1,66 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.Arrays;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main{
+    static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int n;
-    static int cntW, cntB;
-    static int[][] map;
+    static int n, ansW, ansB;
+    static int[][] a;
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         n = Integer.parseInt(br.readLine());
 
-        map = new int[n][n];
+        a = new int[n + 4][n + 4];
+
         for(int i = 0; i < n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j = 0; j < n; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
+                a[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         go(n, 0, 0);
 
-        System.out.println(cntW);
-        System.out.println(cntB);
+        sb.append(ansW).append("\n").append(ansB);
 
+        bw.write(sb + "");
+        bw.flush();
+        bw.close();
     }
 
     static boolean check(int s, int x, int y){
-
-        int prev = map[x][y];
+        int prev = a[x][y];
 
         for(int i = x; i < x + s; i++){
-
             for(int j = y; j < y + s; j++){
-                if(prev != map[i][j])return false;
+                if(prev != a[i][j])return false;
             }
         }
-
         return true;
     }
-    static void go(int s, int x, int y){
 
-        if(check(s, x, y)){
-            if(map[x][y] == 1)cntB++;
-            else cntW++;
+/*    void go(){
+        탈출조건
+        if(...){
 
-            return;
         }
 
-        int halfS = s / 2;
+        분할정복
+    }*/
 
-        go(halfS, x, y);
-        go(halfS, x, y + halfS);
-        go(halfS, x + halfS, y);
-        go(halfS, x + halfS, y + halfS);
+    static void go(int s, int x, int y){
+        if(check(s, x, y)){ // 모두 같은 색 O
+            if(a[x][y] == 0)ansW++;
+            else ansB++;
+        }else{ // 모두 같은 색 X
+            int halfS = s/ 2;
+
+            go(halfS, x, y);
+            go(halfS, x + halfS, y);
+            go(halfS, x, y  + halfS);
+            go(halfS, x + halfS, y + halfS);
+        }
     }
 }
