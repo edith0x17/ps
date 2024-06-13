0dd2d0824a3c7bf222fbf37c1aa9a6e27cd47713
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-06-13 14:22:59 +0900

    [Gold IV] Title: 치즈, Time: 108 ms, Memory: 13764 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,94 +1,93 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int n, m;
-    static ArrayList<Data> arrayList = new ArrayList<>();
-    static int[][] a;
-    static boolean[][] visited;
-    static int cntTime, cntLast;
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        a = new int[n][m];
-
-        visited = new boolean[n][m];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                a[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        while(true){
-            arrayList.clear();
-            visited = new boolean[n][m];
-            cntLast = 0;
-
-            dfs(0, 0);
-
-            for(Data d: arrayList){ // 치즈 녹이기
-                a[d.x][d.y] = 0;
-                cntLast++;
-            }
-
-            boolean flag = false;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (a[i][j] != 0) flag = true; // 치즈 O
-                }
-            }
-
-            cntTime++;
-            if(!flag)break; // 치즈 X
-        }
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(cntTime).append('\n').append(cntLast);
-        System.out.println(sb);
-
-//        System.out.println(cntTime);
-//        System.out.println(cntLast);
-    }
-
-    static void dfs(int x, int y) {
-        visited[x][y] = true;
-
-        if(a[x][y] == 1){ // 치즈 -> 넣기
-            arrayList.add(new Data(x, y));
-            return;
-        }
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny]) continue;
-
-            dfs(nx, ny);
-        }
-    }
-
-    static class Data{ // pair
+public class Main{
+    static class Data{
         int x;
         int y;
-
-        public Data() {
-        }
 
         public Data(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+    }
+
+    static final int dx[] = {-1, 0, 1, 0};
+    static final int dy[] = {0, 1, 0, -1};
+
+    static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int n, m;
+    static int[][] a;
+    static boolean[][] visited;
+    static ArrayList<Data> adj;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        a = new int[n][m];
+        visited = new boolean[n][m];
+        adj = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++){
+                a[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int cntTime = 0, cntLast = 0;
+        while(true){
+            // init
+            visited = new boolean[n][m];
+            adj.clear();
+            cntLast = 0;
+            
+            go(0, 0);
+
+            for(Data d: adj){
+                a[d.x][d.y] = 0;
+                cntLast++;
+            }
+            cntTime++;
+
+            boolean flag = false;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (a[i][j] == 1)flag = true;
+                }
+            }
+
+            if (!flag)break;
+        }
+
+        sb.append(cntTime + "\n" + cntLast);
+
+        bw.write(sb + "");
+        bw.flush();
+        bw.close();
+    }
+
+    static void go(int x, int y){
+        visited[x][y] = true;
+
+        if(a[x][y] == 1){
+            adj.add(new Data(x, y));
+            return;
+        }
+
+        for(int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny])continue; // 방문 || 범위
+
+            go(nx, ny);
         }
     }
 }
