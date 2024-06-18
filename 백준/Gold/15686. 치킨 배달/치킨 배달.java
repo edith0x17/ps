e9994c1805a7e9ffcac877ad68883fb317d7ee91
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-06-18 17:20:10 +0900

    [Gold V] Title: 치킨 배달, Time: 120 ms, Memory: 13524 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,98 +1,84 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
 
-    static int n, m, sum = 987654321;
-    static ArrayList<Data> homeList = new ArrayList<>();
-    static ArrayList<Data> chickenList = new ArrayList<>();
-    static ArrayList<Data> cc = new ArrayList<>();
-    static ArrayList<Integer> bb = new ArrayList<>();
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < n; i++) {
-
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                int num = Integer.parseInt(st.nextToken());
-                if (num == 2) chickenList.add(new Data(i, j)); // 치킨집
-                else if (num == 1) homeList.add(new Data(i, j)); // 집
-            }
-        }
-
-        combi(-1, bb);
-
-        System.out.println(sum);
-
-    }
-
-    static void go() {
-        int sumsum = 0;
-        for (Data h : homeList) {
-            int distance = Integer.MAX_VALUE; // 최대값으로 초기화
-
-            for (Data chicken : cc) {
-                distance = Math.min(distance, Math.abs(h.x - chicken.x) + Math.abs(h.y - chicken.y));
-            }
-
-            sumsum += distance;
-        }
-
-        sum = Math.min(sum, sumsum);
-    }
-
-    static void combi(int start, ArrayList<Integer> b) {
-        if (b.size() == m) {
-            for (int j : b) {
-
-//                int tempX = chickenList.get(j).x;
-//                int tempY = chickenList.get(j).y;
-//                cc.add(new Data(tempX, tempY));
-                cc.add(chickenList.get(j));
-            }
-
-            // cc로 로직
-            go();
-            cc.clear();
-            return;
-        }
-
-        for (int i = start + 1; i < chickenList.size(); i++) {
-            b.add(i);
-            combi(i, b);
-            b.remove(b.size() - 1);
-        }
-    }
-
-    static class Data {
+    static class Data{
         int x;
         int y;
-
-        public Data() {
-        }
 
         public Data(int x, int y) {
             this.x = x;
             this.y = y;
         }
+    }
 
-        @Override
-        public String toString() {
-            return "Data{" +
-                    "x=" + x +
-                    ", y=" + y +
-                    '}';
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+
+    static int n, m, ans = 987654321;
+    static int[][] a;
+    static int[] ret;
+    static ArrayList<Data> home = new ArrayList<>();
+    static ArrayList<Data> chicken = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        a = new int[n][n];
+        Data[] chi = new Data[m];
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++){
+                a[i][j] = Integer.parseInt(st.nextToken());
+
+                if(a[i][j] == 1)home.add(new Data(i, j)); // 집
+                else if(a[i][j] == 2)chicken.add(new Data(i, j)); // 치킨집
+            }
+        }
+
+        combi(0, 0, chi);
+
+        sb.append(ans);
+
+        bw.write(ans + "");
+        bw.flush();
+        bw.close();
+    }
+
+    static int dist(int x1, int y1, int x2, int y2){
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
+    }
+
+    static int go(Data[] chi){
+        int ret = 0;
+        for(int i = 0; i < home.size(); i++){
+            int mi = 987654321;
+            for(int j = 0; j < chi.length; j++){
+                mi = Math.min(mi, dist(home.get(i).x, home.get(i).y, chi[j].x, chi[j].y));
+            }
+            ret += mi;
+        }
+
+        return ret;
+    }
+
+    static void combi(int start, int idx, Data[] chi){
+        if(idx == m){
+            ans = Math.min(ans, go(chi));
+            return;
+        }
+
+        for(int i = start; i < chicken.size(); i++){
+            chi[idx] = chicken.get(i);
+            combi(i + 1, idx + 1, chi);
         }
     }
 }
