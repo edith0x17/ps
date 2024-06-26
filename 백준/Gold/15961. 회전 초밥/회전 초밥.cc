12fcfad13cf345edefbbf6fad8109ef908c8f597
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-06-26 20:24:45 +0900

    [Gold IV] Title: 회전 초밥, Time: 552 ms, Memory: 26728 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.cc
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.cc
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int n, d, k, c, ret;
+int n, d, k, c;
 vector<int> a;
 unordered_map<int, int> mp;
-
-int main() {
+int ret = -987654321;
+int main(){
     ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
     
     cin >> n >> d >> k >> c;
@@ -13,42 +12,33 @@ int main() {
     for(int i = 0; i < n; i++){
         int temp;
         cin >> temp;
-
         a.push_back(temp);
     }
-
-    // 원형 리스트를 위해 앞의 k - 1개 요소를 배열 끝에 추가
-    for (int i = 0; i < k - 1; i++) {
+    for(int i = 0; i < k - 1; i++){
         a.push_back(a[i]);
         n++;
     }
 
-    // 처음 k개의 요소와 쿠폰 초밥을 맵에 추가
     for(int i = 0; i < k; i++){
         mp[a[i]]++;
     }
     mp[c]++;
 
     int l = 0, r = k;
-
-    // 슬라이딩 윈도우를 사용하여 초밥 종류 계산
     for(int i = r; i < n; i++){
-        // 왼쪽 요소 제거
+        // 왼
         if(--mp[a[l]] == 0){
             mp.erase(a[l]);
         }
-
-        // 오른쪽 요소 추가
+        
+        // 오
         mp[a[i]]++;
 
-        // 현재 초밥 종류의 최대값 갱신
-        ret = max(ret, (int)mp.size());
-
-        // 슬라이딩 윈도우 l++
         l++;
+
+        ret = max(ret, (int)mp.size());
     }
 
     cout << ret << "\n";
-
     return 0;
 }
