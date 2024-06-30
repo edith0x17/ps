dd4e917693b328fa41370063fea1424b1feac9d6
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-06-30 23:24:43 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,28 +1,20 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-int d[1004];
-void FastIO() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-}
+int n;
+int dp[1004];
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    
+    cin >> n;
 
-void Solve() {
-	d[1] = 1;
-	d[2] = 2;
+    dp[1] = 1;
+    dp[2] = 2;
 
-	int n;
-	cin >> n;
+    for(int i = 3; i <= n; i++){
+        dp[i] = ((dp[i - 1] % 10007) + (dp[i - 2] % 10007)) % 10007;
+    }
 
-	for (int i = 3; i <= n; i++) {
-		d[i] = (d[i - 1] % 10007) + (d[i - 2] % 10007); // d[i - 1] -> 가로 2, d[i - 2] -> 가로 1
-		d[i] %= 10007;
-	}
-	cout << d[n] << '\n';
-}
-int main() {
-	FastIO();
-	Solve();
-	return 0;
+    cout << dp[n] << "\n";
+    
+    return 0;
 }
