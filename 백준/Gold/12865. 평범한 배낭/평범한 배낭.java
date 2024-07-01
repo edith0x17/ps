c4075044ba2723d702bab4c020bc172729db420d
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-07-01 13:06:11 +0900

    [Gold V] Title: 평범한 배낭, Time: 148 ms, Memory: 52908 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -10,9 +10,7 @@ public class Main{
     static int[] v = new int[100004];
     static int[][] dp = new int[104][100004];
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
@@ -24,23 +22,13 @@ public class Main{
             v[i] = Integer.parseInt(st.nextToken());
         }
 
-        /*
-            dp[i][j] = k
-            i번째 물건
-            가방의 무게 j
-            가치의 합 k
-        */
-
         for(int i = 1; i <= n; i++){
-
             for(int j = 1; j <= k; j++){
-
                 if(j >= w[i]){ // 담을 수 있는 경우
                     dp[i][j] = Math.max(dp[i - 1][j - w[i]] + v[i], dp[i - 1][j]); // 담는 경우 || 담지 않는 경우
-                }else dp[i][j] = dp[i - 1][j]; // 담을 수 없는 경우 
+                }else dp[i][j] = dp[i - 1][j]; // 담을 수 없는 경우
             }
         }
-
         System.out.println(dp[n][k]);
 
     }
