0a1723b48711ec87c26726636f60a6362e619248
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-07-17 22:00:39 +0900

    [Gold IV] Title: 스도쿠, Time: 148 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.cc
+++ 백준/Gold/2239. 스도쿠/스도쿠.cc
@@ -1,12 +1,11 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+int a[14][14];
+vector<pair<int, int>> vv;
 
-int a[9][9];
-bool check1[104][104], check2[104][104], check3[104][104] ; // check1, 2 -> 행 열, check3 -> ㅁ * 9
-vector<pair<int, int>> vv; // 0인 지점 담기
-bool flag = false;
+bool check1[104][104], check2[104][104], check3[104][104];
 
+bool flag;
 void print(){
     for(int i = 0; i < 9; i++){
         for(int j = 0; j < 9; j++){
@@ -15,62 +14,58 @@ void print(){
         cout << '\n';
     }
 }
-void solve(int idx){ // idx == depth
 
-    if(idx == vv.size()){ // vv 다 소비
+void go(int idx){ // 
+    if(idx == vv.size()){
         flag = true;
         print();
         return;
     }
 
-    int x = vv[idx].first; int y = vv[idx].second;
+    int x = vv[idx].first; 
+    int y = vv[idx].second;
     for(int i = 1; i <= 9; i++){
-        if(check1[x][i])continue; // 행 체크
-
-        if(check2[y][i])continue; // 열 체크
-
-        int sum = (x / 3) * 3 + (y / 3);
-        if(check3[sum][i])continue; // ㅁ 체크
+        if(check1[x][i])continue;
+        if(check2[y][i])continue;
+        int sum = (x/ 3)* 3 + (y/ 3);
+        if(check3[sum][i])continue;
 
         a[x][y] = i;
         check1[x][i] = true;
         check2[y][i] = true;
         check3[sum][i] = true;
 
-        solve(idx + 1);
-        if(flag)return; // 사전순 출력
+        go(idx + 1);
+        if(flag)return;
 
         check1[x][i] = false;
         check2[y][i] = false;
         check3[sum][i] = false;
     }
-
-    
-
 }
 
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    
     for(int i = 0; i < 9; i++){
+        string s;
+        cin >> s;
         for(int j = 0; j < 9; j++){
-            scanf("%1d", &a[i][j]);
-
+            a[i][j] = s[j] - '0';
             int value = a[i][j];
 
-            if(a[i][j] == 0){ // 0인 지점 담기
-                vv.push_back({i, j}); 
-            }else{ // 0이 아닌 지점 방문 표시
-                check1[i][value] = true;
-                check2[j][value] = true;
+            if(value == 0)vv.push_back({i, j});
+            else{
+                check1[i][value] = true;// 행 ex) 0, 1, 2, 3
+                check2[j][value] = true;// 열 
 
-                int sum = (i / 3) * 3 + (j / 3);
+                int sum = (i/ 3) * 3 + (j/ 3); //ㅁ
                 check3[sum][value] = true;
             }
         }
     }
-
-    solve(0);
+    
+    go(0);
 
     return 0;
 }
