7a1e4f8a1e505f1496a87fa9c3cebe77d1ed392a
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-08-24 11:15:04 +0900

    [Gold III] Title: 중량제한, Time: 56 ms, Memory: 5584 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.cc
+++ 백준/Gold/1939. 중량제한/중량제한.cc
@@ -1,51 +1,50 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-
-int n, m, from, to, ret;
+int n, m, s, e;
+vector<pair<int, int>> adj[10004];
 int visited[10004];
-vector<pair<int, int>> v[10004];
-// pair<int, int> 목적지, 중량제한
-void dfs(int here, int ww) {
-	for (pair<int, int> p : v[here]) {
-		if (!visited[p.first] && p.second >= ww) { // 방문X && 가중치 >= ww
+int ret;
 
-			visited[p.first] = 1;
-			dfs(p.first, ww);
+void dfs(int here, int weight) {
+
+	visited[here] = 1;
+	for (pair<int, int> there : adj[here]){
+		if (!visited[there.first] && there.second >= weight) { // {there.first, there.second} -> {there, c}
+			dfs(there.first, weight);
 		}
 	}
 }
 
-int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+int main(){
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
 	cin >> n >> m;
 
 	for (int i = 0; i < m; i++) {
-		int f, t, w;
-		cin >> f >> t >> w; // from to weight
-		v[f].push_back({ t, w }); // to weight
-		v[t].push_back({ f, w }); // from weight
+		int f, t, c;
+		cin >> f >> t >> c;
+		adj[f].push_back({ t, c });
+		adj[t].push_back({ f, c });
 	}
 
-	cin >> from >> to;
+	cin >> s >> e;
 
-	
 	int l = 0, r = 1000000000;
 	while (l <= r) {
 		fill(&visited[0], &visited[0] + 10004, 0);
 
 		int mid = (l + r) / 2;
 
-		dfs(from, mid);
+		dfs(s, mid);
 
-		if (visited[to]) {
-			l = mid + 1;
-		}
+		if (visited[e]){
+            ret = mid;
+            l = mid + 1;
+        }
 		else r = mid - 1;
 	}
 
-	cout << r << '\n';
+	cout << ret << '\n';
 
 	return 0;
 }
