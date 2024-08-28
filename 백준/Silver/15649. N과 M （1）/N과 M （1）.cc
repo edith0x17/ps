62fb661446b9ef1800921613d61409a2eaedb89a
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-08-28 22:16:29 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,40 +1,37 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+
 int n, m;
-int visited[12];
+int a[12], visited[12];
+void go(int idx, vector<int> b){
+	// 종료
+		// 시뮬레이션
+	if(idx == m){
+		for(auto at: b){
+			cout << at << " ";
+		}
+		cout << "\n";
+		return;
+	}
 
-void solve(vector<int> b){
-    // 종료
-        // 시뮬레이션
-
-    // 실행
-    // 재귀호출
-    if(b.size() == m){
-        for(int num: b){
-            cout << num << " ";
-        }
-        cout << '\n';
-        return;
-    }
-    
-    for(int i = 1; i <= n; i++){
-        if(!visited[i]){
-            visited[i] = 1;
-            b.push_back(i);
-            
-            solve(b);
-            
-            visited[i] = 0;
-            b.pop_back();
-        }
-    }
+	for(int i = 0; i < n; i++){
+		if(!visited[i]){
+			visited[i] = 1;
+			b.push_back(a[i]);
+			go(idx + 1, b);
+			b.pop_back();
+			visited[i] = 0;
+		}
+	}
 }
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-    cin >> n >> m;
-    vector<int> b;
-    solve(b);
-    return 0;
+	cin >> n >> m;
+	for(int i = 0; i < n; i++){
+		a[i] = i + 1;
+	}
+	vector<int> b;
+	go(0, b);
+	return 0;
 }
