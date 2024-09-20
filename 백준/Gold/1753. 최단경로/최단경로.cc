02c121707922b583800778c6adf4154aa85d019c
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-09-20 09:22:17 +0900

    [Gold IV] Title: 최단경로, Time: 88 ms, Memory: 14888 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,68 +1,51 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 const int INF = 987654321;
-int n, m;
-int d[20001];
+
 struct A {
-   int x, d; // 해당정점까지, 해당정점까지의 가중치 => x, d[x]
-
-//    bool operator<(const A& p)const {
-//       return d > p.d;
-//    }
+	int to;
+	int weight;
 };
-vector<A> v[20001];
-// priority_queue<A> pq;
-
-struct cmp{
-    
-    bool operator()(A o1, A o2){
-        return o1.d > o2.d;
-    }
+struct cmp {
+	bool operator()(const A& a, const A& b)const {
+		return a.weight > b.weight;// 오름차순
+	}
 };
+int v, e, k;
+int visited[200004], d[200004];
+vector<A> vv[200004];
 priority_queue<A, vector<A>, cmp> pq;
-
-void dijkstra(int start) {
-   for (int i = 1; i <= n; i++)d[i] = INF; // INF
-
-   d[start] = 0;
-   
-   pq.push({start, 0});
-   while (!pq.empty()) {
-      int x = pq.top().x;
-      pq.pop();
-
-      for (auto next: v[x]) { // next -> struct A...
-
-         if (d[next.x] > d[x] + next.d) { // direct || bypass
-
-            d[next.x] = d[x] + next.d;
-            pq.push({next.x, d[next.x]});
-         }
-      }
-   }
-   
-}
 int main() {
-   ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-   cin >> n >> m;
+	cin >> v >> e >> k;
+	for (int i = 0; i < e; i++) {
+		int f, t, w;
+		cin >> f >> t >> w;
+		vv[f].push_back({ t, w });
+	}
+	for (int i = 0; i <= v; i++) {
+		d[i] = INF;
+	}
 
-   int start;
-   cin >> start;
+	d[k] = 0;
+	pq.push({k, d[k]});
+	while (pq.size()) {
+		A here = pq.top();
+		pq.pop();
 
-   while (m--) {
-      int x, y, z;
-      cin >> x >> y >> z;
+		for (A there : vv[here.to]) {
+			if (visited[there.to])continue;
 
-      v[x].push_back({y, z}); // 방향그래프
-   }
-
-   dijkstra(start);
-
-   for (int i = 1; i <= n; i++) {
-
-      if (d[i] == INF)cout << "INF\n";
-      else cout << d[i] << '\n';
-   }
+			if (d[there.to] > d[here.to] + there.weight) {
+				d[there.to] = d[here.to] + there.weight;
+				pq.push({ there.to, d[there.to] });
+			}
+		}
+	}
+	for (int i = 1; i <= v; i++) {
+		if (d[i] == INF)cout << "INF\n";
+		else cout << d[i] << "\n";
+	}
+	return 0;
 }
