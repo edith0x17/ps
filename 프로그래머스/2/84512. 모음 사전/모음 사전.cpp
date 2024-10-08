18c98733fcab01b2e8a6afb4a30f4506fe3a4370
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-10-08 22:15:06 +0900

    [level 2] Title: 모음 사전, Time: 0.44 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -1,26 +1,23 @@
-#include <iostream>
 #include <string>
 #include <vector>
-#include <unordered_map>
 using namespace std;
-int cnt;
-unordered_map<string, int> mp;
-void dfs(string s, string aeiou) {
+string aeiou = "AEIOU";
+int answer, cnt = -1;
+string target;
+void dfs(string s){
     if(s.size() == 6)return;
-    // 현재 문자열을 mp에 저장하고 카운트 증가
-    mp.insert({s, cnt++});
-
-    // aeiou 중 하나를 추가하여 새로운 문자열을 만들고 재귀 호출
-    for (char c : aeiou) {
-        string next = s + c;  // 현재 문자열에 'aeiou' 중 하나를 추가
-        if (mp.find(next) != mp.end()) continue;  // 이미 처리된 문자열은 스킵
-        dfs(next, aeiou);  // 재귀적으로 새로운 문자열을 탐색
+    
+    cnt++;
+    if(s == target){
+        answer = cnt;
+    }
+    
+    for(int i = 0; i< 5; i++){
+        dfs(s + aeiou[i]);
     }
 }
-
 int solution(string word) {
-    int answer = 0;
-    dfs("", "AEIOU");
-    answer = mp[word];
+    target = word;
+    dfs("");
     return answer;
 }
