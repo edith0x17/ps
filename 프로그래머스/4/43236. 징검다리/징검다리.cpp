4918b879f4797a48e6c994e81adcf6d456eb169e
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-10-14 22:08:39 +0900

    [level 4] Title: 징검다리, Time: 5.60 ms, Memory: 5.03 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.cpp
+++ 프로그래머스/4/43236. 징검다리/징검다리.cpp
@@ -1,32 +1,28 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <cmath>
 
 using namespace std;
-// 묻는 바를 반대로 생각하여 n개의 돌을 없애서 돌 사이 거리의 최솟값이 x로 만들 수 있는가? 
+
 int solution(int distance, vector<int> rocks, int n) {
     int answer = 0;
-    
     sort(rocks.begin(), rocks.end());
-    
-    int l = 0, r = distance;
-    while(l <= r){
-        int cnt = 0;
-        int mid = (l + r)/ 2;
-        
-        int prev = 0;
+    int mi = 0, mx = distance;
+    while(mi <= mx){
+        int mid = (mi + mx)/ 2;// 최소길이
+        int cnt = 0, prev = 0;
         for(int i = 0; i < rocks.size(); i++){
-            if(rocks[i] - prev < mid)cnt++; // 삭제할 돌
+            if(rocks[i] - prev < mid)cnt++;
             else prev = rocks[i];
         }
-        if(distance - prev < mid) cnt++;
+        if(distance - prev < mid)cnt++;
         
-        if(cnt <= n){
+        if(cnt <= n){// 최소길이 높이기
             answer = max(answer, mid);
-            l = mid + 1;
-        }else r = mid - 1;
+            mi = mid + 1;
+        }else{// 최소길이 낮추기
+            mx = mid - 1;
+        }
     }
-    
     return answer;
 }
