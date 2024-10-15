99490ca322e80a6446878de201eb201e2bc42ce7
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-10-16 02:21:10 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 68 ms, Memory: 11768 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,58 +1,51 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main{
 
     static StringBuilder sb = new StringBuilder();
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
     static int n, m;
     static boolean[][] a;
-    static int ret = Integer.MAX_VALUE;
+    static int answer = 54 * 54;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        a = new boolean[n][m];
-
+        a = new boolean[n + 4][m + 4];
         for(int i = 0; i < n; i++){
-            String s = "";
-            s = br.readLine();
+            String temp = br.readLine();
             for(int j = 0; j < m; j++){
-                if(s.charAt(j) == 'W')a[i][j] = true; //W
-                else a[i][j] = false; //B
+                if(temp.charAt(j) == 'W')a[i][j] = false;
+                else a[i][j] = true;
             }
         }
-
         for(int i = 0; i <= n - 8; i++){
             for(int j = 0; j <= m - 8; j++){
-                ret = Math.min(ret, go(i, j));
+                answer = Math.min(answer, check(i, j));
             }
         }
-
-        sb.append(ret);
-
+        sb.append(answer);
         bw.write(sb + "");
         bw.flush();
         bw.close();
     }
-
-    static int go(int x, int y){
+    static int check(int x, int y){
         int cnt = 0;
-        boolean check = a[x][y]; //W
-
+        boolean check = a[x][y];
         for(int i = x; i < x + 8; i++){
             for(int j = y; j < y + 8; j++){
-                if(check != a[i][j])cnt++;
-
-                check = !check; //BWBW
+                if(check != a[i][j])cnt++;// 예상하는 결과값 다를 시 cnt++
+                check = !check;// B -> W, W -> B
             }
-            check = !check; //WBWB, 반대로
+            check = !check;// ...
         }
-
         return Math.min(cnt, 64 - cnt);
     }
 }
