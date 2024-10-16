247594534370d0352786a0db933a148d2fd7924b
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-10-16 21:37:22 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 568 ms, Memory: 85848 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,44 +1,45 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main{
 
-    static int n, m; // n개의 수, m번
-    static int[] a, aSum;
+    static StringBuilder sb = new StringBuilder();
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = null;
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        a = new int[n + 4];
-        aSum = new int[n + 4];
-
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] a = new int[n + 1];
+        int[] sum = new int[n + 1];
         st = new StringTokenizer(br.readLine());
         for(int i = 1; i <= n; i++){
             a[i] = Integer.parseInt(st.nextToken());
         }
-
         for(int i = 1; i <= n; i++){
-            aSum[i] = aSum[i - 1] + a[i]; // 전 + 현
+            sum[i] = sum[i - 1] + a[i];
         }
-
-//        System.out.println(Arrays.toString(a));
-//        System.out.println(Arrays.toString(aSum));
-
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
 
-            int ret = aSum[e] - aSum[s - 1];
-            bw.write(ret + "");
-            bw.write("\n");
+            if(start == end){
+                sb.append(a[start] + "\n");
+            }else if(start == 1){
+                sb.append(sum[end] + "\n");
+            }else{
+                sb.append(sum[end] - sum[start - 1] + "\n");
+            }
         }
-
+        bw.write(sb + "");
         bw.flush();
         bw.close();
     }
