28acea62c8fa00f17199103c35c280bed0c4c022
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-10-16 22:37:12 +0900

    [Silver I] Title: 회의실 배정, Time: 368 ms, Memory: 41740 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,66 +1,57 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main{
 
-    static class Data implements Comparable<Data>{
-        int start;
-        int end;
-
-        public Data(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-
-        @Override
-        public int compareTo(Data o) {
-            if(this.end == o.end){
-                return (this.start - o.start); // 시작 -> 오름차순
-            }
-
-            return (this.end - o.end); // 끝 -> 오름차순
-        }
-
-        @Override
-        public String toString() {
-            return "Data{" +
-                    "start=" + start +
-                    ", end=" + end +
-                    '}';
-        }
-    }
     static StringBuilder sb = new StringBuilder();
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    static int n;
-    static PriorityQueue<Data> pq = new PriorityQueue<>();
-    static int ret;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
 
-        n = Integer.parseInt(br.readLine());
-
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Data> pq = new PriorityQueue<>();
         for(int i = 0; i < n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             pq.offer(new Data(start, end));
         }
-
-        int last = -1;
+        int answer = 0, endTime = -1;
         while(!pq.isEmpty()){
             Data data = pq.poll();
-            if(last <= data.start){
-                ret++;
-                last = data.end;
+            if(endTime <= data.s){
+                answer++;
+                endTime = data.e;
             }
         }
-
-        sb.append(ret);
-
-        bw.write(sb + "");
+        sb.append(answer);
+        bw.write(answer + "");
         bw.flush();
         bw.close();
+    }
+
+    static class Data implements Comparable<Data>{
+        int s, e;
+        Data(){
+
+        }
+        Data(int s, int e){
+            this.s = s;
+            this.e = e;
+        }
+
+        @Override
+        public int compareTo(Data o) {
+            if(this.e == o.e){
+                return this.s - o.s;
+            }
+            return this.e - o.e;
+        }
     }
 }
