22f2434a4972ee86031cc25a7d5dcdc9dd48a1ea
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-11-13 22:32:24 +0900

    [Gold IV] Title: 최단경로, Time: 820 ms, Memory: 118764 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,83 +1,61 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
-    static int v, e;
-    static int k;
-    static ArrayList<A>[] adj; // {to, c}
-    static int[] d = new int[20001];
-    static PriorityQueue<A> pq = new PriorityQueue<>();
-    static int INF = Integer.MAX_VALUE;
-    static class A implements Comparable<A>{
-        int to;
-        int c;
-
-        public A(int to, int c) {
+    static class Data implements Comparable<Data>{// from
+        int to;// to
+        int distance;// distance
+        public Data(int to, int distance) {
             this.to = to;
-            this.c = c;
+            this.distance = distance;
         }
-
         @Override
-        public int compareTo(A o) {
-            return Integer.compare(this.c, o.c);
+        public int compareTo(Data o){
+            return this.distance - o.distance;
         }
     }
-
+    static StringBuilder sb = new StringBuilder();
+    static int v, e, k;
+    static ArrayList<Data>[] adj = new ArrayList[20004];
+    static PriorityQueue<Data> pq = new PriorityQueue<>();
+    static int[] d = new int[20004];
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        for(int i = 0; i < 20004; i++){
+            adj[i] = new ArrayList<Data>();
+            d[i] = 987654321;
+        }
         v = Integer.parseInt(st.nextToken());
         e = Integer.parseInt(st.nextToken());
-
-        k = Integer.parseInt(br.readLine());
-
-        adj = new ArrayList[v + 4];
-        for (int i = 0; i < v + 4; i++) {
-            adj[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < e; i++) {
+        k = Integer.parseInt(br.readLine());// start
+        for(int i = 0; i < e; i++){
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            int z = Integer.parseInt(st.nextToken());
-
-            adj[x].add(new A(y, z)); // 단방향
+            int f = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            int distance = Integer.parseInt(st.nextToken());
+            adj[f].add(new Data(t, distance));
         }
 
-        dijkstra(k);
+        d[k] = 0;
+        pq.add(new Data(k, 0));
+        while(!pq.isEmpty()){
+            Data data = pq.poll();
+            int here = data.to;
+            int distance = data.distance;
+            
+            if (d[here] < distance) continue;// 최적화
 
-        for (int i = 1; i <= v; i++){
-            bw.write(d[i] == INF ? "INF\n" : d[i] + "\n");
-        }
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    static void dijkstra(int start) {
-
-        for (int i = 0; i < 20001; i++) d[i] = INF; // INF
-
-        d[start] = 0;
-        pq.add(new A(start, 0));
-
-        while (!pq.isEmpty()) {
-
-            A here = pq.poll(); // front, pop
-
-            for (A next : adj[here.to]) {
-                if (d[next.to] > d[here.to] + next.c) { // direct || bypass
-                    d[next.to] = d[here.to] + next.c;
-                    pq.add(new A(next.to, d[next.to]));
+            for(Data there: adj[here]){
+                if(d[there.to] > d[here] + there.distance){// 직빵 > 우회
+                    d[there.to] = d[here] + there.distance;
+                    pq.add(new Data(there.to, d[there.to]));
                 }
             }
+        }
+        for (int i = 1; i <= v; i++) {
+            if (d[i] == 987654321) System.out.println("INF");
+            else System.out.println(d[i]);
         }
     }
 }
