0c51522a440b7f4b44c649da7df2a5e338778fa4
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-11-27 23:30:45 +0900

    [Gold III] Title: 줄 세우기, Time: 376 ms, Memory: 49440 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,49 +1,41 @@
+import java.awt.font.ShapeGraphicAttribute;
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
+public class Main{
     static StringBuilder sb = new StringBuilder();
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n, m;
     static int[] ind;
-
-    public static void main(String[] args) throws IOException {
+    static ArrayList<Integer>[] adj;
+    static Queue<Integer> q = new ArrayDeque<>();
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        ArrayList<Integer>[] adj = new ArrayList[n + 1];
-        for (int i = 0; i < n + 1; i++) {
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        ind = new int[n + 4];
+        adj = new ArrayList[n + 4];
+        for(int i = 0; i < n + 4; i++){
             adj[i] = new ArrayList<>();
         }
-        ind = new int[n + 1];
-        for (int i = 0; i < m; i++) {
+        for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
             int f = Integer.parseInt(st.nextToken());
             int t = Integer.parseInt(st.nextToken());
             adj[f].add(t);
             ind[t]++;
         }
-        for (int i = 1; i <= n; i++) {
-            if (ind[i] == 0) {
-                pq.offer(i);
-            }
+        for(int i = 1; i <= n; i++){
+            if(ind[i] == 0)q.offer(i);
         }
-        while (!pq.isEmpty()) {
-            int here = pq.poll();
+        while(!q.isEmpty()){
+            int here = q.poll();
             sb.append(here + " ");
-
-            for (int there : adj[here]) {
+            for(int there: adj[here]){
                 ind[there]--;
-                if (ind[there] == 0) {
-                    pq.offer(there);
-                }
+                if(ind[there] == 0)q.offer(there);
             }
         }
-        bw.write(sb + " ");
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
