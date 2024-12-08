daaced31d83827eeff08c228ad9207a063f592d4
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-12-09 02:05:55 +0900

    [Silver I] Title: 곱셈, Time: 64 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static int a, b, c;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        long answer = go(a, b, c);
+        System.out.println(answer);
+    }
+    static long go(long base, long exponent, long C){
+        if(exponent == 1){
+            return base % C;
+        }
+        long temp = go(base, exponent/ 2, C);// base
+
+        // finally
+        if(exponent % 2 == 1){
+            return ((temp * temp % C) * (base % C)) % C;
+        }
+        return ((temp % C) * (temp % C)) % C;
+    }
+}
