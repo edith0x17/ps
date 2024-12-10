90d27a60aaf0cb536a762c7c0bf1bb6c31fcac2e
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2024-12-11 04:37:39 +0900

    [Silver I] Title: 용돈 관리, Time: 188 ms, Memory: 20772 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -6,30 +6,26 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static int n, m, l, r, answer = Integer.MAX_VALUE;
     static int[] a;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         a = new int[n];
-        l = 0; r = 0;
-
         for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(br.readLine());
             l = Math.max(l, a[i]);
             r += a[i];
         }
-
         while (l <= r) {
             int mid = (l + r) / 2;
             int cnt = 1, sum = mid;
-
             for (int i = 0; i < n; i++) {
                 if (a[i] > mid) {
                     cnt = m + 1; // mid로는 해결 불가
                     break;
                 }
+
                 if (sum - a[i] >= 0) {
                     sum -= a[i];
                 } else {
@@ -37,15 +33,16 @@ public class Main {
                     sum = mid - a[i];
                 }
             }
-
-            if (cnt > m) {
+            
+            if (cnt > m) {// mid를 크게 시도(자잘자잘하게 잘림)
                 l = mid + 1;
             } else {
+                // cnt <= m
+                // mid를 작게 시도(큼지막하게 잘림)
                 answer = Math.min(answer, mid);
                 r = mid - 1;
             }
         }
-
         sb.append(answer);
         bw.write(sb.toString());
         bw.flush();
