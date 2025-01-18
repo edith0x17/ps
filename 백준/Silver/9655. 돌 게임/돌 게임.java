3fa3248295bebfbb1d64d8ae3a6fa9b33f3f939c
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-01-18 19:46:38 +0900

    [Silver V] Title: 돌 게임, Time: 64 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        if(n % 2 == 0) System.out.println("CY");
+        else System.out.println("SK");
+    }
+}
