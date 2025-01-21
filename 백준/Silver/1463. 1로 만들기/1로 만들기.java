268bb06492d9b960dff9adbec8cc08e065ee28a3
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-01-21 17:11:32 +0900

    [Silver III] Title: 1로 만들기, Time: 96 ms, Memory: 15536 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,38 +1,22 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
-// 화폐단위 1, 4, 6
-public class Main{
-
+public class Main {
+    static int n;
+    static int[] dp;
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n + 1];
+        dp[1] = 0;
+        for(int i = 2; i <= n; i++){
+            if(i == 2 || i == 3)dp[i] = 1;
 
-        int[] D = new int[N + 1];
-
-        D[0] = 0;
-        D[1] = 0;
-
-        for(int i = 2; i <= N; i++){
-            int min = D[i - 1] + 1;
-
-            if(i % 3 == 0) min = Math.min(D[i / 3] + 1, min);
-
-            if(i % 2 == 0) min = Math.min(D[i / 2] + 1, min);
-
-
-            D[i] = min;
+            int mi = dp[i - 1] + 1;
+            if(i % 3 == 0)mi = Math.min(mi, dp[i/ 3] + 1);
+            if(i % 2 == 0)mi = Math.min(mi, dp[i/ 2] + 1);
+            dp[i] = mi;
         }
-
-//        System.out.println(Arrays.toString(D));
-
-        bw.write(D[N] +"");
-        bw.flush();
-
-        bw.close();
-        br.close();
+        System.out.println(dp[n]);
     }
 }
