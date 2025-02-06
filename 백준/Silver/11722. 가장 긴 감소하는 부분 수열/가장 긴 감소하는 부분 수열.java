91d43e3b62a6ed4eae538ba3e3ee728e9c083282
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-02-06 16:11:21 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 76 ms, Memory: 12028 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -2,29 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, answer = 1;
-    static int[] arr, dp;
+    static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    static int n;
+    static int[] arr;
+    static ArrayList<Integer> d = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        arr = new int[n + 1];
-        dp = new int[n + 1];
+        arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            dp[i] = 1;
-        }
-
-        // dp[i] => i번째 왔을 때 최대 길이
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(arr[j] > arr[i]){
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
-            }
-            answer = Math.max(answer, dp[i]);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(answer);
+        int answer = lds();
+        sb.append(answer);
+        bw.write(sb + "");
+        bw.flush();
+        bw.close();
+    }
+    static int lds(){
+        d.add(arr[0]);
+
+        for(int i = 1; i < n; i++){
+            if(arr[i] < d.get(d.size() - 1)){// 작을 경우
+                d.add(arr[i]);
+            } else {// 같거나 클 경우
+                int pos = Collections.binarySearch(d, arr[i], Collections.reverseOrder());
+                if(pos < 0) pos = -(pos + 1);
+                d.set(pos, arr[i]);
+            }
+        }
+        return d.size();
+    }
+    static int lis(){
+        d.add(arr[0]);
+
+        for(int i = 1; i < n; i++){
+            if(arr[i] > d.get(d.size() - 1)){// 큰 경우
+                d.add(arr[i]);
+            }else{// 같거나 작을 경우
+                int pos = Collections.binarySearch(d, arr[i]);// 있으면 양수로 들어갈 위치, 없으면 음수로 들어갈 위치 - 1,
+                if(pos < 0)pos = -(pos + 1);
+                d.set(pos, arr[i]);
+            }
+        }
+        return d.size();
     }
 }
