6ee88d91ba992a37d1cc592ac217a7c9d5e034b9
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-03-04 16:06:53 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 276 ms, Memory: 29640 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] a;
+    static ArrayList<Integer> adj = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = n - lis();
+        System.out.println(answer);
+    }
+    static int lis(){
+        adj.add(a[0]);
+        for(int i = 1; i < n; i++){
+            if(a[i] > adj.get(adj.size() - 1)){
+                adj.add(a[i]);
+            }else{
+                int pos = Collections.binarySearch(adj, a[i]);
+                if(pos < 0)pos = -(pos + 1);
+                adj.set(pos, a[i]);
+            }
+        }
+        return adj.size();
+    }
+}
