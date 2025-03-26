0d6442b96e35d563d9d943732bdc11c4fd949f4e
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-03-26 21:48:29 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,8 +1,5 @@
--- 코드를 입력하세요
-SELECT i.ANIMAL_ID, i.NAME
-from ANIMAL_INS i
-inner join ANIMAL_OUTS o 
-where i.ANIMAL_ID = o.ANIMAL_ID
-and
-o.DATETIME - i.DATETIME < 0
-order by i.DATETIME asc;
+select ai.ANIMAL_ID, ai.NAME
+from animal_ins ai
+join animal_outs ao on ai.animal_id = ao.animal_id
+where ao.datetime < ai.datetime
+order by ai.datetime asc;
