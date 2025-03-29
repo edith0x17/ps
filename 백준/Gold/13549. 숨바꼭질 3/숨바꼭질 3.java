a72d2e465b0f78830c02704c4b58e99ef469ac07
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-03-29 17:30:18 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 176 ms, Memory: 21004 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -3,49 +3,58 @@ import java.util.*;
 
 public class Main {
     static int n, k;
-    static int[] visited = new int[100_001]; // 시간 저장
+    static int[] visited = new int[100_001];
     static int answer = Integer.MAX_VALUE;
-
+    static PriorityQueue<Data> pq = new PriorityQueue<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        Arrays.fill(visited, Integer.MAX_VALUE); // 초기화: 모든 노드에 무한대 시간
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1])); // {위치, 시간} 최소 힙
-        pq.offer(new int[]{n, 0}); // 시작점 {위치, 시간}
+        Arrays.fill(visited, Integer.MAX_VALUE);
         visited[n] = 0;
-
+        pq.offer(new Data(n, 0));
         while (!pq.isEmpty()) {
-            int[] current = pq.poll();
-            int here = current[0];
-            int time = current[1];
+            Data cur = pq.poll();
+            int here = cur.pos;
+            int time = cur.time;
 
             if (here == k) {
                 answer = Math.min(answer, time);
                 continue;
             }
 
-            // 이미 더 짧은 경로로 방문한 경우 무시
             if (visited[here] < time) continue;
 
-            // 순간이동 (0초 소요)
-            int teleport = 2 * here;
+            // 순간이동 (0초)
+            int teleport = here * 2;
             if (teleport <= 100_000 && time < visited[teleport]) {
                 visited[teleport] = time;
-                pq.offer(new int[]{teleport, time});
+                pq.offer(new Data(teleport, time));
             }
 
-            // 걷기 (1초 소요)
-            for (int offset : new int[]{1, -1}) {
+            // 걷기 (1초)
+            for (int offset : new int[]{-1, 1}) {
                 int there = here + offset;
                 if (there >= 0 && there <= 100_000 && time + 1 < visited[there]) {
                     visited[there] = time + 1;
-                    pq.offer(new int[]{there, time + 1});
+                    pq.offer(new Data(there, time + 1));
                 }
             }
         }
         System.out.println(answer);
+    }
+    static class Data implements Comparable<Data> {
+        int pos;
+        int time;
+        Data(int pos, int time) {
+            this.pos = pos;
+            this.time = time;
+        }
+        @Override
+        public int compareTo(Data o) {
+            return this.time - o.time; // 시간 기준 오름차순
+        }
     }
 }
