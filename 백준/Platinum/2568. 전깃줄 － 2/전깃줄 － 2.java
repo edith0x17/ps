19dd1ce7441ad1aa74842763f2f0430a983e830e
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-03-31 16:53:53 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 1544 ms, Memory: 75624 KB -BaekjoonHub

--- 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.java
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.java
@@ -5,74 +5,86 @@ public class Main {
     static int n;
     static ArrayList<Data> arrayList = new ArrayList<>();
     static int[] arr, trace;
+    static int[] dpIdx; // LIS 위치 저장
+    static ArrayList<Integer> lis = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n];
         trace = new int[n];
-        StringTokenizer st = null;
+        dpIdx = new int[n];
 
-        for(int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
+        // 입력 처리
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             arrayList.add(new Data(a, b));
         }
+
+        // A 전봇대 기준 정렬
         Collections.sort(arrayList);
-        for(int i = 0; i < n; i++){
+
+        // B 전봇대 값으로 배열 만들기
+        for (int i = 0; i < n; i++) {
             arr[i] = arrayList.get(i).b;
         }
-        int answer = lis();
-        System.out.println(n - answer);
 
-        ArrayList<Integer> lisB = new ArrayList<>();
-        int len = answer - 1;
-        for(int i = n - 1; i >= 0; i--){
-            if(trace[i] == len){
-                lisB.add(arr[i]);
-                len--;
-            }
-        }
-        Set<Integer> lisSet = new HashSet<>(lisB);
-        ArrayList<Integer> removeList = new ArrayList<>();
-        for (Data d : arrayList) {
-            if (!lisSet.contains(d.b)) {
-                removeList.add(d.a);
-            }
-        }
-        Collections.sort(removeList);
-        for(int a : removeList){
-            System.out.println(a);
-        }
-    }
-    static int lis(){
-        ArrayList<Integer> d = new ArrayList<>();
-        d.add(arr[0]);
+        // LIS 계산
+        lis.add(arr[0]);
         trace[0] = 0;
-        for(int i = 1; i < n; i++){
-            if(arr[i] > d.get(d.size() - 1)){
-                d.add(arr[i]);
-                trace[i] = d.size() - 1;
-            }else{
-                int pos = Collections.binarySearch(d, arr[i]);
-                if(pos < 0) pos = -(pos + 1);
-                d.set(pos, arr[i]);
+        for (int i = 1; i < n; i++) {
+            int value = arr[i];
+
+            if (value > lis.get(lis.size() - 1)) {
+                lis.add(value);
+                trace[i] = lis.size() - 1;
+            } else {
+                int pos = Collections.binarySearch(lis, value);
+                if (pos < 0) pos = -(pos + 1);
+                lis.set(pos, value);
                 trace[i] = pos;
             }
         }
-        return d.size();
+
+        // 출력: 제거할 A 전봇대 번호 찾기
+        int len = lis.size() - 1;
+        boolean[] isInLIS = new boolean[n];
+        for (int i = n - 1; i >= 0; i--) {
+            if (trace[i] == len) {
+                isInLIS[i] = true;
+                len--;
+            }
+        }
+
+        // 제거 대상 수집
+        ArrayList<Integer> removeList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!isInLIS[i]) {
+                removeList.add(arrayList.get(i).a); // A 전봇대 번호 출력
+            }
+        }
+
+        // 결과 출력
+        System.out.println(removeList.size());
+        Collections.sort(removeList);
+        for (int num : removeList) {
+            System.out.println(num);
+        }
     }
-    static class Data implements Comparable<Data>{
-        int a;
-        int b;
+
+    static class Data implements Comparable<Data> {
+        int a, b;
+
         public Data(int a, int b) {
             this.a = a;
             this.b = b;
         }
+
         @Override
-        public int compareTo(Data o){
-            return this.a - o.a;
+        public int compareTo(Data o) {
+            return Integer.compare(this.a, o.a);
         }
     }
 }
