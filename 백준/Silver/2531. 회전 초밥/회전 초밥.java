c4f9509f1abd66a68e11298b04f90b67d38df47a
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-04-04 20:43:40 +0900

    [Silver I] Title: 회전 초밥, Time: 124 ms, Memory: 16384 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -1,42 +1,45 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
     static int n, d, k, c;
-    static int[] arr, visited;
-    static int answer;
-    public static void main(String[] args) throws IOException{
+    static int[] a, check;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         d = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
-        arr = new int[n];
-        visited = new int[d + 1];
-        for(int i = 0; i < n; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+        a = new int[n];
+        check = new int[d + 1];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
         }
-        // i %n
-        int mx = 0, cnt = 0;
-        for(int i = 0; i < k; i++){
-            if(visited[arr[i]] == 0)cnt++;
-            visited[arr[i]]++;
-        }
-        if(visited[c] == 0)cnt++;
-        visited[c]++;
-        mx = cnt;
-        for(int i = 1; i < n; i++){
-            // LEFT
-            visited[arr[i - 1]]--;
-            if(visited[arr[i - 1]] == 0)cnt--;
 
-            // RIGHT
-            visited[arr[(i + (k - 1)) % n]]++;// i + (k - 1)
-            if(visited[arr[(i + (k - 1)) % n]] == 1)cnt++;
-
-            mx = Math.max(mx, cnt);
+        int unique = 0;
+        for (int i = 0; i < k; i++) {
+            if (check[a[i]]++ == 0) unique++;// check[a[i]] == 0 -> check[a[i]]++
         }
-        System.out.println(mx);
+        int answer = check[c] == 0 ? unique + 1 : unique;
+
+        int idx = 0;
+        for (int i = k; i < n + k; i++) {
+            // 넣기
+            int add = a[i % n];
+            if (check[add]++ == 0) unique++;
+
+            // 빼기
+            int remove = a[idx++ % n];
+            if (--check[remove] == 0) unique--;
+
+//            // 넣기
+//            int add = a[i % n];
+//            if (check[add]++ == 0) unique++;
+
+            int temp = check[c] == 0 ? unique + 1 : unique;
+            answer = Math.max(answer, temp);
+        }
+        System.out.println(answer);
     }
 }
