d6e61c5379fadd1f6f9277bedca1b28375c3afe9
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-04-28 21:20:46 +0900

    [Silver I] Title: 쿼드트리, Time: 68 ms, Memory: 11612 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,58 +1,47 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     static StringBuilder sb = new StringBuilder();
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
     static int n;
     static int[][] a;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         n = Integer.parseInt(br.readLine());
-
         a = new int[n][n];
-
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
             for (int j = 0; j < n; j++) {
                 a[i][j] = s.charAt(j) - '0';
             }
         }
-
         go(n, 0, 0);
-
-        bw.write(sb + "");
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 
-    static boolean check(int s, int x, int y) {
+    static boolean check(int size, int x, int y) {
         int prev = a[x][y];
-
-        for (int i = x; i < x + s; i++) {
-            for (int j = y; j < y + s; j++) {
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
                 if (prev != a[i][j]) return false;
             }
         }
         return true;
     }
 
-    static void go(int s, int x, int y) {
-        if (check(s, x, y)) {
+    static void go(int size, int x, int y) {
+        if (check(size, x, y)) {
             if (a[x][y] == 1) sb.append(1);
             else sb.append(0);
 
             return;
         }
-        sb.append("(");
-        int halfS = s / 2;
-
-        go(halfS, x, y);
-        go(halfS, x, y + halfS);
-        go(halfS, x + halfS, y);
-        go(halfS, x + halfS, y + halfS);
-        sb.append(")");
+        sb.append('(');
+        go(size / 2, x, y);
+        go(size / 2, x, y + size / 2);
+        go(size / 2, x + size / 2, y);
+        go(size / 2, x + size / 2, y + size / 2);
+        sb.append(')');
     }
 }
