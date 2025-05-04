2c9c865b88abf9161b3e193b0817c413b73d9f39
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-05-04 14:19:09 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 31.37 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,37 +1,35 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int left = 1;
-        int right = 100_000; // diffs의 최대값으로 설정
-        int answer = right;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (canClear(diffs, times, limit, mid)) {
+        int answer = 0;
+        int l = 1, r = 100_000;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (go(diffs, times, limit, mid)) {
                 answer = mid;
-                right = mid - 1;
+                r = mid - 1;
             } else {
-                left = mid + 1;
+                l = mid + 1;
             }
         }
-
         return answer;
     }
 
-    static boolean canClear(int[] diffs, int[] times, long limit, int level) {
+    static boolean go(int[] diffs, int[] times, long limit, int level) {
+        int n = diffs.length;
         long total = 0;
-        for (int i = 0; i < diffs.length; i++) {
+
+        for (int i = 0; i < n; i++) {
             if (level >= diffs[i]) {
                 total += times[i];
             } else {
-                int prev = (i == 0 ? times[i] : times[i - 1]);
-                long mistakeCount = diffs[i] - level;
-                total += (long)(prev + times[i]) * mistakeCount + times[i];
+                if (i == 0) return false; // 첫 퍼즐에서 틀리는 경우는 말이 안됨
+                long cnt = diffs[i] - level;
+                total += (long)(times[i - 1] + times[i]) * cnt + times[i];
             }
 
-            if (total > limit) return false; // 조기 종료 최적화
+            if (total > limit) return false; // 가지치기
         }
-        return total <= limit;
+
+        return true;
     }
 }
