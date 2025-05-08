f79e8e759b71190e5c6d098936e1bd5220581dce
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-05-08 15:12:21 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 51368 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,35 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-
+public class Main {
+    static final int INF = Integer.MAX_VALUE;
     static int n, k;
-    static int[] w = new int[104];
-    static int[] v = new int[100004];
-    static int[][] dp = new int[104][100004];
+    static int[] w, v;
+    static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-
+        w = new int[n + 1];
+        v = new int[n + 1];
+        dp = new int[n + 1][k + 1];
         for(int i = 1; i <= n; i++){
             st = new StringTokenizer(br.readLine());
             w[i] = Integer.parseInt(st.nextToken());
             v[i] = Integer.parseInt(st.nextToken());
         }
-
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= k; j++){
-                if(j >= w[i]){ // 담을 수 있는 경우
-                    dp[i][j] = Math.max(dp[i - 1][j - w[i]] + v[i], dp[i - 1][j]); // 담는 경우 || 담지 않는 경우
-                }else dp[i][j] = dp[i - 1][j]; // 담을 수 없는 경우
+                if(j >= w[i]){
+                    dp[i][j] = Math.max(dp[i - 1][j - w[i]] + v[i], dp[i - 1][j]);
+                }else{
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
         System.out.println(dp[n][k]);
-
     }
 }
