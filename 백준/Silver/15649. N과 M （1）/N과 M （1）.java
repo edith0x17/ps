95e6b9777dd9ca461daf8d6c9c847d43492c8c71
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-05-20 10:00:56 +0900

    [Silver III] Title: N과 M (1), Time: 208 ms, Memory: 57192 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,56 +1,41 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
+    static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int n, m;
     static int[] a, ret;
     static boolean[] visited;
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
         a = new int[n];
         ret = new int[m];
-        visited = new boolean[n];
-
         for(int i = 0; i < n; i++){
             a[i] = i + 1;
         }
-
+        visited = new boolean[n];
         go(0);
-
+        bw.write(sb + "");
         bw.flush();
-        bw.close();
     }
-
-    static void go(int idx) throws IOException {
-        if(idx == m){
-            print();
+    static void go(int depth){
+        if(depth == m){
+            for(int i = 0; i < m; i++){
+                sb.append(ret[i] + " ");
+            }
+            sb.append("\n");
             return;
         }
-
         for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                ret[idx] = a[i];
-
-                go(idx + 1);
-
-                visited[i] = false;
-            }
+            if(visited[i])continue;
+            visited[i] = true;
+            ret[depth] = a[i];
+            go(depth + 1);
+            visited[i] = false;
         }
-    }
-
-    static void print() throws IOException {
-        for(int i = 0; i < m; i++){
-            bw.write(ret[i] + " ");
-        }
-        bw.write("\n");
     }
 }
