87cbbf1f7f2404b5188ba3f60fd776ee5e096877
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-05-22 14:38:27 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 68 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,42 +1,36 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main{
-
+public class Main {
     static int n;
-    static long[] s, b; // 신맛(곱), 쓴맛(합)
-    static long ret = Long.MAX_VALUE;
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] a, b;
+    static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-        s = new long[n];
-        b = new long[n];
-
-        for(int i = 0; i < n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            s[i] = Long.parseLong(st.nextToken());
-            b[i] = Long.parseLong(st.nextToken());
+        a = new int[n];
+        b = new int[n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            a[i] = Integer.parseInt(st.nextToken());
+            b[i] = Integer.parseInt(st.nextToken());
         }
-
-        for(int i = 1; i < (1 << n); i++){
-            long sour = 1;
-            long bitter = 0;
-            for(int j = 0; j < n; j++){
-                if((i & (1 << j)) != 0){ // 선택O
-                    sour *= s[j];
-                    bitter += b[j];
-                }else{ // 선택X
-                    continue;
-                }
+        int S = (1 << n);
+        for (int i = 1; i < S; i++) {
+            ArrayList<Integer> adj = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) adj.add(j);// 선택 O
+                else continue;// 선택 X
             }
-            ret = Math.min(ret, Math.abs(sour - bitter));
+            int A = 1, B = 0;
+            for (int j : adj) {
+                A *= a[j];
+                B += b[j];
+            }
+            answer = Math.min(answer, Math.abs(A - B));
         }
-
-        bw.write(ret + "\n");
-        bw.flush();
-        bw.close();
+        System.out.println(answer);
     }
 }
