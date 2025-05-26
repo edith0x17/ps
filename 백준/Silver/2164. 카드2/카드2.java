0e7069b40e13ef67129e02ac10837715ec06ba18
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-05-26 15:50:18 +0900

    [Silver IV] Title: 카드2, Time: 124 ms, Memory: 31684 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,32 +1,20 @@
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.*;
 
-public class Main{
-    static StringBuilder sb = new StringBuilder();
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
     static int n;
     static Queue<Integer> q = new ArrayDeque<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         n = Integer.parseInt(br.readLine());
-
         for(int i = 0; i < n; i++){
             q.offer(i + 1);
         }
-
-        while(q.size() > 1){
-            q.poll(); // 제일 위에 있는 카드를 바닥에 버린다
-
-            int temp = q.poll(); // 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다
+        while(q.size() != 1){
+            q.poll();
+            int temp = q.poll();
             q.offer(temp);
         }
-
-        sb.append(q.peek());
-
-        bw.write(sb + "");
-        bw.flush();
-        bw.close();
+        System.out.println(q.poll());
     }
 }
