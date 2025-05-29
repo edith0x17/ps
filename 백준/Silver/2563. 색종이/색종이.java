359642236a7198d3d898766e07b9151f1048ef57
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-05-29 13:18:03 +0900

    [Silver V] Title: 색종이, Time: 64 ms, Memory: 11636 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,38 +1,30 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main{
-    static StringBuilder sb = new StringBuilder();
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
     static int n;
-    static int[][] map = new int[104][104];
+    static int[][] a = new int[101][101];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringTokenizer st;
         n = Integer.parseInt(br.readLine());
-
-        for(int i = 0; i < n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            for(int x = a; x < a + 10; x++){
-                for(int y = b; y < b + 10; y++){
-                    map[x][y]++;
+        while (n-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            for (int i = x; i < x + 10; i++) {
+                for (int j = y; j < y + 10; j++) {
+                    a[i][j] = 1;
                 }
             }
         }
-
-        int ret = 0;
-        for(int i = 0; i < 100; i++){
-            for(int j = 0; j < 100; j++){
-                if(map[i][j] != 0)ret++;
+        int answer = 0;
+        for (int i = 0; i <= 100; i++) {
+            for (int j = 0; j <= 100; j++) {
+                if (a[i][j] == 1) answer++;
             }
         }
-        sb.append(ret);
-
-        bw.write(sb+ "");
-        bw.flush();
-        bw.close();
+        System.out.println(answer);
     }
 }
