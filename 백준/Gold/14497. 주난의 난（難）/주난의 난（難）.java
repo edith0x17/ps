3c5f6da95319462ef4b3a0ec3d27f85991395b70
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-07-16 13:44:11 +0900

    [Gold IV] Title: 주난의 난(難), Time: 160 ms, Memory: 19276 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -4,10 +4,12 @@ import java.util.*;
 public class Main {
     static final int[] dx = {-1, 0, 1, 0};
     static final int[] dy = {0, 1, 0, -1};
-    static int n, m;
+    static int n, m, turn;
     static int x1, y1, x2, y2;
     static int[][] a;
     static boolean[][] visited;
+    static Queue<Data> q = new ArrayDeque<>();
+    static Queue<Data> tempQ = new ArrayDeque<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,51 +26,48 @@ public class Main {
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
             for (int j = 0; j < m; j++) {
-                if (s.charAt(j) == '*' || s.charAt(j) == '#') a[i][j] = 0;
-                else if (s.charAt(j) == '0') a[i][j] = 0;
+                if (s.charAt(j) == '0') a[i][j] = 0;
                 else if (s.charAt(j) == '1') a[i][j] = 1;
+                else if (s.charAt(j) == '*') a[i][j] = 0; // 주난
+                else if (s.charAt(j) == '#') a[i][j] = 0; // 범인
             }
         }
-
-        Queue<Data> curQ = new ArrayDeque<>();
-        Queue<Data> nextQ = new ArrayDeque<>();
-        curQ.offer(new Data(x1, y1));
         visited[x1][y1] = true;
-        int turn = 0;
-
+        q.offer(new Data(x1, y1));
         while (true) {
-            while (!curQ.isEmpty()) {
-                Data d = curQ.poll();
+            turn++;
+            while (!q.isEmpty()) {
+                Data now = q.poll();
 
-                if (d.x == x2 && d.y == y2) {
-                    System.out.println(turn + 1);
+                if (now.x == x2 && now.y == y2) {
+                    System.out.println(turn);
                     return;
                 }
 
                 for (int i = 0; i < 4; i++) {
-                    int nx = d.x + dx[i];
-                    int ny = d.y + dy[i];
+                    int nx = now.x + dx[i];
+                    int ny = now.y + dy[i];
                     if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue; // 범위
                     if (visited[nx][ny]) continue; // 방문
-                    if (a[nx][ny] == 0) { // 장애물1
+                    if (a[nx][ny] == 1) {
+                        a[nx][ny] = 0;
                         visited[nx][ny] = true;
-                        curQ.offer(new Data(nx, ny));
-                    } else if (a[nx][ny] == 1) { // 장애물2
+                        tempQ.offer(new Data(nx, ny));
+                    } else if (a[nx][ny] == 0) {
                         visited[nx][ny] = true;
-                        nextQ.offer(new Data(nx, ny));
+                        q.offer(new Data(nx, ny));
                     }
                 }
             }
-            curQ = nextQ;
-            nextQ = new ArrayDeque<>();
-            turn++;
+            q = tempQ;
+            tempQ = new ArrayDeque<>();
         }
     }
 
     static class Data {
         int x, y;
 
-        Data(int x, int y) {
+        public Data(int x, int y) {
             this.x = x;
             this.y = y;
         }
