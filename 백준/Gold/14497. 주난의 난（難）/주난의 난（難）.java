74097e7b48f70e27cd09c3a905d21f990946ce20
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-07-17 12:19:20 +0900

    [Gold IV] Title: 주난의 난(難), Time: 152 ms, Memory: 18596 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -16,20 +16,18 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        a = new int[n][m];
+        visited = new boolean[n][m];
         st = new StringTokenizer(br.readLine());
         x1 = Integer.parseInt(st.nextToken()) - 1;
         y1 = Integer.parseInt(st.nextToken()) - 1;
         x2 = Integer.parseInt(st.nextToken()) - 1;
         y2 = Integer.parseInt(st.nextToken()) - 1;
-        a = new int[n][m];
-        visited = new boolean[n][m];
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
             for (int j = 0; j < m; j++) {
-                if (s.charAt(j) == '0') a[i][j] = 0;
-                else if (s.charAt(j) == '1') a[i][j] = 1;
-                else if (s.charAt(j) == '*') a[i][j] = 0; // 주난
-                else if (s.charAt(j) == '#') a[i][j] = 0; // 범인
+                if (s.charAt(j) == '1') a[i][j] = 1; // 친구
+                else a[i][j] = 0; // 빈 공간, 주난, 범인
             }
         }
         visited[x1][y1] = true;
@@ -47,17 +45,16 @@ public class Main {
                 for (int i = 0; i < 4; i++) {
                     int nx = now.x + dx[i];
                     int ny = now.y + dy[i];
-                    if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue; // 범위
-                    if (visited[nx][ny]) continue; // 방문
-                    if (a[nx][ny] == 1) {
+                    if (nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny]) continue; // 범위 || 방문
+                    if (a[nx][ny] == 0) { // 장애물
+                        visited[nx][ny] = true;
+                        q.offer(new Data(nx, ny));
+                    } else if (a[nx][ny] == 1) {
                         a[nx][ny] = 0;
                         visited[nx][ny] = true;
                         tempQ.offer(new Data(nx, ny));
-                    } else if (a[nx][ny] == 0) {
-                        visited[nx][ny] = true;
-                        q.offer(new Data(nx, ny));
                     }
-                }
+                } // <-
             }
             q = tempQ;
             tempQ = new ArrayDeque<>();
