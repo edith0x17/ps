b5c4dc794625ca7a06bd4193494abdb4be578f47
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-08-20 15:32:43 +0900

    [Silver III] Title: 파일 정리, Time: 592 ms, Memory: 83136 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -2,24 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
-    static Map<String, Integer> mp = new TreeMap<>();
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine().trim());
+
+        // 사전순 출력이 필요하면 TreeMap, 아니면 HashMap + 나중에 정렬
+        Map<String, Integer> mp = new TreeMap<>();
+
         for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            String[] strings = s.split("\\."); //
-            if (mp.containsKey(strings[1])) {
-                int cnt = mp.get(strings[1]);
-                mp.put(strings[1], cnt + 1);
-            } else {
-                mp.put(strings[1], 1);
-            }
+            String s = br.readLine().trim();
+            int dot = s.lastIndexOf('.');
+            String ext = s.substring(dot + 1);        // 마지막 점 뒤가 확장자
+            mp.merge(ext, 1, Integer::sum);           // 없으면 1, 있으면 +1
         }
-        for (Map.Entry<String, Integer> entry : mp.entrySet()) { //
-            System.out.println(entry.getKey() + " " + entry.getValue());
+
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, Integer> e : mp.entrySet()) {
+            sb.append(e.getKey()).append(' ').append(e.getValue()).append('\n');
         }
+        System.out.print(sb.toString());
     }
 }
