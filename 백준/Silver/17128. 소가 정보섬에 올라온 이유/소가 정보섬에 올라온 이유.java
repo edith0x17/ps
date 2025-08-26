f1fae6ee642f2fe78a47799f0752b5a83f93ae5e
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-08-26 19:02:25 +0900

    [Silver II] Title: 소가 정보섬에 올라온 이유, Time: 2676 ms, Memory: 102956 KB -BaekjoonHub

--- 백준/Silver/17128. 소가 정보섬에 올라온 이유/소가 정보섬에 올라온 이유.java
+++ 백준/Silver/17128. 소가 정보섬에 올라온 이유/소가 정보섬에 올라온 이유.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class Main {
     static int n, q;
-    static long[] a, p; // 배열, 구간배열
-    static long s;
+    static long[] a, p; //배열, 구간배열
+    static long total;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,17 +19,18 @@ public class Main {
         }
         for (int i = 0; i < n; i++) {
             p[i] = a[i] * a[(i + 1) % n] * a[(i + 2) % n] * a[(i + 3) % n]; //구간
-            s += p[i];
+            total += p[i];
         }
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < q; i++) {
             int num = Integer.parseInt(st.nextToken()) - 1;
-            for (int d = 0; d < 4; d++) {
+            for (int d = 0; d < 4; d++) { //x, x - 1, x - 2, x - 3
                 int t = wrap(num - d);
-                s -= 2 * p[t];
+
+                total -= 2 * p[t];
                 p[t] = -p[t];
             }
-            System.out.println(s);
+            System.out.println(total);
         }
     }
 
