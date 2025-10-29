3b1ae8daa67fc5c7cd563cea613bad4945d70649
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-10-29 14:56:20 +0900

    [Gold III] Title: 소문난 칠공주, Time: 4 ms, Memory: 133096 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
@@ -1,103 +1,53 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-const int dx[] = {-1, 0, 1, 0};
-const int dy[] = {0, 1, 0, -1};
-const int n = 5;
+const int dx[] = { -1, 0, 1, 0 };
+const int dy[] = { 0, 1, 0, -1 };
 
-int ret;
-int a[9][9], aTemp[9][9], visited[9][9];
-vector<pair<int, int>> vv;
+int a[5][5], visited[(1 << 25)]; // 성공한 경로...
+int ans;
+void dfs(int depth, int cnt, int path) { // path -> 경로, cnt -> 'S'개수
+    if (depth == 7) {
+        if (cnt >= 4) ans++;
+        return;
+    }
 
-int dfs(int x, int y){
-	int ret = 1;
+    for (int k = 0; k < 25; k++) {
+        if ((path & (1 << k)) == 0) continue; // 이미 포함된 경로
+        int x = k / 5;
+        int y = k % 5;
 
-	visited[x][y] = 1;
-	for(int i = 0; i < 4; i++){
-		int nx = x + dx[i];
-		int ny = y + dy[i];
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            int num = nx * 5 + ny;
+            if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
+            if (visited[path | (1 << num)]) continue; // 이미 탐색된 경로 건너뜀
 
-		if (nx < 0 || nx >= n || ny < 0 || ny >= n)continue;
-
-		if (aTemp[nx][ny] == 1 && !visited[nx][ny]){
-			ret += dfs(nx, ny);
-		}
-	}
-
-	return ret;
+            visited[path | (1 << num)] = 1; // 새로운 경로 방문 처리
+            if (a[nx][ny] == 1) dfs(depth + 1, cnt + 1, path | (1 << num));
+            else dfs(depth + 1, cnt, path | (1 << num));
+        }
+    }
 }
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-bool colorCheck(vector<int> temp){
+    for (int i = 0; i < 5; i++) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < 5; j++) {
+            if (s[j] == 'S') a[i][j] = 1;
+            else a[i][j] = 0;
+        }
+    }
 
-	int cntS = 0, cntY = 0;
-	for(int num = 0; num < temp.size(); num++){
-		int x = vv[temp[num]].first;
-		int y = vv[temp[num]].second;
-		if(a[x][y] == 1)cntS++;
-		else cntY++;
-	}
-
-	if(cntY < cntS)return true;
-	else return false;
-}
-
-bool connectCheck(vector<int> temp){
-	
-	fill(&aTemp[0][0], &aTemp[0][0] + 9 * 9, 0);
-	fill(&visited[0][0], &visited[0][0] + 9 * 9, 0);
-
-	for(int num = 0; num < temp.size(); num++){
-		int x = vv[temp[num]].first;
-		int y = vv[temp[num]].second;
-		
-		aTemp[x][y] = 1;
-	}
-
-	int x = vv[temp[0]].first;
-	int y = vv[temp[0]].second;
-
-	if(dfs(x, y) == 7){
-		return true;
-	}
-
-	return false;
-}
-
-void combi(int start, vector<int> b){
-	if(b.size() == 7){
-
-		if(colorCheck(b)){
-			if(connectCheck(b))ret++;
-		}
-		return;
-	}
-
-	for(int i = start + 1; i < vv.size(); i++){
-		b.push_back(i);
-		combi(i, b);
-		b.pop_back();
-	}
-}
-
-int main(){
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n; j++){
-
-			char ch;
-			cin >> ch;
-			if(ch == 'S')a[i][j] = 1;
-			else a[i][j] = 2;
-
-			vv.push_back({i, j});
-		}
-	}
-
-	vector<int> b;
-	combi(-1, b);
-
-	cout << ret << '\n';
-	return 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            visited[1 << (i * 5 + j)] = 1;
+            if (a[i][j] == 1) dfs(1, 1, 1 << (i * 5 + j));
+            else dfs(1, 0, 1 << (i * 5 + j));
+        }
+    }
+    cout << ans << "\n";
+    return 0;
 }
