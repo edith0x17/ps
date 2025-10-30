5e00df694fe718492fb632a3f5e96abc6050d64f
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-10-30 16:44:16 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 76 ms, Memory: 11912 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,37 +1,33 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    static StringBuilder sb = new StringBuilder();
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
     static int n;
-    static int[] a;
-    static ArrayList<Integer> d = new ArrayList<>();
-    public static void main(String[] args)throws IOException{
+    static int[] arr;
+    static ArrayList<Integer> adj = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        a = new int[n + 4];
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++){
-            a[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        sb.append(lis());
-        bw.write(sb + "");
-        bw.flush();
-        bw.close();
+        lis();
+        System.out.println(adj.size());
     }
-    static int lis(){
-        d.add(a[0]);
-        for(int i = 1; i < n; i++){
-            if(a[i] > d.get(d.size() - 1)){// d의 마지막 보다 큰 경우
-                d.add(a[i]);
-            }else{// d의 마지막 보다 작은 경우
-                int pos = Collections.binarySearch(d, a[i]);// 있으면 양수, 없으면 음수(들어갈 위치, 인덱스니깐 + 1)
-                if(pos < 0)pos = -(pos + 1);
-                d.set(pos, a[i]);
+
+    static void lis() {
+        adj.add(arr[0]);
+        for (int i = 1; i < n; i++) {
+            if (adj.get(adj.size() - 1) < arr[i]) {
+                adj.add(arr[i]);
+            } else {
+                int pos = Collections.binarySearch(adj, arr[i]);
+                if (pos < 0) pos = -(pos + 1);
+                adj.set(pos, arr[i]);
             }
         }
-        return d.size();
     }
 }
