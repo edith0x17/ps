bf8d33ec6423dc5ecf313f1ba10c10353303b922
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-10-31 21:06:03 +0900

    [level 3] Title: 가장 먼 노드, Time: 33.72 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,9 +1,11 @@
+//59 -> 
 import java.io.*;
 import java.util.*;
 
 class Solution {
     static ArrayList<Integer>[] adj;
     static int[] visited;
+    static int answer = 0;
     public int solution(int n, int[][] edge) {
         adj = new ArrayList[n + 1];
         for(int i = 0; i < n + 1; i++){
@@ -11,33 +13,31 @@ class Solution {
         }
         visited = new int[n + 1];
         for(int i = 0; i < edge.length; i++){
-            //edge[i][0] edge[i][1]
-            adj[edge[i][0]].add(edge[i][1]);
-            adj[edge[i][1]].add(edge[i][0]);
+            int f = edge[i][0];
+            int t = edge[i][1];
+            adj[f].add(t); 
+            adj[t].add(f); 
         }
-        bfs(1);
-        int mx = 0, answer = 0;
-        for(int i = 2; i <= n; i++){
-            if(mx < visited[i]){
-                mx = visited[i];
-                answer = 1;
-            }else if(mx == visited[i]){
-                answer++;
-            }
-        }
-        return answer;
-    }
-    static void bfs(int here){
         Queue<Integer> q = new ArrayDeque<>();
-        visited[here] = 1;
-        q.offer(here);
+        visited[1] = 1;
+        q.offer(1);
         while(!q.isEmpty()){
-            here = q.poll();
+            int here = q.poll();
+            
             for(int there: adj[here]){
                 if(visited[there] != 0)continue;
                 visited[there] = visited[here] + 1;
                 q.offer(there);
             }
         }
+        //System.out.println(Arrays.toString(visited));
+        int mx = Integer.MIN_VALUE;
+        for(int i = 1; i < n + 1; i++){
+            mx = Math.max(mx, visited[i]);
+        }
+        for(int i = 1; i < n + 1; i++){
+            if(mx == visited[i])answer++;
+        }
+        return answer;
     }
 }
