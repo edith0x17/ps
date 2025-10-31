b04a5e2d3657c153d5ed7f7572387120caa55110
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2025-10-31 22:04:00 +0900

    [Silver I] Title: 숨바꼭질, Time: 108 ms, Memory: 17384 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -3,25 +3,38 @@ import java.util.*;
 
 public class Main {
     static int n, k;
-    static int[] vistied = new int[100_004];
-    static Queue<Integer> q = new ArrayDeque<>();
+    static int[] visited = new int[100_004];
+    ;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        vistied[n] = 1;
-        q.offer(n);
-        while(!q.isEmpty()){
-            int here = q.poll();
-            for(int there: new int[]{here - 1, here + 1, here * 2}){
-                if(there < 0 || there > 100_000)continue; // 범위
-                if(vistied[there] != 0)continue; // 방문
-                // 장애물
-                vistied[there] = vistied[here] + 1;
+        if (n == k) {
+            System.out.println(0);
+            return;
+        }
+        bfs(n);
+        System.out.println(visited[k] - 1);
+    }
+
+    static void bfs(int here) {
+        Queue<Integer> q = new ArrayDeque<>();
+
+        visited[here] = 1;
+        q.offer(here);
+        while (!q.isEmpty()) {
+            here = q.poll();
+
+            if (here == k) return;
+
+            for (int there : new int[]{here - 1, here + 1, here * 2}) {
+                if (there < 0 || there > 100_000) continue; //범위
+                if (visited[there] != 0) continue; //방문
+                visited[there] = visited[here] + 1;
                 q.offer(there);
             }
         }
-        System.out.println(vistied[k] - 1);
     }
 }
