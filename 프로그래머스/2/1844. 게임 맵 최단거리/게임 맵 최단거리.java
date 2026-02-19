3515f55149f9d60b19bc3b41fcc9132f4dae8cb3
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-02-19 12:26:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.35 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,37 +2,35 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    static final int[] dx = {-1, 0, 1, 0};
-    static final int[] dy = {0, 1, 0, -1};
-    static int n, m, answer = -1;
-    static int[][] visited;
+    static int dx[] = {-1, 0, 1, 0};
+    static int dy[] = {0, 1, 0, -1};
+    static int n, m;
     public int solution(int[][] maps) {
         n = maps.length;
         m = maps[0].length;
-        visited = new int[n][m];
-        bfs(0, 0, maps);
-        if(visited[n - 1][m - 1] != 0)answer = visited[n - 1][m - 1];
-        return answer;
-    }
-    static void bfs(int x, int y, int[][] maps){
         Queue<Data> q = new ArrayDeque<>();
-        
-        visited[x][y] = 1;
-        q.offer(new Data(x, y));
+        int[][] visited = new int[n][m];
+        visited[0][0] = 1;
+        q.offer(new Data(0, 0));
         while(!q.isEmpty()){
             Data data = q.poll();
+            //
             for(int i = 0; i < 4; i++){
                 int nx = data.x + dx[i];
                 int ny = data.y + dy[i];
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m)continue;// 범위
-                if(maps[nx][ny] == 0 || visited[nx][ny] != 0)continue;// 장애물 || 방문
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m)continue;
+                if(visited[nx][ny] != 0)continue;
+                if(maps[nx][ny] == 0)continue;
                 visited[nx][ny] = visited[data.x][data.y] + 1;
                 q.offer(new Data(nx, ny));
             }
         }
+        if(visited[n - 1][m - 1] == 0)return - 1;
+        else return visited[n - 1][m - 1];
     }
     static class Data{
         int x, y;
+        
         public Data(int x, int y){
             this.x = x;
             this.y = y;
