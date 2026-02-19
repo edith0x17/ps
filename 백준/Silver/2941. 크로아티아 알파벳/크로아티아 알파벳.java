748a67b9fd5e193d4e8475c1fb172ef9770a3467
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-02-19 17:20:10 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 64 ms, Memory: 11648 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,28 +1,16 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+    static String[] cro = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
     static String s;
-    static String[] targets = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
-    static int answer;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         s = br.readLine();
-        for(String target: targets){
-            int index = 0;
-            while((index = s.indexOf(target, index)) != -1){
-                answer++;
-                s = s.substring(0, index) + "*" +s.substring(index + target.length());
-            }
+        for (String c : cro) {
+            s = s.replace(c, "*");
         }
-        for(char ch: s.toCharArray()){
-            if(ch != '*')answer++;
-        }
-        sb.append(answer);
-        bw.write(sb + "");
-        bw.flush();
-        bw.close();
+        System.out.println(s.length());
     }
 }
