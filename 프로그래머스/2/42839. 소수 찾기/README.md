bf511b52dbc1e7737ed081a7b83d7d8ab2133bc1
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-02-19 11:37:15 +0900

    [level 2] Title: 소수 찾기, Time: 4.29 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 72.7 MB, 시간: 3.57 ms
+메모리: 71.8 MB, 시간: 4.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 10일 23:10:44
+2026년 02월 19일 11:37:12
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,49 +2,46 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    static int[] arr, ret;
+    static int[] a, ret;
     static boolean[] visited;
     static Set<Integer> set = new HashSet<>();
     public int solution(String numbers) {
-        arr = new int[numbers.length()];
-        ret = new int[numbers.length()];
-        
+        a = new int[numbers.length()];
         for(int i = 0; i < numbers.length(); i++){
-            arr[i] = numbers.charAt(i) - '0';
+            a[i] = numbers.charAt(i) - '0';
         }
-        Arrays.sort(arr);
+        ret = new int[numbers.length()];
+        visited = new boolean[numbers.length()];
         
-        // logic1
         for(int i = 1; i <= numbers.length(); i++){
-            visited = new boolean[numbers.length()];
             go(0, i);
         }
         return set.size();
     }
-    static void go(int depth, int k){
-        if(depth == k){
-            // logic2
-            int temp = 0;
-            for(int i = 0; i < k; i++){
-                temp = temp * 10 + ret[i];
-            }
-            if(temp == 0 || temp == 1)return;// 0, 1 제외 
-            if(check(temp))set.add(temp);
-            return;
-        }
-        for(int i = 0; i < arr.length; i++){
-            if(visited[i])continue;
-            visited[i] = true;
-            ret[depth] = arr[i];
-            go(depth + 1, k);
-            visited[i] = false;
-        }
-    }
-    static boolean check(int temp){
-        for(int i = 2; i * i <= temp; i++){
-            if(temp % i == 0)return false;
-            else continue;
+    static boolean check(int x){
+        if (x < 2) return false;
+        for (int i = 2; i * i <= x; i++){
+            if(x % i == 0) return false;
         }
         return true;
+    }
+    static void go(int depth, int s){
+        if(depth == s){
+            int x = 0;
+            for (int i = 0; i < s; i++) {
+                x = x * 10 + ret[i];
+            }
+            if(check(x)){
+                set.add(x);
+            }
+            return ;
+        }
+        for(int i = 0; i < a.length; i++){
+            if(visited[i])continue;
+            ret[depth] = a[i];
+            visited[i] = true;
+            go(depth + 1, s);
+            visited[i] = false;
+        }
     }
 }
