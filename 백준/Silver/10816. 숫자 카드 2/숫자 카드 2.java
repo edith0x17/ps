5e0840cf46264f11ac7928e42eba8a43828af9f1
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-02-23 11:15:03 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 968 ms, Memory: 234000 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,45 +1,27 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main{
+public class Main {
     static int n, m;
-    static StringBuilder sb = new StringBuilder();
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    public static void main(String[] args) throws IOException{
-        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+    static Map<Integer, Integer> mp = new HashMap<>();
 
-        n = Integer.parseInt(br.readLine());
-        HashMap<Integer, Integer> mp = new HashMap<Integer, Integer>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if(mp.containsKey(num)){
-                Integer cnt = mp.get(num);
-                mp.put(num, cnt + 1);
-            }else{
-                mp.put(num, 1);
-            }
+            mp.put(num, mp.getOrDefault(num, 0) + 1);
         }
-
-        m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < m; i++){
-            int num = Integer.parseInt(st.nextToken());
-            if(mp.containsKey(num)){
-                int cnt = mp.get(num);
-                sb.append(cnt + " ");
-            }else{
-                sb.append(0 + " ");
-            }
+        m = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            sb.append(mp.getOrDefault(tmp, 0)).append(" ");
         }
-        bw.write(sb + "");
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
