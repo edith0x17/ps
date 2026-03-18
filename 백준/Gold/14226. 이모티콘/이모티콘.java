2bc51dd82e82511f2b2246443f4bd7afbc3098a3
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-03-18 11:50:09 +0900

    [Gold IV] Title: 이모티콘, Time: 116 ms, Memory: 18636 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.java
+++ 백준/Gold/14226. 이모티콘/이모티콘.java
@@ -3,34 +3,38 @@ import java.util.*;
 
 public class Main {
     static int s;
-    static boolean[][] visited = new boolean[1004][1004];//[screen][clip]
+    static Queue<int[]> q = new ArrayDeque<>();
+    static int[][] visited = new int[1004][1004];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         s = Integer.parseInt(br.readLine());
-        Queue<int[]> q = new ArrayDeque<>();
-        visited[1][0] = true;
-        q.offer(new int[]{1, 0, 0});
+        visited[1][0] = 1;
+        q.offer(new int[]{1, 0, 0});//board, clipboard, time
         while (!q.isEmpty()) {
-            int[] tmp = q.poll();
-            int screen = tmp[0];
-            int clip = tmp[1];
-            int time = tmp[2];
-            if (screen == s) {
+            int[] cur = q.poll();
+            int board = cur[0];
+            int clipboard = cur[1];
+            int time = cur[2];
+
+            if (cur[0] == s) {
                 System.out.println(time);
-                return;
+                break;
             }
-            if (!visited[screen][screen]) {//복사
-                visited[screen][screen] = true;
-                q.offer(new int[]{screen, screen, time + 1});
+
+            if (visited[board][board] == 0) {//1
+                visited[board][board] = 1;
+                q.offer(new int[]{board, board, time + 1});
             }
-            if (0 < clip && screen + clip <= 1000 && !visited[screen + clip][clip]) {//붙여넣기
-                visited[screen + clip][clip] = true;
-                q.offer(new int[]{screen + clip, clip, time + 1});
+
+            if (clipboard > 0 && board + clipboard <= 1000 && visited[board + clipboard][clipboard] == 0) {//2
+                visited[board + clipboard][clipboard] = 1;
+                q.offer(new int[]{board + clipboard, clipboard, time + 1});
             }
-            if (0 < screen && !visited[screen - 1][clip]) {//삭제
-                visited[screen - 1][clip] = true;
-                q.offer(new int[]{screen - 1, clip, time + 1});
+
+            if (board > 0 && visited[board - 1][clipboard] == 0) {
+                visited[board - 1][clipboard] = 1;
+                q.offer(new int[]{board - 1, clipboard, time + 1});
             }
         }
     }
