39bc447968c411716083e49dda702ae840583318
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-03-19 15:47:28 +0900

    [Gold III] Title: 감시, Time: 300 ms, Memory: 88268 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,122 +1,103 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    public static class Data {
-        int x;
-        int y;
-        int type;
-
-        public Data(int x, int y, int type) {
-            this.x = x;
-            this.y = y;
-            this.type = type;
-        }
-    }
-
-    static int dx[] = {-1, 0, 1, 0};
-    static int dy[] = {0, 1, 0, -1};
-
-    // possible combination
-    static int[][][] mode = {
-            {{0}},
-            {{0}, {1}, {2}, {3}}, // 1번 CCTV
-            {{0, 2}, {1, 3}}, // 2번 CCTV
-            {{0, 1}, {1, 2}, {2, 3}, {3, 0}}, // 3번 CCTV
-            {{3, 0, 1}, {0, 1, 2}, {1, 2, 3}, {2, 3, 0}}, // 4번 CCTV
-            {{0, 1, 2, 3}} // 5번 CCTV
-    };
-
-    static int n, m, ans = 987654321;
-    static ArrayList<Data> cctv = new ArrayList<>();
-    public static int[][] mapOri;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int n, m;
+    static int[][] map;
+    static ArrayList<int[]> cctvList = new ArrayList<>();
+    static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        mapOri = new int[n][m];
-
-        // input
+        map = new int[n][m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-
             for (int j = 0; j < m; j++) {
-                mapOri[i][j] = Integer.parseInt(st.nextToken());
-
-                if (1 <= mapOri[i][j] && mapOri[i][j] <= 5) {
-                    cctv.add(new Data(i, j, mapOri[i][j]));
-                }
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (1 <= map[i][j] && map[i][j] <= 5) cctvList.add(new int[]{i, j});
             }
         }
-
-        dfs(0, mapOri);
-
-        sb.append(ans);
-
-        bw.write(ans + "");
-        bw.flush();
-        bw.close();
+        dfs(0, map);
+        System.out.println(answer);
     }
 
-    static void dfs(int depth, int[][] currentMap) {
-        if (depth == cctv.size()) {
-            int cnt = check(currentMap);
-            ans = Math.min(ans, cnt);
-            return;
-        }
-
-        int x = cctv.get(depth).x;
-        int y = cctv.get(depth).y;
-        int cctvType = cctv.get(depth).type;
-
-        // 
-        for (int i = 0; i < mode[cctvType].length; i++) {
-
-            // 원상복구
-            int[][] mapTemp = new int[n][m];
-            for (int k = 0; k < n; k++) {
-                mapTemp[k] = currentMap[k].clone();
-            }
-
-            // direction
-            for (int j = 0; j < mode[cctvType][i].length; j++) {
-                int dir = mode[cctvType][i][j];
-
-                int nx = x + dx[dir];
-                int ny = y + dy[dir];
-
-                while (true) {
-                    if (nx < 0 || nx >= n || ny < 0 || ny >= m || currentMap[nx][ny] == 6) break;
-
-                    mapTemp[nx][ny] = -1;
-                    nx += dx[dir];
-                    ny += dy[dir];
-                }
-            }
-
-            dfs(depth + 1, mapTemp);
-        }
-    }
-
-    // blind spot
-    static int check(int[][] currentMap) {
+    static void check(int[][] map) {
         int cnt = 0;
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (currentMap[i][j] == 0) {
-                    cnt++;
-                }
+                if (map[i][j] == 0) cnt++;
             }
         }
-        return cnt;
+        answer = Math.min(answer, cnt);
+    }
+
+    static void dfs(int depth, int[][] map) {
+        if (depth == cctvList.size()) {
+            check(map);
+            return;
+        }
+        int[] cur = cctvList.get(depth);
+        int x = cur[0];
+        int y = cur[1];
+        int type = map[x][y];
+        if (type == 1) {
+            for (int d = 0; d < 4; d++) {
+                int[][] copy = copy(map);
+                watch(copy, x, y, d);
+                dfs(depth + 1, copy);
+            }
+        } else if (type == 2) {
+            for (int d = 0; d < 2; d++) {
+                int[][] copy = copy(map);
+                watch(copy, x, y, d);
+                watch(copy, x, y, d + 2);
+                dfs(depth + 1, copy);
+            }
+        } else if (type == 3) {
+            for (int d = 0; d < 4; d++) {
+                int[][] copy = copy(map);
+                watch(copy, x, y, d);
+                watch(copy, x, y, (d + 1) % 4);
+                dfs(depth + 1, copy);
+            }
+        } else if (type == 4) {
+            for (int d = 0; d < 4; d++) {
+                int[][] copy = copy(map);
+                for (int k = 0; k < 3; k++) {
+                    watch(copy, x, y, (d + k) % 4);
+                }
+                dfs(depth + 1, copy);
+            }
+        } else if (type == 5) {
+            int[][] copy = copy(map);
+            for (int d = 0; d < 4; d++) {
+                watch(copy, x, y, d);
+            }
+            dfs(depth + 1, copy);
+        }
+    }
+
+    static void watch(int[][] map, int x, int y, int d) {
+        while (true) {
+            x += dx[d];
+            y += dy[d];
+
+            if (x < 0 || x >= n || y < 0 || y >= m) break;//범위
+            if (map[x][y] == 6) break;//장애물
+            if (map[x][y] == 0) map[x][y] = -1;
+        }
+    }
+
+    static int[][] copy(int[][] map) {
+        int[][] tmp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            tmp[i] = Arrays.copyOf(map[i], m);
+        }
+        return tmp;
     }
 }
