a7ec166dd2790efc5362c789d97d8c0e1e288a8e
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-03-20 15:27:44 +0900

    [Gold III] Title: 감시, Time: 308 ms, Memory: 91992 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -5,7 +5,7 @@ public class Main {
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     static int n, m;
-    static int[][] map;
+    static int[][] a;
     static ArrayList<int[]> cctvList = new ArrayList<>();
     static int answer = Integer.MAX_VALUE;
 
@@ -14,83 +14,26 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        map = new int[n][m];
+        a = new int[n][m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (1 <= map[i][j] && map[i][j] <= 5) cctvList.add(new int[]{i, j});
+                a[i][j] = Integer.parseInt(st.nextToken());
+                if (0 < a[i][j] && a[i][j] < 6) cctvList.add(new int[]{i, j, a[i][j]});//x, y, type
             }
         }
-        dfs(0, map);
+        go(0, a);
         System.out.println(answer);
     }
 
-    static void check(int[][] map) {
+    static void check(int[][] tmp) {
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (map[i][j] == 0) cnt++;
+                if (tmp[i][j] == 0) cnt++;
             }
         }
         answer = Math.min(answer, cnt);
-    }
-
-    static void dfs(int depth, int[][] map) {
-        if (depth == cctvList.size()) {
-            check(map);
-            return;
-        }
-        int[] cur = cctvList.get(depth);
-        int x = cur[0];
-        int y = cur[1];
-        int type = map[x][y];
-        if (type == 1) {
-            for (int d = 0; d < 4; d++) {
-                int[][] copy = copy(map);
-                watch(copy, x, y, d);
-                dfs(depth + 1, copy);
-            }
-        } else if (type == 2) {
-            for (int d = 0; d < 2; d++) {
-                int[][] copy = copy(map);
-                watch(copy, x, y, d);
-                watch(copy, x, y, d + 2);
-                dfs(depth + 1, copy);
-            }
-        } else if (type == 3) {
-            for (int d = 0; d < 4; d++) {
-                int[][] copy = copy(map);
-                watch(copy, x, y, d);
-                watch(copy, x, y, (d + 1) % 4);
-                dfs(depth + 1, copy);
-            }
-        } else if (type == 4) {
-            for (int d = 0; d < 4; d++) {
-                int[][] copy = copy(map);
-                for (int k = 0; k < 3; k++) {
-                    watch(copy, x, y, (d + k) % 4);
-                }
-                dfs(depth + 1, copy);
-            }
-        } else if (type == 5) {
-            int[][] copy = copy(map);
-            for (int d = 0; d < 4; d++) {
-                watch(copy, x, y, d);
-            }
-            dfs(depth + 1, copy);
-        }
-    }
-
-    static void watch(int[][] map, int x, int y, int d) {
-        while (true) {
-            x += dx[d];
-            y += dy[d];
-
-            if (x < 0 || x >= n || y < 0 || y >= m) break;//범위
-            if (map[x][y] == 6) break;//장애물
-            if (map[x][y] == 0) map[x][y] = -1;
-        }
     }
 
     static int[][] copy(int[][] map) {
@@ -99,5 +42,59 @@ public class Main {
             tmp[i] = Arrays.copyOf(map[i], m);
         }
         return tmp;
+    }
+
+    static void watch(int x, int y, int d, int[][] tmp) {
+        while (true) {
+            x += dx[d];
+            y += dy[d];
+            if (x < 0 || x >= n || y < 0 || y >= m) break;
+            if (tmp[x][y] == 6) break;
+            if (tmp[x][y] == 0) tmp[x][y] = 999;
+        }
+    }
+
+    static void go(int depth, int[][] map) {
+        if (depth == cctvList.size()) {
+            check(map);
+            return;
+        }
+        int[] cur = cctvList.get(depth);
+        int x = cur[0], y = cur[1], type = cur[2];
+        if (type == 1) {
+            for (int i = 0; i < 4; i++) {
+                int[][] tmp = copy(map);
+                watch(x, y, i, tmp);
+                go(depth + 1, tmp);
+            }
+        } else if (type == 2) {
+            for (int i = 0; i < 4; i++) {
+                int[][] tmp = copy(map);
+                watch(x, y, i, tmp);
+                watch(x, y, (i + 2) % 4, tmp);
+                go(depth + 1, tmp);
+            }
+        } else if (type == 3) {
+            for (int i = 0; i < 4; i++) {
+                int[][] tmp = copy(map);
+                watch(x, y, i, tmp);
+                watch(x, y, (i + 1) % 4, tmp);
+                go(depth + 1, tmp);
+            }
+        } else if (type == 4) {
+            for (int i = 0; i < 4; i++) {
+                int[][] tmp = copy(map);
+                watch(x, y, i, tmp);
+                watch(x, y, (i + 1) % 4, tmp);
+                watch(x, y, (i + 2) % 4, tmp);
+                go(depth + 1, tmp);
+            }
+        } else if (type == 5) {
+            int[][] tmp = copy(map);
+            for (int i = 0; i < 4; i++) {
+                watch(x, y, i, tmp);
+            }
+            go(depth + 1, tmp);
+        }
     }
 }
