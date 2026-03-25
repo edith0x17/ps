c93ae819339e0cd9f189c6a84c0e32dc6ebf2b65
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-03-25 10:45:49 +0900

    [Gold III] Title: 사다리 조작, Time: 428 ms, Memory: 17628 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.java
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
     static int n, m, h;
     static boolean[][] a;
-    static int answer = Integer.MAX_VALUE;
+    static int ans = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,47 +12,44 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         h = Integer.parseInt(st.nextToken());
-        a = new boolean[h + 1][n + 1];//
+        a = new boolean[h + 1][n + 1];
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            a[x][y] = true;
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            a[r][c] = true;
         }
-
         for (int i = 0; i <= 3; i++) {
             go(0, i, 1);
-            if (answer != Integer.MAX_VALUE) break;
+            if (ans != Integer.MAX_VALUE) break;
         }
-        if (answer != Integer.MAX_VALUE) System.out.println(answer);
+        if (ans != Integer.MAX_VALUE) System.out.println(ans);
         else System.out.println(-1);
     }
 
-    static boolean check() {//사다리
+    static boolean check() {
         for (int i = 1; i <= n; i++) {
             int col = i;
             for (int row = 1; row <= h; row++) {
                 if (a[row][col]) col++;
-                else if (col > 1 && a[row][col - 1]) col--;
+                else if (a[row][col - 1]) col--;
             }
             if (col != i) return false;
         }
         return true;
     }
 
-    static void go(int cnt, int maxCnt, int start) {
-        if (cnt == maxCnt) {
-            if (check()) answer = Math.min(answer, cnt);
+    static void go(int cnt, int mxCnt, int start) {
+        if (cnt == mxCnt) {
+            if (check()) ans = Math.min(ans, cnt);
             return;
         }
-
-        if (cnt >= answer) return;//가지치기
-
+        if (cnt >= ans) return;
         for (int i = start; i <= h; i++) {
             for (int j = 1; j < n; j++) {
-                if (a[i][j] || a[i][j - 1] || a[i][j + 1]) continue;//지금 || 지금의 왼쪽 || 지금의 오른쪽
+                if (a[i][j] || a[i][j - 1] || a[i][j + 1]) continue;
                 a[i][j] = true;
-                go(cnt + 1, maxCnt, i);
+                go(cnt + 1, mxCnt, i);
                 a[i][j] = false;
             }
         }
