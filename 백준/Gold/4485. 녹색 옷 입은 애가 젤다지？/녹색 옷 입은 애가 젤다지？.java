663a7f81cc2db2d40485f29677ab1f85ec4dcda8
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-04-04 20:57:26 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 200 ms, Memory: 19400 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,100 +1,76 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static class Data implements Comparable<Data> {
-        int x;
-        int y;
-        int c;
-
-        public Data(int x, int y, int c) {
-            this.x = x;
-            this.y = y;
-            this.c = c;
-        }
-
-        @Override
-        public int compareTo(Data o) {
-            return Integer.compare(this.c, o.c);
-        }
-    }
-
-    static int INF = Integer.MAX_VALUE;
-    static int dx[] = {-1, 0, 1, 0};
-    static int dy[] = {0, 1, 0, -1};
     static int n;
-    static int[][] a, d; // d-> 비용
-    static boolean[][] visited;
-    static int cnt = 0;
-    public static void main(String[] args) throws IOException {
+    static int[][] a, dist;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static PriorityQueue<Data> pq = new PriorityQueue<>();
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int tc = 1;
         while (true) {
-            cnt++;
             n = Integer.parseInt(br.readLine());
             if (n == 0) break;
-
-            PriorityQueue<Data> pq = new PriorityQueue<>();
-            a = new int[n + 4][n + 4];
-            d = new int[n + 4][n + 4];
-            visited = new boolean[n + 4][n + 4];
-
+            a = new int[n][n];
+            dist = new int[n][n];
             for (int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < n; j++) {
                     a[i][j] = Integer.parseInt(st.nextToken());
+                    //1
+                    dist[i][j] = Integer.MAX_VALUE;
                 }
             }
 
-//            for (int i = 0; i < n; i++) {
-//                for (int j = 0; j < n; j++) {
-//                    System.out.printf("%d ", a[i][j]);
-//                }
-//                System.out.println();
-//            }
+            go();
+            sb.append("Problem ").append(tc++).append(": ").append(dist[n - 1][n - 1]).append('\n');
+        }
+        System.out.print(sb);
+    }
 
-            for (int i = 0; i < n + 4; i++) {
+    static void go() {
+        pq.clear();
 
-                for (int j = 0; j < n + 4; j++) {
-                    d[i][j] = INF;
+        //2
+        dist[0][0] = a[0][0];
+        pq.offer(new Data(0, 0, dist[0][0]));
+
+        //3
+        while (!pq.isEmpty()) {
+            Data tmp = pq.poll();
+            int x = tmp.x, y = tmp.y, weight = tmp.w;
+
+            if (dist[x][y] < weight) continue;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;//범위
+                int newDist = dist[x][y] + a[nx][ny];
+                if (dist[nx][ny] > newDist) {
+                    dist[nx][ny] = newDist;
+                    pq.offer(new Data(nx, ny, newDist));
                 }
             }
+        }
+    }
 
-            d[0][0] = a[0][0];
-            pq.offer(new Data(0, 0, d[0][0]));
+    static class Data implements Comparable<Data> {
+        int x, y, w;
 
-            int cntV = 0;
-            while (!pq.isEmpty()) {
-                Data now = pq.poll();
+        public Data(int x, int y, int w) {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+        }
 
-                if(visited[now.x][now.y]) {
-                    continue;
-                }
-                visited[now.x][now.y] = true;
-
-                cntV++;
-                if(cntV == n * n){
-                    break;
-                }
-
-                for (int i = 0; i < 4; i++) {
-                    int nx = now.x + dx[i];
-                    int ny = now.y + dy[i];
-
-                    if (nx < 0 || nx >= n || ny < 0 || ny >= n || visited[nx][ny]) continue;
-
-                    if(d[nx][ny] > d[now.x][now.y] + a[nx][ny]){ // direct || bypass
-                        d[nx][ny] = d[now.x][now.y] + a[nx][ny];
-                        pq.offer(new Data(nx, ny, d[nx][ny])); // push - nx, ny, d[nx][ny]
-                    }
-                }
-            }
-            System.out.println("Problem" + " " + cnt + ": " + d[n - 1][n - 1]);
-
+        @Override
+        public int compareTo(Data o) {
+            return Integer.compare(this.w, o.w);
         }
     }
 }
