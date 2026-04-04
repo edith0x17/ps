97a10bfa1a091436523d4125fe78938f34e16c36
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-04-04 15:34:01 +0900

    [Gold V] Title: 최소비용 구하기, Time: 440 ms, Memory: 50412 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -4,80 +4,61 @@ import java.util.*;
 public class Main {
     static int n, m, s, e;
     static ArrayList<Data>[] adj;
-    static int[] d, prev; // prev 배열 추가
+    static int[] dist;
     static PriorityQueue<Data> pq = new PriorityQueue<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+        StringTokenizer st;
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
         adj = new ArrayList[n + 1];
-        for(int i = 0; i < n + 1; i++){
+        for (int i = 0; i < n + 1; i++) {
             adj[i] = new ArrayList<>();
         }
-        d = new int[n + 1];
-        Arrays.fill(d, Integer.MAX_VALUE);
-
-        prev = new int[n + 1]; // 경로 저장을 위한 prev 배열
-
-        for(int i = 0; i < m; i++){
+        dist = new int[n + 1];
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int f = Integer.parseInt(st.nextToken());
             int t = Integer.parseInt(st.nextToken());
-            int distance = Integer.parseInt(st.nextToken());
-            adj[f].add(new Data(t, distance));
+            int w = Integer.parseInt(st.nextToken());
+            adj[f].add(new Data(t, w));
         }
         st = new StringTokenizer(br.readLine());
         s = Integer.parseInt(st.nextToken());
         e = Integer.parseInt(st.nextToken());
-        dijkstra(s);
-        System.out.println(d[e]); // 최소 비용 출력
-        // printPath(e); // 경로 출력
-    }
-    static void dijkstra(int start) {
-        d[start] = 0;
-        pq.offer(new Data(start, 0));
+
+        for (int i = 0; i < n + 1; i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
+        dist[s] = 0;
+        pq.offer(new Data(s, 0));
         while (!pq.isEmpty()) {
-            Data data = pq.poll();
-            int here = data.to;
-
-            if (d[here] < data.distance) continue;
-
-            for (Data there : adj[here]) {
-                if (d[there.to] > d[here] + there.distance) {
-                    d[there.to] = d[here] + there.distance;
-
-                    prev[there.to] = here; // 부모 노드 저장
-
-                    pq.offer(new Data(there.to, d[there.to]));
+            Data tmp = pq.poll();
+            int now = tmp.t, nowWeight = tmp.w;
+            if (dist[now] < nowWeight) continue;
+            for (Data data : adj[now]) {
+                int next = data.t, nextWeight = data.w;
+                if (dist[next] > dist[now] + nextWeight) {
+                    dist[next] = dist[now] + nextWeight;
+                    pq.offer(new Data(next, dist[now] + nextWeight));
                 }
             }
         }
-    }
-    static void printPath(int end) {
-        Stack<Integer> path = new Stack<>();
-        int node = end;
-        while (node != 0) { // 시작 노드까지 추적
-            path.push(node);
-            node = prev[node];
-        }
-
-        System.out.println(path.size()); // 경로 개수 출력
-        while (!path.isEmpty()) {
-            System.out.print(path.pop() + " "); // 경로 출력
-        }
+        System.out.println(dist[e]);
     }
 
     static class Data implements Comparable<Data> {
-        int to, distance;
-        public Data(int to, int distance) {
-            this.to = to;
-            this.distance = distance;
+        int t, w;
+
+        public Data(int t, int w) {
+            this.t = t;
+            this.w = w;
         }
+
         @Override
         public int compareTo(Data o) {
-            return this.distance - o.distance;
+            return Integer.compare(this.w, o.w);//오름차순
         }
     }
 }
