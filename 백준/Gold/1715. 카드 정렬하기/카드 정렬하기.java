c00b0b28a41418736b9e270cd04dc64f6cbe0312
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-04-07 16:48:16 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 312 ms, Memory: 27692 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -3,26 +3,36 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    static PriorityQueue<Data> pq = new PriorityQueue<Data>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
-            pq.offer(Integer.parseInt(br.readLine()));
+            int x = Integer.parseInt(br.readLine());
+            pq.offer(new Data(x));
         }
-        if (n == 1) System.out.println(0);
-        else {
-            int answer = 0;
-            while (!pq.isEmpty()) {
-                int a = pq.poll();
-                int b = pq.poll();
-                answer += (a + b);
+        int ans = 0;
+        while (pq.size() != 1) {
+            Data d1 = pq.poll();
+            Data d2 = pq.poll();
+            int tmp = d1.x + d2.x;
+            pq.offer(new Data(tmp));
+            ans += tmp;
+        }
+        System.out.println(ans);
+    }
 
-                if (pq.isEmpty()) break;
-                else pq.offer(a + b);
-            }
-            System.out.println(answer);
+    static class Data implements Comparable<Data> {
+        int x;
+
+        public Data(int x) {
+            this.x = x;
+        }
+
+        @Override
+        public int compareTo(Data o) {
+            return Integer.compare(this.x, o.x);
         }
     }
 }
