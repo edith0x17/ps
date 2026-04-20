f08fed1c8fdff6cefc336e6cb8f538ce2e08b232
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-04-20 13:52:26 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 90.38 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,71 +1,53 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
-    static final int[] dx = {-1, 0, 1, 0};
-    static final int[] dy = {0, 1, 0, -1};
-    static int n, m;
-    static int[][] a;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static boolean[][] visited;
-    static int[] colOil;
 
-    static class Data {
-        int x, y;
-
-        public Data(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    public int solution(int[][] land) {
-        n = land.length;
-        m = land[0].length;
-        a = land;
+    public int solution(int[][] land) {//0이면 빈 땅을, 1이면 석유
+        int answer = 0, n = land.length, m = land[0].length;
         visited = new boolean[n][m];
-        colOil = new int[m];
-
+        int[] colOil = new int[m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (a[i][j] == 1 && !visited[i][j]) {
+                if (land[i][j] == 1 && !visited[i][j]) {
                     Set<Integer> set = new HashSet<>();
-                    int size = bfs(new Data(i, j), set);
+                    int size = bfs(i, j, set, land, n, m);
                     for (int col : set) {
                         colOil[col] += size;
                     }
                 }
             }
         }
-
-        int answer = 0;
         for (int val : colOil) {
             answer = Math.max(answer, val);
         }
         return answer;
     }
 
-    static int bfs(Data start, Set<Integer> set) {
-        Queue<Data> q = new LinkedList<>();
-        q.offer(start);
-        visited[start.x][start.y] = true;
-        set.add(start.y);
+    static int bfs(int x, int y, Set<Integer> set, int[][] land, int n, int m) {
+        Queue<int[]> q = new ArrayDeque<>();
+
+        set.add(y);
         int ret = 1;
-
+        visited[x][y] = true;
+        q.offer(new int[]{x, y});
         while (!q.isEmpty()) {
-            Data cur = q.poll();
-            for (int d = 0; d < 4; d++) {
-                int nx = cur.x + dx[d];
-                int ny = cur.y + dy[d];
-
+            int[] cur = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
                 if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                if (a[nx][ny] == 0 || visited[nx][ny]) continue;
-
-                visited[nx][ny] = true;
+                if (visited[nx][ny]) continue;
+                if (land[nx][ny] == 0) continue;
                 set.add(ny);
-                q.offer(new Data(nx, ny));
                 ret++;
+                visited[nx][ny] = true;
+                q.offer(new int[]{nx, ny});
             }
         }
-
         return ret;
     }
 }
