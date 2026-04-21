f171422c840a34415448735def620aa087ba2c49
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-04-21 13:11:07 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 69.23 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,49 +1,53 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
+    static final int[] dx = {-1, 0, 1, 0};
+    static final int[] dy = {0, 1, 0, -1};
+    
+    static int n, m;
     static boolean[][] visited;
-
-    public int solution(int[][] land) {//0이면 빈 땅을, 1이면 석유
-        int answer = 0, n = land.length, m = land[0].length;
+    static int[] colOil;
+    
+    public int solution(int[][] land) {
+        int answer = 0;
+        n = land.length; m = land[0].length;
         visited = new boolean[n][m];
-        int[] colOil = new int[m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (land[i][j] == 1 && !visited[i][j]) {
+        colOil = new int[m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(land[i][j] == 1 && !visited[i][j]){
                     Set<Integer> set = new HashSet<>();
-                    int size = bfs(i, j, set, land, n, m);
-                    for (int col : set) {
-                        colOil[col] += size;
+                    int cnt = go(i, j, land, set);
+                    //System.out.println("cnt = " + cnt);
+                    for(int tmp: set){
+                        colOil[tmp] += cnt;
                     }
                 }
             }
         }
-        for (int val : colOil) {
-            answer = Math.max(answer, val);
+        for(int i: colOil){
+            answer = Math.max(answer, i);
         }
         return answer;
     }
-
-    static int bfs(int x, int y, Set<Integer> set, int[][] land, int n, int m) {
+    
+    static int go(int x, int y, int[][] land, Set<Integer> set){
         Queue<int[]> q = new ArrayDeque<>();
-
-        set.add(y);
         int ret = 1;
+        set.add(y);
         visited[x][y] = true;
         q.offer(new int[]{x, y});
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             int[] cur = q.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                if (visited[nx][ny]) continue;
-                if (land[nx][ny] == 0) continue;
-                set.add(ny);
+            x = cur[0]; y = cur[1];
+            for(int i = 0; i < 4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m)continue;
+                if(visited[nx][ny])continue;
+                if(land[nx][ny] == 0)continue;
                 ret++;
+                set.add(ny);
                 visited[nx][ny] = true;
                 q.offer(new int[]{nx, ny});
             }
