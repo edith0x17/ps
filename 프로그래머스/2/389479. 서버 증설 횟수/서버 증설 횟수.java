ba787ecdc6d7bdc172bab9e7675655dcfa5a5c09
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-04-23 17:03:24 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.06 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -4,14 +4,13 @@ class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
         int[] increase = new int[24];//i시에 증설한 서버 수
-        //현재 운영중인 서버 수 = increase[i - k + 1] + ... + increase[i] 합계
-        for(int i = 0; i < players.length; i++){
+        for (int i = 0; i < players.length; i++) {
             int cur = 0;
-            for(int j = Math.max(0, i - k + 1); j <= i; j++){
+            for (int j = Math.max(0, i - k + 1); j <= i; j++) {
                 cur += increase[j];
             }
             int need = cur - (players[i] / m);
-            if(need < 0){
+            if (need < 0) {
                 increase[i] = Math.abs(need);
                 answer += Math.abs(need);
             }
