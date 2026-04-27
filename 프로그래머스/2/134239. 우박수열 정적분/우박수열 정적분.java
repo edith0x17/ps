d01a35a33efd26c625ebe8a52e34aee698c9dd29
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-04-27 20:55:41 +0900

    [level 2] Title: 우박수열 정적분, Time: 1.07 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -4,36 +4,46 @@ class Solution {
     public double[] solution(int k, int[][] ranges) {
         int[] ret = go(k);
         int n = ret.length - 1;
+
         double[] prefix = new double[ret.length];
-        //끝기준 [0~1], [1~2], [2~3], [3~4], [4~5] 
+
         for (int i = 1; i < ret.length; i++) {
             double area = (ret[i - 1] + ret[i]) / 2.0;
-            prefix[i] = prefix[i - 1] + area;//prev + cur
+            prefix[i] = prefix[i - 1] + area;
         }
 
         double[] answer = new double[ranges.length];
+
         for (int i = 0; i < ranges.length; i++) {
-            int start = ranges[i][0], end = n + ranges[i][1];
-            
-            if (start > end) answer[i] = -1.0;
-            else answer[i] = prefix[end] - prefix[start];
+            int start = ranges[i][0];
+            int end = n + ranges[i][1];
+
+            if (start > end) {
+                answer[i] = -1.0;
+            } else {
+                answer[i] = prefix[end] - prefix[start];
+            }
         }
+
         return answer;
     }
 
     static int[] go(int k) {
         ArrayList<Integer> list = new ArrayList<>();
+
         while (k != 1) {
             list.add(k);
             if (k % 2 == 0) k /= 2;
             else k = k * 3 + 1;
         }
+
         list.add(1);
 
         int[] ret = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             ret[i] = list.get(i);
         }
+
         return ret;
     }
 }
