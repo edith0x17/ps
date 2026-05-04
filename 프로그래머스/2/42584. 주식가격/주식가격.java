55ae3634270e3572e4a53c9d97066bb72d200d43
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-05-04 22:25:36 +0900

    [level 2] Title: 주식가격, Time: 36.83 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,17 +3,18 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Integer> stk = new Stack<>();
+        Stack<Integer> stk = new Stack<>();//idx
         for (int i = 0; i < prices.length; i++) {
             while (!stk.isEmpty() && prices[stk.peek()] > prices[i]) {
                 int idx = stk.pop();
                 answer[idx] = i - idx;
             }
+
             stk.push(i);
         }
         while (!stk.isEmpty()) {
             int idx = stk.pop();
-            answer[idx] = prices.length - 1 - idx;
+            answer[idx] = prices.length - idx - 1;
         }
         return answer;
     }
