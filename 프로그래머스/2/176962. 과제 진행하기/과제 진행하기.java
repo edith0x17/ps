8f19e432fb9918ff438e89fb41dce3ab6def9eee
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-05-04 20:44:33 +0900

    [level 2] Title: 과제 진행하기, Time: 4.24 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -1,42 +1,45 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 class Solution {
     public String[] solution(String[][] plans) {
         String[] answer = {};
         ArrayList<Data> list = new ArrayList<>();
-        for (String[] p : plans) {
-            list.add(new Data(p[0], timeCal(p[1]), Integer.parseInt(p[2])));
+        for (String[] plan : plans) {
+            list.add(new Data(plan[0], timeCal(plan[1]), Integer.parseInt(plan[2])));
         }
-        Collections.sort(list, (a, b) -> a.start - b.start);//오름차순
-
+        list.sort((a, b) -> a.start - b.start);//start오름차순
         ArrayList<String> ret = new ArrayList<>();
         Stack<Data> stk = new Stack<>();
         for (int i = 0; i < list.size() - 1; i++) {
             Data cur = list.get(i);
             Data next = list.get(i + 1);
             int available = next.start - cur.start;
-            if (cur.remain <= available) {
+            if (cur.playTime <= available) {
                 ret.add(cur.name);
-                int extra = available - cur.remain;
+                int extra = available - cur.playTime;
                 while (!stk.isEmpty() && extra > 0) {
                     Data prev = stk.pop();
-                    if (prev.remain <= extra) {
+
+                    if (extra >= prev.playTime) {
                         ret.add(prev.name);
-                        extra -= prev.remain;
+                        extra -= prev.playTime;
                     } else {
-                        prev.remain -= extra;
-                        extra = 0;
+                        prev.playTime -= extra;
                         stk.push(prev);
+                        extra = 0;
                     }
                 }
             } else {
-                cur.remain -= available;
+                cur.playTime -= available;
                 stk.push(cur);
             }
         }
-        ret.add(list.get(list.size() - 1).name);//for
-        while (!stk.isEmpty()) {//stk
-            ret.add(stk.pop().name);
+        //list, stk순
+        ret.add(list.get(list.size() - 1).name);
+        while (!stk.isEmpty()) {
+            Data prev = stk.pop();
+            ret.add(prev.name);
         }
         return ret.toArray(new String[0]);
     }
@@ -49,12 +52,12 @@ class Solution {
 
     static class Data {
         String name;
-        int start, remain;
+        int start, playTime;
 
-        public Data(String name, int start, int remain) {
+        public Data(String name, int start, int playTime) {
             this.name = name;
             this.start = start;
-            this.remain = remain;
+            this.playTime = playTime;
         }
     }
 }
