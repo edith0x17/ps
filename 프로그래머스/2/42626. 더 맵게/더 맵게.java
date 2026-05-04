e4b6cd77f923fab64cb062ac971d127017cee9fc
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-05-04 21:23:08 +0900

    [level 2] Title: 더 맵게, Time: 1809.78 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,19 +3,20 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue<Long> pq = new PriorityQueue<>();
+        PriorityQueue<Long> pq = new PriorityQueue<>();//오름차순
         for (int i : scoville) {
             pq.offer((long) i);
         }
         while (pq.size() >= 2) {
             long first = pq.poll();
-            if (first >= K) return answer;
+            if (first >= K) break;
+
             long second = pq.poll();
-            long mixed = first + (second * 2);
-            pq.offer(mixed);
+            long sum = first + second * 2;
+            pq.offer(sum);
             answer++;
         }
         if (!pq.isEmpty() && pq.peek() >= K) return answer;
-        return -1;//pq 1개 && < K
+        return -1;
     }
 }
