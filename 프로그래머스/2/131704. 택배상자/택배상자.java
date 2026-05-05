a45b77ef79075feeae7d65db9241aba282c47647
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-05-05 18:39:04 +0900

    [level 2] Title: 택배상자, Time: 39.80 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,13 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
-        int answer = 0, num = 1;
+        int answer = 0;
         Stack<Integer> stk = new Stack<>();
+        int box = 1;
         for(int i = 0; i < order.length; i++){
-            while(num <= order[i]){
-                stk.push(num++);
+            while(box <= order[i]){//자기자신도 넣기
+                stk.push(box++);
             }
-            if(!stk.isEmpty() && order[i] == stk.pop())answer++;
+            if(!stk.isEmpty() && stk.pop() == order[i])answer++;
             else break;
         }
         return answer;
