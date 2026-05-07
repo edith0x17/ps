011b38da83c14b2b61645f0014648b9bdee2ad6a
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-05-07 20:49:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.39 ms, Memory: 58.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,39 +1,34 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    static int dx[] = {-1, 0, 1, 0};
-    static int dy[] = {0, 1, 0, -1};
-    static int n, m;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
     public int solution(int[][] maps) {
-        n = maps.length;
-        m = maps[0].length;
-        Queue<Data> q = new ArrayDeque<>();
+        int answer = -1;
+        int n = maps.length, m = maps[0].length;
         int[][] visited = new int[n][m];
+        Queue<int[]> q = new ArrayDeque<>();
+
         visited[0][0] = 1;
-        q.offer(new Data(0, 0));
-        while(!q.isEmpty()){
-            Data data = q.poll();
-            //
-            for(int i = 0; i < 4; i++){
-                int nx = data.x + dx[i];
-                int ny = data.y + dy[i];
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m)continue;
-                if(visited[nx][ny] != 0)continue;
-                if(maps[nx][ny] == 0)continue;
-                visited[nx][ny] = visited[data.x][data.y] + 1;
-                q.offer(new Data(nx, ny));
+        q.offer(new int[]{0, 0});
+        while (!q.isEmpty()) {
+            int[] tmp = q.poll();
+            int x = tmp[0], y = tmp[1];
+            if (x == n - 1 && y == m - 1) {
+                if (visited[x][y] != 0) return visited[x][y];
+                else return -1;
+            }
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (visited[nx][ny] != 0) continue;
+                if (maps[nx][ny] == 0) continue;
+                visited[nx][ny] = visited[x][y] + 1;
+                q.offer(new int[]{nx, ny});
             }
         }
-        if(visited[n - 1][m - 1] == 0)return - 1;
-        else return visited[n - 1][m - 1];
-    }
-    static class Data{
-        int x, y;
-        
-        public Data(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
+        return answer;
     }
 }
