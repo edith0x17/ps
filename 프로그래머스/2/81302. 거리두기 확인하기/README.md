424346aa33bf9a82959087a0dfb76eb5a33e0cef
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-06-02 16:36:06 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.19 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
 
 ### 성능 요약
 
-메모리: 63.9 MB, 시간: 0.11 ms
+메모리: 63.4 MB, 시간: 0.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 29일 16:01:44
+2026년 06월 02일 16:36:02
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -3,32 +3,35 @@ import java.util.*;
 class Solution {
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-
     static int[][] map;
+    static ArrayList<int[]> list;
 
     public int[] solution(String[][] places) {
-        int n = places.length;
-        int[] answer = new int[n];
-        for (int k = 0; k < n; k++) {
+        int[] answer = new int[places.length];
+        for (int k = 0; k < places.length; k++) {
+            //places[k]
             map = new int[5][5];
-            ArrayList<int[]> adj = new ArrayList<>();
+            list = new ArrayList<>();
             for (int i = 0; i < 5; i++) {
                 for (int j = 0; j < 5; j++) {
                     if (places[k][i].charAt(j) == 'P') {
                         map[i][j] = 1;
-                        adj.add(new int[]{i, j});
-                    } else if (places[k][i].charAt(j) == 'X') map[i][j] = -1;
+                        list.add(new int[]{i, j});
+                    } else if (places[k][i].charAt(j) == 'X') {
+                        map[i][j] = -1;
+                    } else {
+                        map[i][j] = 0;
+                    }
                 }
             }
             boolean flag = false;
-            for (int i = 0; i < adj.size(); i++) {
-                int[] tmp = adj.get(i);
-                if (!bfs(tmp[0], tmp[1])) {
+            for (int i = 0; i < list.size(); i++) {
+                int[] cur = list.get(i);
+                if (!bfs(cur[0], cur[1])) {
                     flag = true;
                     break;
                 }
             }
-
             if (flag) answer[k] = 0;
             else answer[k] = 1;
         }
@@ -44,7 +47,7 @@ class Solution {
         while (!q.isEmpty()) {
             int[] cur = q.poll();
 
-            if (cur[2] == 2) continue;//거리2까지만 검사, continue(O), break(X) 
+            if (cur[2] == 2) continue;
 
             for (int i = 0; i < 4; i++) {
                 int nx = cur[0] + dx[i];
@@ -52,9 +55,7 @@ class Solution {
                 if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
                 if (visited[nx][ny]) continue;
                 if (map[nx][ny] == -1) continue;
-
                 if (map[nx][ny] == 1) return false;
-
                 visited[nx][ny] = true;
                 q.offer(new int[]{nx, ny, cur[2] + 1});
             }
