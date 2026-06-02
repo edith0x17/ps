c6fcbc01ac276c61ac7366e66865e4f38f04cf72
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-06-02 16:52:18 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 9.90 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,39 +1,36 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public String[] solution(String[] files) {
+        String[] answer = {};
         ArrayList<Data> list = new ArrayList<>();
-        for (int idx = 0; idx < files.length; idx++) {
+        for (int i = 0; i < files.length; i++) {
             int s = -1, e = -1;
-            for (int i = 0; i < files[idx].length(); i++) {
-                if ('0' <= files[idx].charAt(i) && files[idx].charAt(i) <= '9') {
-                    s = i;
+            for (int j = 0; j < files[i].length(); j++) {
+                if (files[i].charAt(j) >= '0' && files[i].charAt(j) <= '9') {
+                    s = j;
                     break;
                 }
             }
             e = s;
-            while (e < files[idx].length() && Character.isDigit(files[idx].charAt(e))) {
+            while (e < files[i].length() && files[i].charAt(e) >= '0' && files[i].charAt(e) <= '9') {
                 e++;
             }
-            String head = files[idx].substring(0, s);
-            String number = files[idx].substring(s, e);
-            list.add(new Data(files[idx], head, Integer.parseInt(number), idx));
+            String head = files[i].substring(0, s);
+            String number = files[i].substring(s, e);
+            list.add(new Data(files[i], head, Integer.parseInt(number), i));
         }
         Collections.sort(list);
-        String[] answer = new String[files.length];
-        for (int i = 0; i < files.length; i++) {
+        answer = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i).ori;
         }
         return answer;
     }
 
     static class Data implements Comparable<Data> {
-        String ori;
-        String head;
-        int number;
-        int idx;
+        String ori, head;
+        int number, idx;
 
         public Data(String ori, String head, int number, int idx) {
             this.ori = ori;
@@ -44,14 +41,11 @@ class Solution {
 
         @Override
         public int compareTo(Data o) {
-            int cmp = head.compareToIgnoreCase(o.head);
-            if (cmp != 0) return cmp;
-            if (number != o.number) return Integer.compare(number, o.number);
-            return Integer.compare(idx, o.idx);
+            int cmp = this.head.toLowerCase().compareTo(o.head.toLowerCase());
+            if (cmp != 0) return this.head.toLowerCase().compareTo(o.head.toLowerCase());
+            if (this.number != o.number) return Integer.compare(this.number, o.number);
+            return Integer.compare(this.idx, o.idx);
         }
     }
 }
-//head, number, tail
-//head 대소문자 구분X -> how???
-//number Integer.parseInt()
-//tail -> 원래
+//head number tail
