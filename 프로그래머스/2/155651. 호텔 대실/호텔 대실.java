2ea33ec0b1e780b1789aafe63234ec3cbb1fec64
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-06-03 15:51:34 +0900

    [level 2] Title: 호텔 대실, Time: 5.01 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -1,30 +1,38 @@
-// 8
 import java.util.*;
 
 class Solution {
     public int solution(String[][] book_time) {
         int answer = 0;
-        int[][] tmp = new int[book_time.length][2];
-        for(int i = 0; i < book_time.length; i++){
-            tmp[i][0] = timeCal(book_time[i][0]);
-            tmp[i][1] = timeCal(book_time[i][1]) + 10;
+        ArrayList<Data> list = new ArrayList<>();
+        for (String[] ss : book_time) {
+            //ss[0] ss[1]
+            int start = timeCal(ss[0]);
+            int end = timeCal(ss[1]) + 10;
+            list.add(new Data(start, end));
         }
-        Arrays.sort(tmp, (a, b) -> a[0] - b[0]);
+        Collections.sort(list, (a, b) -> (a.start - b.start));
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int[] ary: tmp){
-            int s = ary[0], e = ary[1];
-            while(!pq.isEmpty() && pq.peek() <= s){
-                pq.poll();
-            }
-            pq.offer(e);
+        for (int i = 0; i < list.size(); i++) {
+            Data cur = list.get(i);
+            if (!pq.isEmpty() && pq.peek() <= cur.start) pq.poll();
+            pq.offer(cur.end);
             answer = Math.max(answer, pq.size());
         }
         return answer;
     }
-    
-    static int timeCal(String s){
+
+    static int timeCal(String s) {
         int h = Integer.parseInt(s.substring(0, 2));
         int m = Integer.parseInt(s.substring(3, 5));
         return h * 60 + m;
+    }
+
+    static class Data {
+        int start, end;
+
+        public Data(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
     }
 }
