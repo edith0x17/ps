e71e09b04b0f6ef83706e3e3ded9af75a9904b09
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-07-14 15:05:19 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 71 MB, 시간: 0.14 ms
+메모리: 85.8 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 16일 22:34:46
+2026년 07월 14일 15:05:16
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,32 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        // Step 1: 여벌 체육복을 가진 학생이 도난당한 경우 처리
-        Set<Integer> lostSet = new HashSet<>();
-        Set<Integer> reserveSet = new HashSet<>();
-        
-        for (int l : lost) lostSet.add(l);
-        for (int r : reserve) {
-            if (lostSet.contains(r)) {
-                lostSet.remove(r); // 도난당했지만 여벌이 있는 학생
-            } else {
-                reserveSet.add(r); // 순수한 여벌 학생
+        int answer = 0;
+        int[] cnt = new int[n + 1];//-1, 0, 1
+        for (int i : lost) {
+            cnt[i]--;
+        }
+        for (int i : reserve) {
+            cnt[i]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (cnt[i] == -1) {
+                if (i > 1 && cnt[i - 1] == 1) {
+                    cnt[i - 1]--;
+                    cnt[i]++;
+                } else if (i < n && cnt[i + 1] == 1) {
+                    cnt[i + 1]--;
+                    cnt[i]++;
+                }
             }
         }
-
-        // Step 2: 남은 lost 학생들에 대해 여벌 빌리기
-        int answer = n - lostSet.size(); // 기본적으로 체육복 있는 학생 수
-        
-        for (int l : lostSet) {
-            if (reserveSet.contains(l - 1)) {
-                reserveSet.remove(l - 1); // 왼쪽 학생이 여벌 빌려줌
-                answer++;
-            } else if (reserveSet.contains(l + 1)) {
-                reserveSet.remove(l + 1); // 오른쪽 학생이 여벌 빌려줌
-                answer++;
-            }
+        for (int i = 1; i <= n; i++) {
+            if (cnt[i] >= 0) answer++;
         }
-        
         return answer;
     }
 }
