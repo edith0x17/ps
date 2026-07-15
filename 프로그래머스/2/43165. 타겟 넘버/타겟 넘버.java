bc7c1856ef8fa67276fb8eb4377136b544ad6889
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-07-15 16:50:06 +0900

    [level 2] Title: 타겟 넘버, Time: 326.38 ms, Memory: 306 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,29 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    static int answer = 0;
     public int solution(int[] numbers, int target) {
-        go(0, 0, numbers, target);
-        return answer;
-    }
-    static void go(int idx, int sum, int[] numbers, int target){
-        if(idx == numbers.length){
-            if(sum == target)answer++;
-            return;
+        int answer = 0;
+        int n = numbers.length;
+        int S = (1 << n);
+        for (int i = 0; i < S; i++) {
+            ArrayList<Integer> adj = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    adj.add(1);
+                } else {
+                    adj.add(0);
+                }
+            }
+            int sum = 0;
+            for (int j = 0; j < adj.size(); j++) {
+                if (adj.get(j) == 1) {
+                    sum += numbers[j];
+                } else {
+                    sum -= numbers[j];
+                }
+            }
+            if (sum == target) answer++;
         }
-        go(idx + 1, sum + numbers[idx], numbers, target);
-        go(idx + 1, sum - numbers[idx], numbers, target);
+        return answer;
     }
 }
