c187690ccfe37d51aaa2454e19222f4f3ff28776
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-07-18 21:01:17 +0900

    [level 2] Title: 소수 찾기, Time: 7.50 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,47 +1,63 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    static int[] a, ret;
+
     static boolean[] visited;
-    static Set<Integer> set = new HashSet<>();
+    static int[] ret;
+    static HashSet<Integer> set = new HashSet<>();
+
     public int solution(String numbers) {
-        a = new int[numbers.length()];
-        for(int i = 0; i < numbers.length(); i++){
-            a[i] = numbers.charAt(i) - '0';
-        }
-        ret = new int[numbers.length()];
+
         visited = new boolean[numbers.length()];
-        
-        for(int i = 1; i <= numbers.length(); i++){
-            go(0, i);
+        ret = new int[numbers.length()];
+
+        for (int len = 1; len <= numbers.length(); len++) {
+            go(0, len, numbers);
         }
+
         return set.size();
     }
-    static boolean check(int x){
-        if (x < 2) return false;
-        for (int i = 2; i * i <= x; i++){
-            if(x % i == 0) return false;
-        }
-        return true;
-    }
-    static void go(int depth, int s){
-        if(depth == s){
-            int x = 0;
-            for (int i = 0; i < s; i++) {
-                x = x * 10 + ret[i];
+
+    static void go(int depth, int len, String numbers) {
+
+        if (depth == len) {
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < len; i++) {
+                sb.append(numbers.charAt(ret[i]));
             }
-            if(check(x)){
-                set.add(x);
+
+            int num = Integer.parseInt(sb.toString());
+
+            if (isPrime(num)) {
+                set.add(num);
             }
-            return ;
+
+            return;
         }
-        for(int i = 0; i < a.length; i++){
-            if(visited[i])continue;
-            ret[depth] = a[i];
+
+        for (int i = 0; i < numbers.length(); i++) {
+
+            if (visited[i]) continue;
+
             visited[i] = true;
-            go(depth + 1, s);
+            ret[depth] = i;
+
+            go(depth + 1, len, numbers);
+
             visited[i] = false;
         }
+    }
+
+    static boolean isPrime(int num) {
+
+        if (num < 2) return false;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+
+        return true;
     }
 }
