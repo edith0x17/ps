b499951cd6409bd397609135a6157e5e8016e159
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-07-23 13:51:45 +0900

    [level 3] Title: 단어 변환, Time: 0.16 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,34 +1,41 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     static boolean[] visited;
     static int answer = Integer.MAX_VALUE;
+    
     public int solution(String begin, String target, String[] words) {
+        boolean flag = false;
+        for(String word : words){
+            if(target.equals(word))flag = true;
+        }
+        if(!flag)return 0;
+        
         visited = new boolean[words.length];
-        dfs(begin, target, words, 0);
-        if(answer == Integer.MAX_VALUE)return 0;
+        dfs(0, begin, target, words);
         return answer;
     }
-    static void dfs(String begin, String target, String[] words, int cnt){ //depth, visited
+    
+    static boolean check(String a, String b){
+        int cnt = 0;
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i) != b.charAt(i))cnt++;
+        }
+        return cnt == 1;
+    }
+    
+    static void dfs(int cnt, String begin, String target, String[] words){
         if(answer <= cnt)return;
         if(begin.equals(target)){
             answer = Math.min(answer, cnt);
             return;
         }
         for(int i = 0; i < words.length; i++){
-            if(visited[i])continue; //방문
-            if(!check(begin, words[i]))continue; //장애물
+            if(visited[i])continue;
+            if(!check(begin, words[i]))continue;
             visited[i] = true;
-            dfs(words[i], target, words, cnt + 1);
+            dfs(cnt + 1, words[i], target, words);
             visited[i] = false;
         }
-    }
-    static boolean check(String a, String b){ //한글자
-        int cnt = 0;
-        for(int i = 0; i < a.length(); i++){
-            if(a.charAt(i) != b.charAt(i))cnt++;
-        }
-        return cnt == 1;
     }
 }
