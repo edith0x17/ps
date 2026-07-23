2ed4f4c5400be33f00bb72ec3c8fed0ff15582e4
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-07-23 13:26:55 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.60 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,32 +3,33 @@ import java.util.*;
 class Solution {
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-
+    
     public int solution(int[][] maps) {
-        int answer = -1;
+        int answer = 0;
         int n = maps.length, m = maps[0].length;
         int[][] visited = new int[n][m];
         Queue<int[]> q = new ArrayDeque<>();
-
+        
         visited[0][0] = 1;
         q.offer(new int[]{0, 0});
-        while (!q.isEmpty()) {
-            int[] tmp = q.poll();
-            int x = tmp[0], y = tmp[1];
-            if (x == n - 1 && y == m - 1) {
-                if (visited[x][y] != 0) return visited[x][y];
-                else return -1;
-            }
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (visited[nx][ny] != 0) continue;
-                if (maps[nx][ny] == 0) continue;
-                visited[nx][ny] = visited[x][y] + 1;
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            
+            if(cur[0] == n - 1 && cur[1] == m - 1)break;
+            
+            for(int i = 0; i < 4; i++){
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m)continue;//범위
+                if(visited[nx][ny] != 0)continue;//방문
+                if(maps[nx][ny] == 0)continue;//장애물
+                visited[nx][ny] = visited[cur[0]][cur[1]] + 1;
                 q.offer(new int[]{nx, ny});
             }
         }
+        if(visited[n - 1][m - 1] == 0)answer = -1;
+        else answer = visited[n - 1][m - 1];
         return answer;
     }
 }
+//bfs visited push 
