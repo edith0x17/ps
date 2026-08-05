3d865a02de8a48acd58096d00012c4a447b3c86e
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-05 15:14:39 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 12.92 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -4,22 +4,22 @@ class Solution {
     public String solution(int n, int t, int m, String[] timetable) {
         String answer = "";
         int[] crew = new int[timetable.length];
-        for (int i = 0; i < timetable.length; i++) {
-            crew[i] = timeCal(timetable[i]);
+        int idx = 0;
+        for (String s : timetable) {
+            crew[idx++] = timeCal(s);
         }
         Arrays.sort(crew);
-        int idx = 0;
+        idx = 0;
         for (int i = 0; i < n; i++) {
-            int busTime = 540 + i * t;
+            int busTime = 9 * 60 + i * t;
             int cnt = 0;
-
-            while (idx < crew.length && crew[idx] <= busTime && cnt < m) {//길이 
+            while (idx < timetable.length && crew[idx] <= busTime && cnt < m) {
                 idx++;
                 cnt++;
             }
             if (i == n - 1) {
                 if (cnt < m) return toTime(busTime);
-                else return toTime(crew[idx - 1] - 1);
+                else return toTime(crew[idx - 1] - 1);//idx - 1까지 탑승완료
             }
         }
         return answer;
