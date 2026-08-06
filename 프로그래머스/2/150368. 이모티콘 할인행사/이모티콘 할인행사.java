b5e9455e70a3598d2ab56163b804f0664de3eef6
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-06 10:36:48 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 31.94 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,44 +1,49 @@
 import java.util.*;
 
 class Solution {
-    static int mxCnt, mxPrice;
-    static int n;
+    static int n, price;
     static int[] discount = new int[]{10, 20, 30, 40};
     static int[] ret;
 
     public int[] solution(int[][] users, int[] emoticons) {
-        n = emoticons.length;
-        ret = new int[n];
+        ret = new int[emoticons.length];
+        int[] answer = {};
         go(0, users, emoticons);
-        return new int[]{mxCnt, mxPrice};
+        answer = new int[]{n, price};
+        return answer;
     }
 
     static void go(int depth, int[][] users, int[] emoticons) {
-        if (depth == n) {
-            int cnt = 0, price = 0;
-            for (int[] user : users) {
-                int tmpPrice = 0;
-                for (int i = 0; i < emoticons.length; i++) {
-                    if (discount[ret[i]] >= user[0]) {
-                        tmpPrice += emoticons[i] * (100 - discount[ret[i]]) / 100;
-                    }
-                }
-                if (tmpPrice >= user[1]) cnt++;
-                else price += tmpPrice;
-            }
-            if (cnt > mxCnt) {
-                mxCnt = cnt;
-                mxPrice = price;
-            } else if (cnt == mxCnt) {
-                mxPrice = Math.max(mxPrice, price);
-            }
-            System.out.println(mxCnt + " " + mxPrice);
+        if (depth == emoticons.length) {
+            check(users, emoticons);
             return;
         }
-
-        for (int i = 0; i < 4; i++) {//중복순열
-            ret[depth] = i;
+        for (int i = 0; i < 4; i++) {//10, 20, 30, 40
+            ret[depth] = discount[i];
             go(depth + 1, users, emoticons);
+        }
+    }
+
+    static void check(int[][] users, int[] emoticons) {
+        int nTmp = 0, priceTmp = 0;
+        for (int[] user : users) {
+            //users[0] 40, users[1] 10000
+            int sum = 0;
+            for (int i = 0; i < emoticons.length; i++) {
+                if (ret[i] >= user[0]) {
+                    sum += emoticons[i] * (100 - ret[i]) / 100;
+                }
+            }
+            if (sum >= user[1]) nTmp++;
+            else priceTmp += sum;
+        }
+        if (nTmp > n) {
+            n = nTmp;
+            price = priceTmp;
+        } else if (nTmp == n) {
+            if (priceTmp > price) {
+                price = priceTmp;
+            }
         }
     }
 }
