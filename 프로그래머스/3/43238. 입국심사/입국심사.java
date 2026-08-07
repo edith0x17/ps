7cb6e2b342b5e2ff7e5dc237ae096921a32c0a02
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-07 12:01:43 +0900

    [level 3] Title: 입국심사, Time: 33.50 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -3,23 +3,29 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
+
         Arrays.sort(times);
-        long left = 1, right = 1_000_000_004 * (long)n;
-        while(left <= right){
+
+        long left = 1;
+        long right = (long) times[times.length - 1] * n;
+
+        while (left <= right) {
             long mid = (left + right) / 2;
-            //mid
-            long sum = 0;
-            for(int i = 0; i < times.length; i++){
-                sum += mid / times[i];
+
+            long cnt = 0;
+
+            for (int i = 0; i < times.length; i++) {
+                cnt += mid / times[i];
             }
-            if(sum >= n){
-                System.out.println(mid);
+
+            if (cnt >= n) {
                 answer = mid;
                 right = mid - 1;
-            }else{
+            } else {
                 left = mid + 1;
             }
         }
+
         return answer;
     }
 }
