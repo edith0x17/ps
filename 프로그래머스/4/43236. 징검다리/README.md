f94e3738121151fe7d046e4120656d8daebd5ec5
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-10 14:31:27 +0900

    [level 4] Title: 징검다리, Time: 21.60 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/README.md
+++ 프로그래머스/4/43236. 징검다리/README.md
@@ -1,10 +1,10 @@
 # [level 4] 징검다리 - 43236 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43236?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43236) 
 
 ### 성능 요약
 
-메모리: 91.4 MB, 시간: 17.13 ms
+메모리: 88.5 MB, 시간: 21.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 04일 16:26:35
+2026년 08월 10일 14:31:24
 
 ### 문제 설명
 
--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -4,28 +4,25 @@ class Solution {
     public int solution(int distance, int[] rocks, int n) {
         int answer = 0;
         Arrays.sort(rocks);
-        int l = 0, r = distance;
+        int l = 1, r = distance;
         while (l <= r) {
-            int mid = (l + r) / 2;
-            int remove = check(mid, rocks, distance);
-            if (remove <= n) {
+            int mid = (l + r) / 2;//최소거리 
+            int prev = 0, cnt = 0;
+            for (int i = 0; i < rocks.length; i++) {
+                if (rocks[i] - prev < mid) cnt++;
+                else prev = rocks[i];
+            }
+            if (distance - prev < mid) {//마지막
+                cnt++;
+            }
+
+            if (cnt > n) {
+                r = mid - 1;
+            } else {//cnt <= n
                 answer = mid;
                 l = mid + 1;
-            } else {
-                r = mid - 1;
             }
         }
         return answer;
-    }
-
-    int check(int mid, int[] rocks, int distance) {
-        int remove = 0;
-        int prev = 0;
-        for (int rock : rocks) {
-            if (rock - prev < mid) remove++;
-            else prev = rock;
-        }
-        if (distance - prev < mid) remove++;
-        return remove;
     }
 }
