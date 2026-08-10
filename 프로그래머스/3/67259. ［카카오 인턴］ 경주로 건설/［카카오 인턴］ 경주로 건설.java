76a5f327548d209462c43698d8c3c92747ac4bb8
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-10 17:11:23 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 7.83 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -3,25 +3,25 @@ import java.util.*;
 class Solution {
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static int n;
 
     public int solution(int[][] board) {
-        n = board.length;
-        int[][][] visited = new int[n][n][4];
+        int answer = Integer.MAX_VALUE;
+        int n = board.length, m = board[0].length;
+        int[][][] visited = new int[n][m][4];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 Arrays.fill(visited[i][j], Integer.MAX_VALUE);
             }
         }
         Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{0, 0, -1, 0});
+        q.offer(new int[]{0, 0, -1, 0});//x, y, dir, price
         while (!q.isEmpty()) {
             int[] tmp = q.poll();
             int x = tmp[0], y = tmp[1], dir = tmp[2], price = tmp[3];
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
                 if (board[nx][ny] == 1) continue;
                 int nextPrice = price;
                 if (dir == -1 || dir == i) nextPrice += 100;
@@ -32,10 +32,8 @@ class Solution {
                 }
             }
         }
-
-        int answer = Integer.MAX_VALUE;
-        for (int dir = 0; dir < 4; dir++) {
-            answer = Math.min(answer, visited[n - 1][n - 1][dir]);
+        for (int i = 0; i < 4; i++) {
+            answer = Math.min(answer, visited[n - 1][m - 1][i]);
         }
         return answer;
     }
