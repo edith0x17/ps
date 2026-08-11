b2c4a644a636d61d29c20350443821cb5a1d60fd
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-11 11:54:38 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 9.50 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
@@ -1,117 +1,115 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    
-    static int n;
     static boolean[][] visited;
+
     public int solution(int[][] game_board, int[][] table) {
-        n = game_board.length;
-        List<List<Data>> emptySpaces = new ArrayList<>();
-        visited = new boolean[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(game_board[i][j] == 0 && !visited[i][j]){
-                    emptySpaces.add(extractSpaces(i, j, game_board, 0));
-                }
-            }
-        }
-        List<List<Data>> puzzlePieces = new ArrayList<>();
-        visited = new boolean[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(table[i][j] == 1 && !visited[i][j]){
-                    puzzlePieces.add(extractSpaces(i, j, table, 1));
-                }
-            }
-        }
         int answer = 0;
-        boolean[] used = new boolean[puzzlePieces.size()];
-        for(List<Data> emptySpace :emptySpaces){
-            //emptySpace <- puzzlePieces
-            for(int i = 0; i < puzzlePieces.size(); i++){
-                if(used[i])continue;
-                
-                List<Data> puzzlePiece = puzzlePieces.get(i);
-                for(int d = 0; d < 4; d++){
-                    puzzlePiece = rotate(puzzlePiece);
-                    if(check(emptySpace, puzzlePiece)){
-                        answer += puzzlePiece.size();
+        ArrayList<ArrayList<int[]>> empties = new ArrayList<>();
+        ArrayList<ArrayList<int[]>> blocks = new ArrayList<>();
+        //game_board//0은 빈칸, 1은 이미 채워진 칸
+        visited = new boolean[game_board.length][game_board[0].length];
+        for (int i = 0; i < game_board.length; i++) {
+            for (int j = 0; j < game_board[0].length; j++) {
+                if (game_board[i][j] == 0 && !visited[i][j]) {
+                    ArrayList<int[]> tmp = dfs(i, j, game_board, 0);
+                    empties.add(tmp);
+                }
+            }
+        }
+        //table//0은 빈칸, 1은 조각이 놓인 칸
+        visited = new boolean[table.length][table[0].length];
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[0].length; j++) {
+                if (table[i][j] == 1 && !visited[i][j]) {
+                    ArrayList<int[]> tmp = dfs(i, j, table, 1);
+                    blocks.add(tmp);
+                }
+            }
+        }
+        /*ArrayList<ArrayList<int[]>> empties = new ArrayList<>();
+        ArrayList<ArrayList<int[]>> blocks = new ArrayList<>();*/
+        for (int i = 0; i < empties.size(); i++) {
+            empties.set(i, normalize(empties.get(i)));
+        }
+        for (int i = 0; i < blocks.size(); i++) {
+            blocks.set(i, normalize(blocks.get(i)));
+        }
+
+        boolean[] used = new boolean[blocks.size()];
+        for (ArrayList<int[]> empty : empties) {//빈
+
+            for (int i = 0; i < blocks.size(); i++) {//블록
+                if (used[i]) continue;
+
+                ArrayList<int[]> cur = blocks.get(i);
+
+                if (empty.size() != cur.size()) continue;
+
+                for (int d = 0; d < 4; d++) {
+                    if (same(empty, cur)) {
                         used[i] = true;
+                        answer += empty.size();
                         break;
                     }
+                    cur = rotate(cur);
                 }
-                if(used[i])break;
+                if (used[i]) break;
             }
         }
         return answer;
     }
-    
-    static boolean check(List<Data> a, List<Data> b){
-        if(a.size() != b.size())return false;
-        for(int i = 0; i < a.size(); i++){
-            if((a.get(i).x != b.get(i).x) || a.get(i).y != b.get(i).y)return false;
+
+    static boolean same(ArrayList<int[]> a, ArrayList<int[]> b) {
+        if (a.size() != b.size()) return false;
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i)[0] != b.get(i)[0]) return false;
+            if (a.get(i)[1] != b.get(i)[1]) return false;
         }
         return true;
     }
-    
-    static List<Data> rotate(List<Data> adj){
-        List<Data> rotated = new ArrayList<>();
-        for(Data d: adj){
-            rotated.add(new Data(d.y, -d.x));
+
+    static ArrayList<int[]> rotate(ArrayList<int[]> shape) {
+        ArrayList<int[]> ret = new ArrayList<>();
+        for (int[] p : shape) {
+            int x = p[0];
+            int y = p[1];
+            ret.add(new int[]{-y, x});
         }
-        return normalize(rotated);
+        return normalize(ret);
     }
-    
-    static List<Data> normalize(List<Data> adj){
-        List<Data> norm = new ArrayList<>();
+
+    static ArrayList<int[]> normalize(ArrayList<int[]> shape) {
+        ArrayList<int[]> ret = new ArrayList<>();
         int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE;
-        for(Data d: adj){
-            minX = Math.min(minX, d.x);
-            minY = Math.min(minY, d.y);
+        for (int[] tmp : shape) {
+            minX = Math.min(minX, tmp[0]);
+            minY = Math.min(minY, tmp[1]);
         }
-        for(Data d: adj){
-            norm.add(new Data(d.x - minX, d.y - minY));
+        for (int[] tmp : shape) {
+            ret.add(new int[]{tmp[0] - minX, tmp[1] - minY});
         }
-        Collections.sort(norm); //
-        return norm;
+        ret.sort((a, b) -> {
+            if (a[0] == b[0]) return a[1] - b[1];
+            return a[0] - b[0];
+        });
+        return ret;
     }
-    
-    static List<Data> extractSpaces(int x, int y, int[][] board, int target){
-        List<Data> adj = new ArrayList<>();
-        adj.add(new Data(x, y));
-        Queue<Data> q = new ArrayDeque<>();
+
+    static ArrayList<int[]> dfs(int x, int y, int[][] map, int flag) {
+        ArrayList<int[]> ret = new ArrayList<>();
         visited[x][y] = true;
-        q.offer(new Data(x, y));
-        while(!q.isEmpty()){
-            Data data = q.poll();
-            for(int i = 0; i < 4; i++){
-                int nx = data.x + dx[i];
-                int ny = data.y + dy[i];
-                if(nx < 0 || nx >= n || ny < 0 || ny >= n)continue; //범위
-                if(visited[nx][ny])continue; //방문
-                if(board[nx][ny] != target)continue; //장애물
-                adj.add(new Data(nx, ny));
-                visited[nx][ny] = true;
-                q.offer(new Data(nx, ny));
-            }
+        ret.add(new int[]{x, y});
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= map.length || ny < 0 || ny >= map[0].length) continue;
+            if (visited[nx][ny]) continue;
+            if (map[nx][ny] != flag) continue;
+            ret.addAll(dfs(nx, ny, map, flag));
         }
-        return normalize(adj);
-    }
-    static class Data implements Comparable<Data>{
-        int x, y;
-        
-        Data(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-        
-        @Override
-        public int compareTo(Data o){
-            if(this.x == o.x)return Integer.compare(this.y, o.y);
-            return Integer.compare(this.x, o.x);
-        }
+        return ret;
     }
 }
