395a0a008fee7f389d0a6c86a3e0dfbacac62be6
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-11 10:16:21 +0900

    [level 3] Title: 외벽 점검, Time: 25.73 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -1,84 +1,60 @@
 import java.util.*;
 
 class Solution {
+    static int ans = Integer.MAX_VALUE;
+    static int[] weak2;
     static boolean[] visited;
     static int[] ret;
-    static int answer;
-    static int N;
-    static int[] Weak;
-    static int[] Weak2;
 
     public int solution(int n, int[] weak, int[] dist) {
-        answer = Integer.MAX_VALUE;
-
-        N = n;
-        Weak = weak;
-
+        weak2 = new int[weak.length * 2];
+        for (int i = 0; i < weak.length; i++) {
+            weak2[i] = weak[i];
+        }
+        for (int i = weak.length; i < weak2.length; i++) {
+            weak2[i] = weak[i - weak.length] + n;
+        }
         visited = new boolean[dist.length];
         ret = new int[dist.length];
-
-        Weak2 = new int[weak.length * 2];
-
-        for (int i = 0; i < weak.length; i++) {
-            Weak2[i] = weak[i];
-        }
-
-        for (int i = weak.length; i < weak.length * 2; i++) {
-            Weak2[i] = weak[i - weak.length] + n;
-        }
-
-        go(0, dist);
-
-        return answer == Integer.MAX_VALUE ? -1 : answer;
+        go(0, weak, dist);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 
-    static void check(int[] order) {
-
-        for (int start = 0; start < Weak.length; start++) {//1, 5, 6, 10
+    static void check(int[] weak, int[] dist, int[] order) {
+        for (int start = 0; start < weak.length; start++) {
             int friendIdx = 0;
-            int cover = Weak2[start] + order[friendIdx];
-
-            for (int i = start; i < start + Weak.length; i++) {
-                if (Weak2[i] > cover) {
+            int cover = weak2[start] + order[friendIdx];
+            boolean possible = true;
+            for (int i = start; i < start + weak.length; i++) {
+                if (weak2[i] > cover) {
                     friendIdx++;
-
-                    if (friendIdx == order.length) break;
-                    
-                    cover = Weak2[i] + order[friendIdx];
+                    if (friendIdx == order.length) {
+                        possible = false;
+                        break;
+                    }
+                    cover = weak2[i] + order[friendIdx];
                 }
             }
-
-            // friendIdx는 0부터 시작하니까 사용 친구 수는 +1
-            if (friendIdx < order.length) {
-                answer = Math.min(answer, friendIdx + 1);
+            if (possible) {
+                ans = Math.min(ans, friendIdx + 1);
             }
         }
     }
 
-    static void go(int depth, int[] dist) {
-
+    static void go(int depth, int[] weak, int[] dist) {
         if (depth == dist.length) {
-
-            int[] order = new int[dist.length];
-
-            for (int i = 0; i < dist.length; i++) {
+            int[] order = new int[ret.length];
+            for (int i = 0; i < ret.length; i++) {
                 order[i] = dist[ret[i]];
             }
-
-            check(order);
-
+            check(weak, dist, order);
             return;
         }
-
         for (int i = 0; i < dist.length; i++) {
-
             if (visited[i]) continue;
-
             visited[i] = true;
             ret[depth] = i;
-
-            go(depth + 1, dist);
-
+            go(depth + 1, weak, dist);
             visited[i] = false;
         }
     }
