00733e170d2b25329632c739da4e3dcd8c393695
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-12 16:31:56 +0900

    [level 4] Title: 징검다리, Time: 19.22 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -2,27 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
-        int answer = 0;
+        int ans = 0;
         Arrays.sort(rocks);
         int l = 1, r = distance;
         while (l <= r) {
-            int mid = (l + r) / 2;//최소거리 
+            int mid = (l + r) / 2;
             int prev = 0, cnt = 0;
             for (int i = 0; i < rocks.length; i++) {
                 if (rocks[i] - prev < mid) cnt++;
                 else prev = rocks[i];
             }
-            if (distance - prev < mid) {//마지막
-                cnt++;
-            }
-
+            if (distance - prev < mid) cnt++;
             if (cnt > n) {
                 r = mid - 1;
             } else {//cnt <= n
-                answer = mid;
+                ans = mid;
                 l = mid + 1;
             }
         }
-        return answer;
+        return ans;
     }
 }
