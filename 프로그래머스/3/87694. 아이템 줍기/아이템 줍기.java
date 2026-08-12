2f4be45fa9be185afbf908c9eae026fca8501641
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-12 11:15:04 +0900

    [level 3] Title: 아이템 줍기, Time: 1.05 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,14 +1,14 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static int[][] map = new int[104][104];
+    static int[][] map;
 
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
-        for (int[] tmp : rectangle) {
+        map = new int[104][104];
+        for (int[] tmp : rectangle) {//
             int sx = tmp[0] * 2, sy = tmp[1] * 2, ex = tmp[2] * 2, ey = tmp[3] * 2;
             for (int i = sx; i <= ex; i++) {
                 for (int j = sy; j <= ey; j++) {
@@ -16,7 +16,7 @@ class Solution {
                 }
             }
         }
-        for (int[] tmp : rectangle) {
+        for (int[] tmp : rectangle) {//테두리
             int sx = tmp[0] * 2, sy = tmp[1] * 2, ex = tmp[2] * 2, ey = tmp[3] * 2;
             for (int i = sx + 1; i < ex; i++) {
                 for (int j = sy + 1; j < ey; j++) {
@@ -24,26 +24,27 @@ class Solution {
                 }
             }
         }
-        characterX *= 2;
-        characterY *= 2;
-        itemX *= 2;
-        itemY *= 2;
+        characterX = characterX * 2;
+        characterY = characterY * 2;
+        itemX = itemX * 2;
+        itemY = itemY * 2;
         int[][] visited = new int[104][104];
         Queue<int[]> q = new ArrayDeque<>();
         visited[characterX][characterY] = 1;
         q.offer(new int[]{characterX, characterY});
         while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            if (cur[0] == itemX && cur[1] == itemY) {
-                return visited[cur[0]][cur[1]] / 2;
+            int[] tmp = q.poll();
+            int x = tmp[0], y = tmp[1];
+            if (x == itemX && y == itemY) {
+                return visited[x][y] / 2;
             }
             for (int i = 0; i < 4; i++) {
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 if (nx < 0 || nx >= 104 || ny < 0 || ny >= 104) continue;
                 if (visited[nx][ny] != 0) continue;
                 if (map[nx][ny] == 0) continue;
-                visited[nx][ny] = visited[cur[0]][cur[1]] + 1;
+                visited[nx][ny] = visited[x][y] + 1;
                 q.offer(new int[]{nx, ny});
             }
         }
