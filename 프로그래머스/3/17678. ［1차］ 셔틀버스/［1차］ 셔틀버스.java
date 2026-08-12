4b68c795540e52af4802191ec5c3d384ee39e9bc
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-12 16:46:24 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 17.97 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -1,31 +1,34 @@
 import java.util.*;
 
 class Solution {
-    public String solution(int n, int t, int m, String[] timetable) {
-        int ans = 0;
-        int[] crew = new int[timetable.length];
-        for (int i = 0; i < timetable.length; i++) {
-            crew[i] = timeCal(timetable[i]);
+    public String solution(int n, int t, int m, String[] timeTable) {
+        String answer = "";
+        int[] crew = new int[timeTable.length];
+        for (int i = 0; i < timeTable.length; i++) {
+            crew[i] = timeCal(timeTable[i]);
         }
         Arrays.sort(crew);
         int idx = 0;
         for (int i = 0; i < n; i++) {
-            int busTime = 9 * 60 + i * t, cnt = 0;
+            int busTime = 9 * 60 + i * t;
+            int cnt = 0;
             while (idx < crew.length && crew[idx] <= busTime && cnt < m) {
                 idx++;
                 cnt++;
             }
             if (i == n - 1) {
-                if (cnt < m) ans = busTime;
-                else ans = crew[idx - 1] - 1;
+                if (cnt < m) return String.format("%02d:%02d", busTime / 60, busTime % 60);
+                else return String.format("%02d:%02d", (crew[idx - 1] - 1) / 60, (crew[idx - 1] - 1) % 60);
             }
         }
-        return String.format("%02d:%02d", ans / 60, ans % 60);
+
+        return answer;
     }
 
     static int timeCal(String s) {
         int h = Integer.parseInt(s.substring(0, 2));
         int m = Integer.parseInt(s.substring(3, 5));
+
         return h * 60 + m;
     }
 }
