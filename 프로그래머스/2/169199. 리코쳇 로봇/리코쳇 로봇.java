e56b47fc3bf350e6828e72c4f6415e205378d614
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-12 11:57:30 +0900

    [level 2] Title: 리코쳇 로봇, Time: 2.63 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -3,60 +3,73 @@ import java.util.*;
 class Solution {
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static int n, m;
-    static int[][] map, visited;
     static int sx, sy, ex, ey;
 
     public int solution(String[] board) {
-        int answer = 0;
-        n = board.length;
-        m = board[0].length();
-        map = new int[n][m];
-        visited = new int[n][m];
+        int answer = -1;
+
+        int n = board.length;
+        int m = board[0].length();
+
+        int[][] map = new int[n][m];
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                char ch = board[i].charAt(j);
-                if (ch == '.') {
-                    map[i][j] = 0;
-                } else if (ch == 'D') {
-                    map[i][j] = 1;
-                } else if (ch == 'R') {
-                    map[i][j] = 0;
+
+                if (board[i].charAt(j) == 'R') {
                     sx = i;
                     sy = j;
-                } else if (ch == 'G') {
-                    map[i][j] = 0;
+                } else if (board[i].charAt(j) == 'G') {
                     ex = i;
                     ey = j;
+                } else if (board[i].charAt(j) == 'D') {
+                    map[i][j] = 1;
                 }
             }
         }
-        Queue<int[]> q = new ArrayDeque<>();
-        visited[sx][sy] = 1;
-        q.offer(new int[]{sx, sy});
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
 
-            if (cur[0] == ex && cur[1] == ey) return visited[ex][ey] - 1;
+        boolean[][] visited = new boolean[n][m];
+
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{sx, sy, 0});
+        visited[sx][sy] = true;
+
+        while (!q.isEmpty()) {
+
+            int[] tmp = q.poll();
+
+            int x = tmp[0];
+            int y = tmp[1];
+            int cnt = tmp[2];
+
+            if (x == ex && y == ey) {
+                return cnt;
+            }
 
             for (int i = 0; i < 4; i++) {
-                int nx = cur[0];
-                int ny = cur[1];
+
+                int nx = x;
+                int ny = y;
 
                 while (true) {
+
                     int tx = nx + dx[i];
                     int ty = ny + dy[i];
+
                     if (tx < 0 || tx >= n || ty < 0 || ty >= m) break;
                     if (map[tx][ty] == 1) break;
+
                     nx = tx;
                     ny = ty;
                 }
-                if (visited[nx][ny] == 0) {
-                    visited[nx][ny] = visited[cur[0]][cur[1]] + 1;
-                    q.offer(new int[]{nx, ny});
-                }
+
+                if (visited[nx][ny]) continue;
+
+                visited[nx][ny] = true;
+                q.offer(new int[]{nx, ny, cnt + 1});
             }
         }
-        return -1;
+
+        return answer;
     }
 }
