176734d3af49cebebb80b911643889fd757b4e98
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-14 14:01:57 +0900

    [level 3] Title: 단속카메라, Time: 5.03 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884) 
 
 ### 성능 요약
 
-메모리: 58.5 MB, 시간: 11.64 ms
+메모리: 60.5 MB, 시간: 5.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 21일 17:34:12
+2026년 08월 14일 14:01:54
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,36 +1,16 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    static PriorityQueue<Data> pq = new PriorityQueue<>();
     public int solution(int[][] routes) {
-        int answer = 1;
-        for(int i = 0; i < routes.length; i++){
-            pq.offer(new Data(routes[i][0], routes[i][1]));
-        }
-        Data data = pq.poll();
-        int start = data.s, end = data.e;
-        while(!pq.isEmpty()){
-            Data temp = pq.poll();
-            if(temp.s > end){
+        int answer = 0;
+        Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
+        int camera = Integer.MIN_VALUE;
+        for (int[] route : routes) {
+            if (camera < route[0]) {
                 answer++;
-                end = temp.e;
-            }else{
-                end = Math.min(end, temp.e);   
+                camera = route[1];
             }
         }
         return answer;
-    }
-    class Data implements Comparable<Data>{
-        int s, e;
-        Data(int s, int e){
-            this.s = s;
-            this.e = e;
-        }
-        @Override
-        public int compareTo(Data o){
-            if(this.s == o.s)return Integer.compare(this.e, o.e);
-            return Integer.compare(this.s, o.s);
-        }
     }
 }
