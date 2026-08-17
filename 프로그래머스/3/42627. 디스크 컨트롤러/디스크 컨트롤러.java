ce736f0119f89b6ed46545ea52f56013081841bc
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-17 13:57:46 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.89 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,24 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        int answer = 0, n = jobs.length;
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);//요청시간 소요시간
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);//실행시간 오름차순
+        int answer = 0;
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);//
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         int idx = 0, time = 0, done = 0, sum = 0;
-        while(done < n){
-            while(idx < n && jobs[idx][0] <= time){
+        while (done < jobs.length) {
+            while (idx < jobs.length && jobs[idx][0] <= time) {
                 pq.offer(jobs[idx++]);
             }
-            
-            if(pq.isEmpty()){
+
+            if (pq.isEmpty()) {
                 time = jobs[idx][0];
-            }else{
+            } else {
                 int[] job = pq.poll();
-                time += job[1];
+                int start = job[0], runTime = job[1];
+                time += runTime;
                 done++;
-                sum += time - job[0];//종 - 도
+                sum += time - start;
             }
         }
-        return sum / n;
+        return sum / jobs.length;
     }
 }
