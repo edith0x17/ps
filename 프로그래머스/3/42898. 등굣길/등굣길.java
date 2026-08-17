01e9f7f76afed118cbaa1f9404314b4a2ec870f0
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-17 15:41:32 +0900

    [level 3] Title: 등굣길, Time: 0.41 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,19 +1,37 @@
+import java.util.*;
+
 class Solution {
+    static int INF = Integer.MAX_VALUE;
+    static int mod = 1_000_000_007;
+
     public int solution(int m, int n, int[][] puddles) {
-        int MOD = 1_000_000_007;
         int[][] dp = new int[n + 1][m + 1];
+
         for (int[] puddle : puddles) {
-            dp[puddle[1]][puddle[0]] = -1;
+            int x = puddle[0];
+            int y = puddle[1];
+
+            dp[y][x] = INF;
         }
-        dp[1][1] = 1;
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (i == 1 && j == 1) continue;
 
-                if (dp[i][j] == -1) dp[i][j] = 0;
-                else dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % MOD;
+                if (i == 1 && j == 1) {
+                    dp[i][j] = 1;
+                }
+
+                else if (dp[i][j] == INF) {
+                    dp[i][j] = 0;
+                }
+
+                else {
+                    dp[i][j] =
+                            (dp[i][j - 1] + dp[i - 1][j]) % mod;
+                }
             }
         }
+
         return dp[n][m];
     }
 }
