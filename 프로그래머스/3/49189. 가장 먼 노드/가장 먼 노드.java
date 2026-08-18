29525a82b015c6a66e0d4f3b56121956b30a83b8
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-18 17:08:11 +0900

    [level 3] Title: 가장 먼 노드, Time: 16.23 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,43 +1,58 @@
-//59 -> 
-import java.io.*;
 import java.util.*;
 
 class Solution {
     static ArrayList<Integer>[] adj;
-    static int[] visited;
-    static int answer = 0;
+    static boolean[] visited;
+
     public int solution(int n, int[][] edge) {
+        int answer = 0;
+
         adj = new ArrayList[n + 1];
-        for(int i = 0; i < n + 1; i++){
+
+        for (int i = 0; i <= n; i++) {
             adj[i] = new ArrayList<>();
         }
-        visited = new int[n + 1];
-        for(int i = 0; i < edge.length; i++){
-            int f = edge[i][0];
-            int t = edge[i][1];
-            adj[f].add(t); 
-            adj[t].add(f); 
+
+        for (int[] tmp : edge) {
+            int f = tmp[0];
+            int t = tmp[1];
+
+            adj[f].add(t);
+            adj[t].add(f);
         }
-        Queue<Integer> q = new ArrayDeque<>();
-        visited[1] = 1;
-        q.offer(1);
-        while(!q.isEmpty()){
-            int here = q.poll();
-            
-            for(int there: adj[here]){
-                if(visited[there] != 0)continue;
-                visited[there] = visited[here] + 1;
-                q.offer(there);
+
+        visited = new boolean[n + 1];
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{1, 0});
+        visited[1] = true;
+
+        int mx = 0;
+        int cnt = 0;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            int here = cur[0];
+            int dist = cur[1];
+
+            if (dist > mx) {
+                mx = dist;
+                cnt = 1;
+            } else if (dist == mx) {
+                cnt++;
+            }
+
+            for (int there : adj[here]) {
+                if (visited[there]) continue;
+
+                visited[there] = true;
+                q.offer(new int[]{there, dist + 1});
             }
         }
-        //System.out.println(Arrays.toString(visited));
-        int mx = Integer.MIN_VALUE;
-        for(int i = 1; i < n + 1; i++){
-            mx = Math.max(mx, visited[i]);
-        }
-        for(int i = 1; i < n + 1; i++){
-            if(mx == visited[i])answer++;
-        }
+
+        answer = cnt;
+
         return answer;
     }
 }
