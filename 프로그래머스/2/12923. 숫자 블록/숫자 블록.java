c1dca18de9e497a21a44b25882bbfaa8a844e025
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-24 16:55:56 +0900

    [level 2] Title: 숫자 블록, Time: 94.96 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -5,19 +5,18 @@ class Solution {
         int[] answer = new int[(int) (end - begin) + 1];
         int idx = 0;
         for (long i = begin; i <= end; i++) {
-            answer[idx++] = findNum(i);
+            answer[idx++] = (int) findNum(i);
         }
         return answer;
     }
 
-    static int findNum(long num) {
+    static long findNum(long num) {
         if (num == 1) return 0;
         int ret = 1;
-        for (long i = 2; i * i <= num; i++) {
+        for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) {
-                long big = num / i;
-                if (big <= 10_000_000) return (int) big;
-                ret = (int) i;
+                if (num / i <= 10_000_000) return num / i;
+                ret = i;
             }
         }
         return ret;
