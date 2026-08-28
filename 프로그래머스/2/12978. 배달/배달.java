cacd07fb1c8eb202d22f4713bb354fc4ad475a5c
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-28 15:06:26 +0900

    [level 2] Title: 배달, Time: 5.26 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,40 +1,38 @@
 import java.util.*;
 
 class Solution {
-    static ArrayList<int[]>[] graph;
-    static int[] d;
+    static ArrayList<int[]>[] adj;
 
     public int solution(int n, int[][] road, int k) {
-        graph = new ArrayList[n + 1];
-        for (int i = 0; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+        adj = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            adj[i] = new ArrayList<>();
         }
         for (int[] r : road) {
-            int from = r[0], to = r[1], dist = r[2];
-            graph[from].add(new int[]{to, dist});
-            graph[to].add(new int[]{from, dist});
+            int s = r[0], e = r[1], dist = r[2];
+            adj[s].add(new int[]{e, dist});
+            adj[e].add(new int[]{s, dist});
         }
 
-        d = new int[n + 1];
+        int[] d = new int[n + 1];
         Arrays.fill(d, Integer.MAX_VALUE);
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        d[1] = 0;//d[there] = 지금까지 알고 있던 1 → there 최단거리
+        d[1] = 0;
         pq.offer(new int[]{1, 0});
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
-            int here = cur[0], hereDist = cur[1];//hereDist = 시작점 1 → here까지의 누적 거리
+            int here = cur[0], hereDist = cur[1];
 
             if (d[here] < hereDist) continue;
 
-            for (int[] next : graph[here]) {
-                int there = next[0], thereDist = next[1];//thereDist = here → there의 간선 거리
+            for (int[] tmp : adj[here]) {
+                int there = tmp[0], thereDist = tmp[1];
                 if (d[there] > hereDist + thereDist) {
                     d[there] = hereDist + thereDist;
                     pq.offer(new int[]{there, hereDist + thereDist});
                 }
             }
         }
-
         int answer = 0;
         for (int i = 1; i <= n; i++) {
             if (d[i] <= k) answer++;
