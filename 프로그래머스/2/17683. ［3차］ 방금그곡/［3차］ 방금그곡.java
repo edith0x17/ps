432af946ff744e5414d36f6900d4b34e6befe9fc
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-08-30 16:16:03 +0900

    [level 2] Title: [3차] 방금그곡, Time: 4.72 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -3,47 +3,39 @@ import java.util.*;
 class Solution {
     public String solution(String m, String[] musicinfos) {
         String answer = "";
-        int maxTime = -1;
-
         m = change(m);
-
-        for (String tmp : musicinfos) {
-            String[] ss = tmp.split(",");
-            int s = timeCal(ss[0]), e = timeCal(ss[1]);
-            int playTime = e - s;
-
-            String song = change(ss[3]);
-
+        int mx = Integer.MIN_VALUE;
+        for (String s : musicinfos) {
+            String[] ss = s.split(",");
+            int start = timeCal(ss[0]), end = timeCal(ss[1]);
+            int playTime = end - start;
+            String title = ss[2];
+            String music = change(ss[3]);
             StringBuilder played = new StringBuilder();
             for (int i = 0; i < playTime; i++) {
-                played.append(song.charAt(i % song.length()));
+                played.append(music.charAt(i % music.length()));
             }
-
-            if (played.toString().contains(m)) {//노래.contains(m)
-                if (playTime > maxTime) {
-                    maxTime = playTime;
-                    answer = ss[2];//제목
+            if (played.toString().contains(m)) {
+                if (playTime > mx) {
+                    mx = playTime;
+                    answer = title;
                 }
             }
         }
-
         if (answer.equals("")) return "(None)";
         return answer;
+    }
+
+    static String change(String s) {
+        return s.replace("C#", "c").replace("D#", "d")
+                .replace("F#", "f")
+                .replace("G#", "g")
+                .replace("A#", "a");
     }
 
     static int timeCal(String s) {
         int h = Integer.parseInt(s.substring(0, 2));
         int m = Integer.parseInt(s.substring(3, 5));
-
         return h * 60 + m;
-    }
-
-    static String change(String s) {
-        return s.replace("C#", "c")
-                .replace("D#", "d")
-                .replace("F#", "f")
-                .replace("G#", "g")
-                .replace("A#", "a")
-                .replace("B#", "b");
     }
 }
