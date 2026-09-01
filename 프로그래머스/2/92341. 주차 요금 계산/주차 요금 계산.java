536b6e56cec11eb5cc11090543f6e91359bfd645
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-09-01 15:21:27 +0900

    [level 2] Title: 주차 요금 계산, Time: 28.33 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,42 +2,38 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        HashMap<String, Integer> inTime = new HashMap<>();   //현재 입차 시간
-        HashMap<String, Integer> total = new HashMap<>();    //누적 주차 시간
-        for (String s : records) {
+        int[] answer = {};
+        Map<String, Integer> mp1 = new HashMap<>();//<차, 시간>
+        TreeMap<String, Integer> mp2 = new TreeMap<>();//<차, 시간>
+        for(String s : records){
             String[] ss = s.split(" ");
-            if (ss[2].equals("IN")) {
-                inTime.put(ss[1], timeCal(ss[0]));
-            } else {//"OUT"
-                int time = timeCal(ss[0]) - inTime.get(ss[1]);
-                total.put(ss[1], total.getOrDefault(ss[1], 0) + time);
-                inTime.remove(ss[1]);
+            //"05:34 5961 IN"
+            int time = timeCal(ss[0]);
+            String num = ss[1];
+            if(ss[2].equals("IN")){
+                mp1.put(num, time);
+            }else{//OUT
+                int start = mp1.get(num), end = timeCal(ss[0]);
+                mp1.remove(num);
+                mp2.put(num, mp2.getOrDefault(num, 0) + end - start);
             }
         }
-        for (Map.Entry<String, Integer> entry : inTime.entrySet()) {
-            int time = timeCal("23:59") - entry.getValue();
-            total.put(entry.getKey(), total.getOrDefault(entry.getKey(), 0) + time);
+        for(Map.Entry<String, Integer> entry : mp1.entrySet()){
+            mp2.put(entry.getKey(), mp2.getOrDefault(entry.getKey(), 0) + timeCal("23:59") - entry.getValue());
         }
-
-        //차량번호 오름차순 정렬
-        ArrayList<String> cars = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : total.entrySet()) {
-            cars.add(entry.getKey());
-        }
-        Collections.sort(cars);
-        int[] answer = new int[total.size()];
+        answer = new int[mp2.size()];
         int idx = 0;
-        for (String car : cars) {
-            int time = total.get(car);
-            int price;
-            if (time <= fees[0]) price = fees[1];
-            else price = fees[1] + (int) Math.ceil((double) (time - fees[0]) / fees[2]) * fees[3];
+        for(Map.Entry<String, Integer> entry : mp2.entrySet()){
+            int time = entry.getValue();
+            int price = fees[1];
+            System.out.println((int)Math.ceil((time - fees[0]) / fees[2]));
+            if(time > fees[0])price += (int)Math.ceil(((double)time - fees[0]) / fees[2]) * fees[3];
             answer[idx++] = price;
         }
         return answer;
     }
-
-    static int timeCal(String s) {
+    
+    static int timeCal(String s){
         int h = Integer.parseInt(s.substring(0, 2));
         int m = Integer.parseInt(s.substring(3, 5));
         return h * 60 + m;
