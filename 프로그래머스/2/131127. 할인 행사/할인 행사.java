bc5891b608741371d0d6340b77a18f03585db5b0
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-09-01 15:50:23 +0900

    [level 2] Title: 할인 행사, Time: 65.77 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,31 +1,21 @@
-// 54
 import java.util.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        Map<String, Integer> wt = new HashMap<>();
-        for(int i = 0; i < want.length; i++){
-            wt.put(want[i], number[i]);
+        Map<String, Integer> mp = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            mp.put(want[i], number[i]);
         }
-        for(int i = 0; i <= discount.length - 10; i++){
-            Map<String, Integer> tmp = new HashMap<>();
-            for(int j = i; j < i + 10; j++){
-                tmp.put(discount[j], tmp.getOrDefault(discount[j], 0) + 1);
+        for (int start = 0; start <= discount.length - 10; start++) {
+            Map<String, Integer> tmp = new HashMap<>(mp);
+            for (int i = start; i < start + 10; i++) {
+                String item = discount[i];
+                tmp.put(item, tmp.getOrDefault(item, 0) - 1);
+                if (tmp.get(item) <= 0) tmp.remove(item);
             }
-            if(check(wt, tmp))answer++;
+            if (tmp.isEmpty()) answer++;
         }
         return answer;
-    }
-    
-    static boolean check(Map<String, Integer> wt, Map<String, Integer> tmp){
-        for(Map.Entry<String, Integer> entry : wt.entrySet()){
-                String s = entry.getKey();
-                int num = entry.getValue();
-                if(tmp.containsKey(s)){
-                    if(tmp.get(s) < num)return false;
-                }else return false;
-        }
-        return true;
     }
 }
