9c59fdc75f80f8359c18481b922c629f9036383d
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-09-10 15:03:00 +0900

    [level 2] Title: 롤케이크 자르기, Time: 132.59 ms, Memory: 210 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,24 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        Set<Integer> left = new HashSet<>();
         Map<Integer, Integer> right = new HashMap<>();
-
-        // 처음에는 모든 topping을 right에 넣기
+        Set<Integer> left = new HashSet<>();
         for (int i : topping) {
             right.put(i, right.getOrDefault(i, 0) + 1);
         }
-        for (int x : topping) {
-            // x 하나를 왼쪽으로 이동
-            left.add(x);
-
-            // right에서 x의 개수 1 감소
-            // 개수가 0이면 right에서 remove
-            right.put(x, right.get(x) - 1);
-            if (right.get(x) == 0) right.remove(x);
-            
-            // left.size() == right.size()면 answer++
-            if (left.size() == right.size()) answer++;
+        for (int i : topping) {
+            //delete
+            right.put(i, right.getOrDefault(i, 0) - 1);
+            if (right.get(i) <= 0) right.remove(i);
+            //insert
+            left.add(i);
+            //pair
+            if (right.size() == left.size()) answer++;
         }
         return answer;
     }
