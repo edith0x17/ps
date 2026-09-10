777a35a8af3bcdf37d4c7ea8a0da6825047fd901
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-09-10 15:10:59 +0900

    [level 2] Title: 할인 행사, Time: 41.87 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -4,17 +4,25 @@ class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         Map<String, Integer> mp = new HashMap<>();
-        for (int i = 0; i < want.length; i++) {
+        for (int i = 0; i < number.length; i++) {
             mp.put(want[i], number[i]);
         }
-        for (int start = 0; start <= discount.length - 10; start++) {
-            Map<String, Integer> tmp = new HashMap<>(mp);
-            for (int i = start; i < start + 10; i++) {
-                String item = discount[i];
-                tmp.put(item, tmp.getOrDefault(item, 0) - 1);
-                if (tmp.get(item) <= 0) tmp.remove(item);
+        for (int i = 0; i <= discount.length - 10; i++) {
+            Map<String, Integer> tmp = new HashMap<>();
+
+            for (int j = i; j < i + 10; j++) {
+                tmp.put(discount[j], tmp.getOrDefault(discount[j], 0) + 1);
             }
-            if (tmp.isEmpty()) answer++;
+
+            boolean flag = true;
+            for (Map.Entry<String, Integer> entry : mp.entrySet()) {
+                //entry.getKey() entry.getValue()
+                if (tmp.get(entry.getKey()) != entry.getValue()) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) answer++;
         }
         return answer;
     }
