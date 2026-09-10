2a451be52b96651395d476091dece921c5464715
edith0x17 <155926909+edith0x17@users.noreply.github.com>
2026-09-10 15:53:48 +0900

    [level 2] Title: 큰 수 만들기, Time: 57.36 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -4,24 +4,25 @@ class Solution {
     public String solution(String number, int k) {
         String answer = "";
         Stack<Integer> stk = new Stack<>();
-        stk.push(number.charAt(0) - '0');
         int cnt = 0;
-        for (int i = 1; i < number.length(); i++) {
+        for (int i = 0; i < number.length(); i++) {
             int num = number.charAt(i) - '0';
-            while (cnt < k && !stk.isEmpty() && num > stk.peek()) {
+            while (!stk.isEmpty() && cnt < k && stk.peek() < num) {
                 cnt++;
                 stk.pop();
             }
             stk.push(num);
         }
-        while (cnt < k) {
+         while (cnt < k) {
             stk.pop();
             cnt++;
         }
+
         StringBuilder sb = new StringBuilder();
-        for (int num : stk) {
-            sb.append(num);
+        for (int i : stk) {
+            sb.append(i + "");
         }
-        return sb.toString();
+        answer = sb.toString();
+        return answer;
     }
 }
